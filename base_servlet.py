a3f687628b887a3f63320daea8ca24a131de9ad4
Mike Lambert <mlambert@gmail.com>
2010-06-05 22:26:48 +0000

    use object_keys as our basis instead of object_ids
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@96 67443d69-2279-40e3-8022-4dbfd14c1b02

--- base_servlet.py
+++ base_servlet.py
@@ -156,59 +156,51 @@ class BatchLookup(object):
     OBJECT_EVENT_MEMBERS = 'EVENT_MEMBERS'
     OBJECT_FQL = 'FQL'
 
-    def _memcache_user_key(self, user_id):
-        return 'FacebookUser.%s.%s' % (self.fb_uid, user_id)
-    def _memcache_event_key(self, event_id):
-        return 'FacebookEvent.%s.%s' % (self.fb_uid, event_id)
-    def _memcache_event_members_key(self, event_id):
-        return 'FacebookEventMembers.%s.%s' % (self.fb_uid, event_id)
-    def _memcache_fql_key(self, fql):
-        return 'FacebookFql.%s.%s' % (self.fb_uid, fql)
+    def _memcache_key(self, object_key):
+        return 'Facebook.%s.%s' % (self.fb_uid, object_key)
 
-    def _build_user_rpcs(self, user_id):
-        return dict(
-            profile=self._fetch_rpc('%s' % user_id),
-            friends=self._fetch_rpc('%s/friends' % user_id),
-            events=self._fetch_rpc('%s/events' % user_id)
-        )
-    def _build_event_rpcs(self, event_id):
-        return dict(
-            info=self._fetch_rpc('%s' % event_id),
-            picture=self._fetch_rpc('%s/picture' % event_id),
-        )
-    def _build_event_members_rpcs(self, event_id):
-        return dict(
-            attending=self._fetch_rpc('%s/attending' % event_id),
-            maybe=self._fetch_rpc('%s/maybe' % event_id),
-            declined=self._fetch_rpc('%s/declined' % event_id),
-            noreply=self._fetch_rpc('%s/noreply' % event_id),
-        )
+    def _is_cacheable(self, object_key, this_object):
+        object_id, object_type = object_key
+        if object_type != self.OBJECT_EVENT:
+            return True
+        elif  'info' in this_object and this_object['info']['privacy'] == 'OPEN':
+            return True
+        else:
+            return False
 
-    def _build_fql_rpcs(self, fql):
-        rpc = urlfetch.create_rpc()
-        url = "https://api.facebook.com/method/fql.query?%s" % urllib.urlencode(dict(query=fql, access_token=self.fb_graph.access_token, format='json'))
-        urlfetch.make_fetch_call(rpc, url)
-        return dict(fql=rpc)
+    def _get_rpcs(self, object_key):
+        object_id, object_type = object_key
+        if object_type == self.OBJECT_USER:
+            return dict(
+                profile=self._fetch_rpc('%s' % object_id),
+                friends=self._fetch_rpc('%s/friends' % object_id),
+                events=self._fetch_rpc('%s/events' % object_id)
+            )
+        elif object_type == self.OBJECT_EVENT:
+            return dict(
+                info=self._fetch_rpc('%s' % object_id),
+                picture=self._fetch_rpc('%s/picture' % object_id),
+            )
+        elif object_type == self.OBJECT_EVENT_MEMBERS:
+            return dict(
+                attending=self._fetch_rpc('%s/attending' % object_id),
+                maybe=self._fetch_rpc('%s/maybe' % object_id),
+                declined=self._fetch_rpc('%s/declined' % object_id),
+                noreply=self._fetch_rpc('%s/noreply' % object_id),
+            )
+        elif object_type == self.OBJECT_FQL:
+            rpc = urlfetch.create_rpc()
+            url = "https://api.facebook.com/method/fql.query?%s" % urllib.urlencode(dict(query=object_id, access_token=self.fb_graph.access_token, format='json'))
+            urlfetch.make_fetch_call(rpc, url)
+            return dict(fql=rpc)
+        else:
+            raise Exception("Unknown object type %s" % object_type)
 
-    _key_generator = {
-        OBJECT_USER: _memcache_user_key,
-        OBJECT_EVENT: _memcache_event_key,
-        OBJECT_EVENT_MEMBERS: _memcache_event_members_key,
-        OBJECT_FQL: _memcache_fql_key,
-    }
-
-    _rpc_generator = {
-        OBJECT_USER: _build_user_rpcs,
-        OBJECT_EVENT: _build_event_rpcs,
-        OBJECT_EVENT_MEMBERS: _build_event_members_rpcs,
-        OBJECT_FQL: _build_fql_rpcs,
-    }
-    
     def __init__(self, fb_uid, fb_graph, allow_memcache=True):
         self.fb_uid = fb_uid
         self.fb_graph = fb_graph
         self.allow_memcache = allow_memcache
-        self.object_ids = set()
+        self.object_keys = set()
 
     def _fetch_rpc(self, path):
         rpc = urlfetch.create_rpc()
@@ -218,19 +210,19 @@ class BatchLookup(object):
 
     def lookup_user(self, user_id):
         assert user_id
-        self.object_ids.add((user_id, self.OBJECT_USER))
+        self.object_keys.add((user_id, self.OBJECT_USER))
 
     def lookup_event(self, event_id):
         assert event_id
-        self.object_ids.add((event_id, self.OBJECT_EVENT))
+        self.object_keys.add((event_id, self.OBJECT_EVENT))
 
     def lookup_event_members(self, event_id):
         assert event_id
-        self.object_ids.add((event_id, self.OBJECT_EVENT_MEMBERS))
+        self.object_keys.add((event_id, self.OBJECT_EVENT_MEMBERS))
 
     def lookup_fql(self, fql_query):
         assert fql_query
-        self.object_ids.add((fql_query, self.OBJECT_FQL))
+        self.object_keys.add((fql_query, self.OBJECT_FQL))
 
     def data_for_user(self, user_id):
         assert user_id
@@ -248,96 +240,78 @@ class BatchLookup(object):
         assert fql_query
         return self.objects[(fql_query, self.OBJECT_FQL)]
 
-    def _get_objects_from_memcache(self, object_ids):
+    def _get_objects_from_memcache(self, object_keys):
         memcache_keys_to_ids = {}
-        for object_id, object_type in object_ids:
-            key_func = self._key_generator[object_type]
-            memcache_keys_to_ids[key_func(self, object_id)] = (object_id, object_type)
-        object_keys_to_objects = memcache.get_multi(memcache_keys_to_ids.keys())
+        memcache_keys_to_object_keys = dict((self._memcache_key(object_key), object_key) for object_key in object_keys)
+        memcache_keys_to_objects = memcache.get_multi(memcache_keys_to_object_keys.keys())
 
-        objects = dict((memcache_keys_to_ids[k], o) for (k, o) in object_keys_to_objects.iteritems())
+        objects = dict((memcache_keys_to_object_keys[k], o) for (k, o) in memcache_keys_to_objects.iteritems())
 
-        get_size = len(pickle.dumps(object_keys_to_objects))
+        get_size = len(pickle.dumps(memcache_keys_to_objects))
         logging.info("BatchLookup: get_multi return size: %s", get_size)
-        logging.info("BatchLookup: get_multi objects: %s", object_ids)
+        logging.info("BatchLookup: get_multi objects: %s", objects.keys())
         return objects
 
     def finish_loading(self):
         if self.allow_memcache:
-            self.objects = self._get_objects_from_memcache(self.object_ids)
-            object_ids_to_lookup = list(set(self.object_ids).difference(self.objects.keys()))
-            logging.info("BatchLookup: get_multi missed objects: %s", object_ids_to_lookup)
+            self.objects = self._get_objects_from_memcache(self.object_keys)
+            object_keys_to_lookup = list(set(self.object_keys).difference(self.objects.keys()))
+            logging.info("BatchLookup: get_multi missed objects: %s", object_keys_to_lookup)
         else:
-            object_ids_to_lookup = list(self.object_ids)
+            object_keys_to_lookup = list(self.object_keys)
 
         FB_FETCH_COUNT = 10 # number of objects, each of which may be 1-5 RPCs
-        for i in range(0, len(object_ids_to_lookup), FB_FETCH_COUNT):
-            fetched_objects = self._fetch_object_ids(object_ids_to_lookup[i:i+FB_FETCH_COUNT])    
+        for i in range(0, len(object_keys_to_lookup), FB_FETCH_COUNT):
+            fetched_objects = self._fetch_object_keys(object_keys_to_lookup[i:i+FB_FETCH_COUNT])    
             # Always store latest fetched stuff in memcache, regardless of self.allow_memcache
-            self._memcache_objects(fetched_objects)
+            self._store_objects_into_memcache(fetched_objects)
             self.objects.update(fetched_objects)
 
-    @staticmethod
-    def _map_rpc_to_json(rpc):
+    @classmethod
+    def _map_rpc_to_data(cls, object_key, object_rpc_name, object_rpc):
+        object_id, object_type = object_key
         try:
-            result = rpc.get_result()
+            result = object_rpc.get_result()
             if result.status_code == 200:
-                text = result.content
-                return simplejson.loads(text)
+                if object_type == cls.OBJECT_EVENT and object_rpc_name == 'picture':
+                    return result.final_url
+                else:
+                    text = result.content
+                    return simplejson.loads(text)
         except urlfetch.DownloadError:
-            logging.error("Error downloading: %s", rpc.request.url())
+            logging.error("Error downloading: %s", object_rpc.request.url())
         return None
 
-    @staticmethod
-    def _map_rpc_to_url(rpc):
-        try:
-            result = rpc.get_result()
-            if result.status_code == 200:
-                return result.final_url
-        except urlfetch.DownloadError, e:
-            logging.error("Error downloading: %s", rpc.request.url())
-        return None
-
-    def _fetch_object_ids(self, object_ids_to_lookup):
-        logging.info("Looking up IDs: %s", object_ids_to_lookup)
+    def _fetch_object_keys(self, object_keys_to_lookup):
+        logging.info("Looking up IDs: %s", object_keys_to_lookup)
         # initiate RPCs
-        self.object_ids_to_rpcs = {}
-        for object_id, object_type in object_ids_to_lookup:
-            rpc_func = self._rpc_generator[object_type]
-            self.object_ids_to_rpcs[(object_id, object_type)] = rpc_func(self, object_id)
+        self.object_keys_to_rpcs = {}
+        for object_key in object_keys_to_lookup:
+            self.object_keys_to_rpcs[object_key] = self._get_rpcs(object_key)
     
         # fetch RPCs
         fetched_objects = {}
-        for (object_id, object_type), object_rpc_dict in self.object_ids_to_rpcs.iteritems():
+        for object_key, object_rpc_dict in self.object_keys_to_rpcs.iteritems():
             this_object = {}
             object_is_bad = False
             for object_rpc_name, object_rpc in object_rpc_dict.iteritems():
-                if object_type == self.OBJECT_EVENT and object_rpc_name == 'picture':
-                    object_json = self._map_rpc_to_url(object_rpc)
-                else:
-                    object_json = self._map_rpc_to_json(object_rpc)
+                object_json = self._map_rpc_to_data(object_key, object_rpc_name, object_rpc)
                 if object_json:
                     this_object[object_rpc_name] = object_json
                 else:
                     object_is_bad = True
             if object_is_bad:
-                logging.error("Failed to complete object: %s, only have keys %s", object_id, this_object.keys())
+                logging.error("Failed to complete object: %s, only have keys %s", object_key, this_object.keys())
             else:
-                fetched_objects[(object_id, object_type)] = this_object
+                fetched_objects[object_key] = this_object
 
         return fetched_objects
 
-    def _memcache_objects(self, fetched_objects):
+    def _store_objects_into_memcache(self, fetched_objects):
         memcache_set = {}
-        for (object_id, object_type), this_object in fetched_objects.iteritems():
-            cacheable = False
-            if object_type != self.OBJECT_EVENT:
-                cacheable = True
-            elif  'info' in this_object and this_object['info']['privacy'] == 'OPEN':
-                cacheable = True
-            if cacheable:
-                key_func = self._key_generator[object_type]
-                memcache_set[key_func(self, object_id)] = this_object
+        for object_key, this_object in fetched_objects.iteritems():
+            if self._is_cacheable(object_key, this_object):
+                memcache_set[self._memcache_key(object_key)] = this_object
 
         if memcache_set:
             for (k, v) in memcache_set.iteritems():
