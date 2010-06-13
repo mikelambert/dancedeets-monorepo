34f907d3164cbd52b4b2c65e05c1d5f317f6be92
Mike Lambert <mlambert@gmail.com>
2010-06-13 09:29:10 +0000

    fetch smaller amounts so we don't overflow fb api
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@122 67443d69-2279-40e3-8022-4dbfd14c1b02

--- logic/backgrounder.py
+++ logic/backgrounder.py
@@ -2,7 +2,7 @@ from google.appengine.api.labs import taskqueue
 
 def load_events(fb_event_ids, allow_memcache=True):
     fb_uid = '701004' # Mike Lambert
-    task_size = 20
+    task_size = 10
     allow_memcache_arg = (allow_memcache and '1' or '0')
     for i in range(0, len(fb_event_ids), task_size):
         taskqueue.add(url='/tasks/load_events', params=dict(user_id=fb_uid, event_ids=','.join(str(x) for x in fb_event_ids[i:i+task_size]), allow_memcache=allow_memcache_arg))
