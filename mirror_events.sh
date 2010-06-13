adc4799e1b7c5e6cecc0c3c7252df64e7180d034
Mike Lambert <mlambert@gmail.com>
2010-06-13 19:53:36 +0000

    fix for cmdline syntax
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@123 67443d69-2279-40e3-8022-4dbfd14c1b02

--- mirror_events.sh
+++ mirror_events.sh
@@ -1,3 +1,3 @@
 rm events.txt
 appcfg.py download_data --application=dancedeets --kind=DBEvent --url=http://dancedeets.appspot.com/remote_api --filename=events.txt
-ppcfg.py upload_data --restore --application=dancedeets --kind=DBEvent --url=http://127.0.0.1:8080/remote_api --filename=events.txt
+appcfg.py upload_data --application=dancedeets --kind=DBEvent --url=http://127.0.0.1:8080/remote_api --filename=events.txt
