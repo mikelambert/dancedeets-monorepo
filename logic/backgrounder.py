0276faa06999c1d523cac3ed72409a541e1014a9
Mike Lambert <mlambert@gmail.com>
2010-06-16 09:22:04 +0000

    switch to GET for /tasks/ so we can see what is going on
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@130 67443d69-2279-40e3-8022-4dbfd14c1b02

--- logic/backgrounder.py
+++ logic/backgrounder.py
@@ -7,7 +7,7 @@ def load_events(fb_event_ids, allow_cache=True):
     task_size = 10
     allow_cache_arg = (allow_cache and '1' or '0')
     for i in range(0, len(fb_event_ids), task_size):
-        taskqueue.add(url='/tasks/load_events?' + urllib.urlencode(dict(user_id=fb_uid, event_ids=','.join(str(x) for x in fb_event_ids[i:i+task_size]), allow_cache=allow_cache_arg)))
+        taskqueue.add(method='GET', url='/tasks/load_events?' + urllib.urlencode(dict(user_id=fb_uid, event_ids=','.join(str(x) for x in fb_event_ids[i:i+task_size]), allow_cache=allow_cache_arg)))
     # Only once we've finished loading the raw events do we bother loading any event members
     for i in range(0, len(fb_event_ids), task_size):
-        taskqueue.add(url='/tasks/load_event_members?'+urllib.urlencode(dict(user_id=fb_uid, event_ids=','.join(str(x) for x in fb_event_ids[i:i+task_size]), allow_cache=allow_cache_arg)))
+        taskqueue.add(method='GET', url='/tasks/load_event_members?'+urllib.urlencode(dict(user_id=fb_uid, event_ids=','.join(str(x) for x in fb_event_ids[i:i+task_size]), allow_cache=allow_cache_arg)))
