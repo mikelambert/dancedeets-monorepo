60d5262025c51ed3355cf5376706ebcb9d7237dc
Mike Lambert <mlambert@gmail.com>
2010-08-10 22:08:42 +0000

    use special gmaps api key to avoid appengine IP blockage, and support running in a non-appengine environment
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@160 67443d69-2279-40e3-8022-4dbfd14c1b02

--- locations.py
+++ locations.py
@@ -1,3 +1,7 @@
+import base64
+import hashlib
+import hmac
+import logging
 import math
 import urllib
 import urllib2
@@ -9,6 +13,7 @@ except ImportError:
 try:
     import smemcache
 except ImportError:
+    smemcache = None
     pass
 
 # http://en.wikipedia.org/wiki/Mile
@@ -23,9 +28,22 @@ class GeocodeException(Exception):
     pass
 
 def _get_geocoded_data(address):
-    url = "http://maps.google.com/maps/api/geocode/json?%s" % urllib.urlencode(dict(address=address, sensor='false'))
+    unsigned_url_path = "/maps/api/geocode/json?%s" % urllib.urlencode(dict(address=address, sensor='false', client='free-dancedeets'))
+    private_key = 'zj918QnslsoOQHl4kLjv-ZCgsDE='
+    decoded_key = base64.urlsafe_b64decode(private_key)
+    print unsigned_url_path
+    signature = hmac.new(decoded_key, unsigned_url_path, hashlib.sha1)
+    encoded_signature = base64.urlsafe_b64encode(signature.digest())
 
-    json_result = simplejson.load(urllib.urlopen(url))
+    url = "http://maps.google.com%s&signature=%s" % (unsigned_url_path, encoded_signature)
+    print url
+
+    results = urllib.urlopen(url).read()
+    try:
+        json_result = simplejson.loads(results)
+    except simplejson.decoder.JSONDecodeError, e:
+        logging.error("Error decoding json: %s: %s", e, results)
+        return None
     if json_result['status'] == 'ZERO_RESULTS':
         return None
     if json_result['status'] != 'OK':
@@ -50,10 +68,11 @@ def _raw_get_geocoded_location(address):
 
 def get_geocoded_location(location):
   memcache_key = memcache_location_key(location)
-  geocoded_location = smemcache.get(memcache_key)
+  geocoded_location = smemcache and smemcache.get(memcache_key)
   if not geocoded_location:
     geocoded_location = _raw_get_geocoded_location(location)
-    smemcache.set(memcache_key, geocoded_location, LOCATION_EXPIRY)
+    if smemcache:
+      smemcache.set(memcache_key, geocoded_location, LOCATION_EXPIRY)
   return geocoded_location
 
 
@@ -88,9 +107,10 @@ def _raw_get_country_for_location(location_name):
 
 def get_country_for_location(location_name):
   memcache_key = _memcache_country_key(location_name)
-  country = smemcache.get(memcache_key)
+  country = smemcache and smemcache.get(memcache_key)
   if not country:
     country = _raw_get_country_for_location(location_name)
-    smemcache.set(memcache_key, country, LOCATION_EXPIRY)
+    if smemcache:
+      smemcache.set(memcache_key, country, LOCATION_EXPIRY)
   return country
 
