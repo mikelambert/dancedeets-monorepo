f132912163dcab41936ec2b144e7469de1b5c092
Mike Lambert <mlambert@gmail.com>
2010-08-14 00:02:29 +0000

    clean up after ourselves
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@166 67443d69-2279-40e3-8022-4dbfd14c1b02

--- mirror_events.sh
+++ mirror_events.sh
@@ -1,3 +1,4 @@
 rm events.txt
 appcfg.py download_data --application=dancedeets --kind=DBEvent --url=http://dancedeets.appspot.com/remote_api --filename=events.txt
 appcfg.py upload_data --application=dancedeets --kind=DBEvent --url=http://127.0.0.1:8080/remote_api --filename=events.txt
+rm bulkloader-*
