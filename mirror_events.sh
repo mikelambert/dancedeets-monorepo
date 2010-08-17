ba94fca05d248a83d0af3e363844f7516d5b5e15
Mike Lambert <mlambert@gmail.com>
2010-08-17 06:58:16 +0000

    grab fb cached data as well
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@168 67443d69-2279-40e3-8022-4dbfd14c1b02

--- mirror_events.sh
+++ mirror_events.sh
@@ -1,4 +1,5 @@
 rm events.txt
-appcfg.py download_data --application=dancedeets --kind=DBEvent --url=http://dancedeets.appspot.com/remote_api --filename=events.txt
-appcfg.py upload_data --application=dancedeets --kind=DBEvent --url=http://127.0.0.1:8080/remote_api --filename=events.txt
+appcfg.py download_data --application=dancedeets --kind="DBEvent" --url=http://dancedeets.appspot.com/remote_api --filename=events.db
+appcfg.py download_data --application=dancedeets --kind="(DBEvent,FacebookCachedObject)" --url=http://dancedeets.appspot.com/remote_api --filename=all_events_data.db
+appcfg.py upload_data --application=dancedeets --kind="(DBEvent,FacebookCachedObject)" --url=http://127.0.0.1:8080/remote_api --filename=all_events_data.db
 rm bulkloader-*
