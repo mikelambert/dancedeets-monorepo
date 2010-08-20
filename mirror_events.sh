5770928b69d32386d955a2b50ade326dc2534fc1
Mike Lambert <mlambert@gmail.com>
2010-08-20 10:49:43 +0000

    Grab separate Event and FBCachedObject data stores, with proper cleanup
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@173 67443d69-2279-40e3-8022-4dbfd14c1b02

--- mirror_events.sh
+++ mirror_events.sh
@@ -1,5 +1,7 @@
-rm events.txt
+rm events.db
+rm all_events_data.db
 appcfg.py download_data --application=dancedeets --kind="DBEvent" --url=http://dancedeets.appspot.com/remote_api --filename=events.db
-appcfg.py download_data --application=dancedeets --kind="(DBEvent,FacebookCachedObject)" --url=http://dancedeets.appspot.com/remote_api --filename=all_events_data.db
-appcfg.py upload_data --application=dancedeets --kind="(DBEvent,FacebookCachedObject)" --url=http://127.0.0.1:8080/remote_api --filename=all_events_data.db
+appcfg.py download_data --application=dancedeets --kind="FacebookCachedObject" --url=http://dancedeets.appspot.com/remote_api --filename=fb_data.db
+appcfg.py upload_data --application=dancedeets --kind="DBEvent" --url=http://127.0.0.1:8080/remote_api --filename=events.db
+appcfg.py upload_data --application=dancedeets --kind="FacebookCachedObject" --url=http://127.0.0.1:8080/remote_api --filename=fb_data.db
 rm bulkloader-*
