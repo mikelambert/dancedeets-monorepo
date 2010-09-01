6f51b6b6df722916966955975c585c8c4037439b
Mike Lambert <mlambert@gmail.com>
2010-09-01 04:10:00 +0000

    Don't trust facebook event location data. Don't use it for geocoding info or for its expanded state names. Also fix a 'NOne' bug
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@191 67443d69-2279-40e3-8022-4dbfd14c1b02

--- events/eventdata.py
+++ events/eventdata.py
@@ -23,24 +23,86 @@ EVENT_IMAGE_LARGE = 'n'
 EVENT_IMAGE_SQUARE = 'q'
 EVENT_IMAGE_TYPES = [EVENT_IMAGE_SMALL, EVENT_IMAGE_MEDIUM, EVENT_IMAGE_LARGE, EVENT_IMAGE_SQUARE]
 
+
 def get_event_image_url(square_url, event_image_type):
     assert event_image_type in EVENT_IMAGE_TYPES
     final_url = square_url.replace('/q', '/%s' % event_image_type)
     return final_url
 
 
+states_abbrev2full = {
+    'AK': 'Alaska',
+    'AL': 'Alabama',
+    'AR': 'Arkansas',
+    'AS': 'American Samoa',
+    'AZ': 'Arizona',
+    'CA': 'California',
+    'CO': 'Colorado',
+    'CT': 'Connecticut',
+    'DC': 'District of Columbia',
+    'DE': 'Delaware',
+    'FL': 'Florida',
+    'GA': 'Georgia',
+    'GU': 'Guam',
+    'HI': 'Hawaii',
+    'IA': 'Iowa',
+    'ID': 'Idaho',
+    'IL': 'Illinois',
+    'IN': 'Indiana',
+    'KS': 'Kansas',
+    'KY': 'Kentucky',
+    'LA': 'Louisiana',
+    'MA': 'Massachusetts',
+    'MD': 'Maryland',
+    'ME': 'Maine',
+    'MI': 'Michigan',
+    'MN': 'Minnesota',
+    'MO': 'Missouri',
+    'MP': 'Northern Mariana Islands',
+    'MS': 'Mississippi',
+    'MT': 'Montana',
+    'NA': 'National',
+    'NC': 'North Carolina',
+    'ND': 'North Dakota',
+    'NE': 'Nebraska',
+    'NH': 'New Hampshire',
+    'NJ': 'New Jersey',
+    'NM': 'New Mexico',
+    'NV': 'Nevada',
+    'NY': 'New York',
+    'OH': 'Ohio',
+    'OK': 'Oklahoma',
+    'OR': 'Oregon',
+    'PA': 'Pennsylvania',
+    'PR': 'Puerto Rico',
+    'RI': 'Rhode Island',
+    'SC': 'South Carolina',
+    'SD': 'South Dakota',
+    'TN': 'Tennessee',
+    'TX': 'Texas',
+    'UT': 'Utah',
+    'VA': 'Virginia',
+    'VI': 'Virgin Islands',
+    'VT': 'Vermont',
+    'WA': 'Washington',
+    'WI': 'Wisconsin',
+    'WV': 'West Virginia',
+    'WY': 'Wyoming'
+}
+
+states_full2abbrev = {}
+for abbr, full in states_abbrev2full.iteritems():
+  states_full2abbrev[full] = abbr
+
 def get_geocoded_location_for_event(fb_event):
     event_info = fb_event['info']
     venue = event_info.get('venue', {})
-    address_components = [event_info.get('location'), venue.get('street'), venue.get('city'), venue.get('state'), venue.get('country')]
+    address_components = [event_info.get('location'), venue.get('street'), venue.get('city'), states_full2abbrev.get(venue.get('state'), venue.get('state')), venue.get('country')]
     address_components = [x for x in address_components if x]
     address = ', '.join(address_components)
-    results = {}
-    if venue.get('latitude') and venue.get('longitude'):
-        results['address'] = address
-        results['latlng'] = (venue['latitude'], venue['longitude'])
-    else:
-        results = locations.get_geocoded_location(address)
+    logging.info("For event = %s, address is %s", fb_event['info']['id'], address)
+    # Do not trust facebook for latitude/longitude data. It appears to treat LA as Louisiana, etc. So always geocode below.
+    results = locations.get_geocoded_location(address)
     return results
 
 
@@ -84,7 +146,7 @@ class DBEvent(db.Model):
         # set up any cached fields or bucketing or whatnot for this event
 
         if fb_dict['deleted']:
-            self.start_time = NOne
+            self.start_time = None
             self.end_time = None
             self.search_tags = []
             self.search_time_period = None
@@ -117,7 +179,7 @@ class DBEvent(db.Model):
             self.search_regions = cities.get_cities_within(self.latitude, self.longitude, REGION_RADIUS)
             logging.info("Nearest cities for %s are %s", self.address, self.search_regions)
             if not self.search_regions:
-                logging.error("Error no search regions for lat/lng (%s,%s) with address %s", self.latitude, self.longitude, self.address)
+                logging.error("Error no search regions for eid=%s with lat/lng (%s,%s) with address %s", self.fb_event_id, self.latitude, self.longitude, self.address)
         else:
             #TODO(lambert): find a better way of reporting/notifying about un-geocodeable addresses
             logging.error("No geocoding results for eid=%s is: %s", self.fb_event_id, results)
