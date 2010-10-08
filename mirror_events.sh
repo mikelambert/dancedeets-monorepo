165440544ce6e34176f7025fcbcb0d7900229f5c
Mike Lambert <mlambert@gmail.com>
2010-10-08 12:50:21 +0000

    add fetch support for potentialevents
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@349 67443d69-2279-40e3-8022-4dbfd14c1b02

--- mirror_events.sh
+++ mirror_events.sh
@@ -1,7 +1,9 @@
 rm events.db
 rm fb_data.db
 appcfg.py download_data --application=dancedeets --kind="DBEvent" --url=http://dancedeets.appspot.com/remote_api --filename=events.db
+appcfg.py download_data --application=dancedeets --kind="PotentialEvent" --url=http://dancedeets.appspot.com/remote_api --filename=potentialevents.db
 appcfg.py download_data --application=dancedeets --kind="FacebookCachedObject" --url=http://dancedeets.appspot.com/remote_api --filename=fb_data.db
 appcfg.py upload_data --application=dancedeets --kind="DBEvent" --url=http://127.0.0.1:8080/remote_api --filename=events.db
+appcfg.py upload_data --application=dancedeets --kind="PotentialEvent" --url=http://127.0.0.1:8080/remote_api --filename=potentialevents.db
 appcfg.py upload_data --application=dancedeets --kind="FacebookCachedObject" --url=http://127.0.0.1:8080/remote_api --filename=fb_data.db
 rm bulkloader-*
