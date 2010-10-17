9097ae7fa21e65ddc9fb808b28019969f74e7993
Mike Lambert <mlambert@gmail.com>
2010-10-17 15:18:41 +0000

    change logo. fix up share page to support twitter and facebook. cleanup footer but still need to add some links. fixup todos
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@439 67443d69-2279-40e3-8022-4dbfd14c1b02

--- servlets/share.py
+++ servlets/share.py
@@ -3,5 +3,6 @@ import base_servlet
 class ShareHandler(base_servlet.BaseRequestHandler):
     def get(self):
         self.finish_preload()
+        self.display['share_url'] = 'http://www.dancedeets.com/?referral=%s' % self.user.fb_uid
         self.render_template('share')
 
