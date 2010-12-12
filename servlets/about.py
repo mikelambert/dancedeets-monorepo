27c5f556e99345d7221bfae33e59d0edaa4435fd
Mike Lambert <mlambert@gmail.com>
2010-12-12 15:17:57 +0000

    don't require login for about page
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@567 67443d69-2279-40e3-8022-4dbfd14c1b02

--- servlets/about.py
+++ servlets/about.py
@@ -1,6 +1,9 @@
 import base_servlet
 
 class AboutHandler(base_servlet.BaseRequestHandler):
+    def requires_login(self):
+        return False
+
     def get(self):
         self.finish_preload()
         self.render_template('about')
