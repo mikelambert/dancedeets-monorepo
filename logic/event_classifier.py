5184bf163006f4d868a0d69a15d5cf320829bc8a
Mike Lambert <mlambert@gmail.com>
2010-12-28 18:01:03 +0000

    stricter filtering of potential events to rule out random clubs
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@583 67443d69-2279-40e3-8022-4dbfd14c1b02

--- logic/event_classifier.py
+++ logic/event_classifier.py
@@ -1,34 +1,43 @@
+import logging
 import re
 
-dance_keywords = [
+easy_dance_keywords = [
     'dances?', 'dancing', 'dancers?',
+    'funk', 'hip.?hop', 'punk',
+]
+dance_keywords = [
     'b.?boys?', 'b.?girls?', 'breaks', 'breaking', 'breakers?',
     'top.?rock(?:ers?|ing|)', 'up.?rock(?:ers?|ing|)',
     'housers?', 'house danc[^ ]*',
     'lockers?', 'locking?',
     'wh?aackers?', 'wh?aacking?',
     'poppers?', 'popping?', 'hitting',
-    'funk', 'g.?style',
     'animation', 'isolation', 'robots?',
     'strutters?', 'strutting',
     'glides?', 'gliding', 'waves?', 'waving', 'wavers?',
     'tuts?', 'tutting', 'tutters?',
-    'hip.?hop', 'new.?styles?', 'all.?styles?'
+    'new.?styles?', 'all.?styles?'
     'choreography', 'choreographers?', 'choreo', 'street jazz', 'street funk', 'jazz funk',
-    'new jack swing', 'hype danc[^ ]*', '90.?s hip.?hop', 'social hip.?hop', 'old schol hip.?hop',
-    'w[ha]acking', 'w[ha]ackers?', 'w[ha]ack', 'punking', 'punkers?', 'punk',
+    'new jack swing', 'hype danc[^ ]*', '90.?s hip.?hop', 'social hip.?hop', 'old school hip.?hop',
+    'w[ha]acking', 'w[ha]ackers?', 'w[ha]ack', 'punking', 'punkers?', 
     'vogue', 'voguers?', 'vogueing',
 ]
 
+easy_event_keywords = [
+    'jams?', 'club', 'shows?', 'performances?', 'contests?',
+]
+
 event_keywords = [
-    'competitions?', 'battles?', 'tournaments?', 'jams?', 'contests?', 'judges?',
-    'cyphers?', 'club',
-    '(\d)[ -]?vs?\.?[ -]?\1',
-    'sessions?', 'shows?', 'performances?',
+    'competitions?', 'battles?', 'tournaments?', 'judges?',
+    r'\d[ -]?vs?\.?[ -]?\d',
+    'cyphers?',
+    'sessions?',
     'workshops?', 'class with', 'master class(?:es)?', 'auditions?', 'try.?outs?', 'class(?:es)?',
     'abdc',
 ]
 
+easy_dance_regex = re.compile(r'\b(?:%s)\b' % '|'.join(easy_dance_keywords))
+easy_event_regex = re.compile(r'\b(?:%s)\b' % '|'.join(easy_event_keywords))
 dance_regex = re.compile(r'\b(?:%s)\b' % '|'.join(dance_keywords))
 event_regex = re.compile(r'\b(?:%s)\b' % '|'.join(event_keywords))
 
@@ -36,9 +45,16 @@ event_regex = re.compile(r'\b(?:%s)\b' % '|'.join(event_keywords))
 
 def is_dance_event(fb_event):
     search_text = (fb_event['info']['name'] + ' ' + fb_event['info'].get('description', '')).lower()
+    easy_dance_matches = set(easy_dance_regex.findall(search_text))
+    easy_event_matches = set(easy_event_regex.findall(search_text))
     dance_matches = set(dance_regex.findall(search_text))
     event_matches = set(event_regex.findall(search_text))
-    if len(dance_matches) >= 1 and len(event_matches) >= 1:
-        return dance_matches, event_matches
+    if (
+            # one critical dance keyword and at least some event-y kind of keyword
+            (len(dance_matches) >= 1 and len(event_matches) + len(easy_event_matches) >= 1) or
+            # one critical event and at least some dance-y kind of keyword
+            (len(event_matches) >= 1 and len(dance_matches) + len(easy_dance_matches) >= 1)
+        ):
+        return dance_matches.union(easy_dance_matches), event_matches.union(easy_event_matches)
     else:
         return False
