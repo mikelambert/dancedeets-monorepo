8cf3f4eb574c948897c7b7b3be6e93edbdb25e5e
Mike Lambert <mlambert@gmail.com>
2011-02-17 09:32:54 +0000

    make emails work without rsvps
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@612 67443d69-2279-40e3-8022-4dbfd14c1b02

--- logic/email_events.py
+++ logic/email_events.py
@@ -38,7 +38,11 @@ def email_for_user(user, batch_lookup, fb_graph, should_send=True):
     # Don't send email...
     if not search_results:
         return
-    rsvp.decorate_with_rsvps(batch_lookup, search_results)
+
+    fb_user = batch_lookup.data_for_user(batch_lookup.fb_uid)
+    if 'rsvp_for_future_events' in fb_user:
+        rsvp.decorate_with_rsvps(batch_lookup, search_results)
+
     past_results, present_results, grouped_results = search.group_results(search_results)
     # Don't include results more than a month out in these emails
     grouped_results = [x for x in grouped_results if x.id != 'year_events']
