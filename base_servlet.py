d3e08e0db466254d750e7c3449b74c7e2ee84ec7
Mike Lambert <mlambert@gmail.com>
2011-06-13 23:43:29 +0000

    fix case of generating forwarding link from url args being broken due to lists getting encoded as stringified-lists instead of raw k=v&k=v2
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@772 67443d69-2279-40e3-8022-4dbfd14c1b02

--- base_servlet.py
+++ base_servlet.py
@@ -98,7 +98,7 @@ class BaseRequestHandler(BareBaseRequestHandler):
         super(BaseRequestHandler, self).initialize(request, response)
         current_url_args = {}
         for arg in sorted(self.request.arguments()):
-            current_url_args[arg] = self.request.get_all(arg)
+            current_url_args[arg] = self.request.get_all(arg, doseq=True)
         final_url = self.request.path + '?' + urllib.urlencode(current_url_args)
         params = dict(next=final_url)
         login_url = '/login?%s' % urllib.urlencode(params)
