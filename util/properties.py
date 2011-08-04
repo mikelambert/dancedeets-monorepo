22d04c4d751a2677a052e21c787f9ff944649c91
Mike Lambert <mlambert@gmail.com>
2011-08-04 08:13:26 +0000

    fix bad property reference
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@886 67443d69-2279-40e3-8022-4dbfd14c1b02

--- util/properties.py
+++ util/properties.py
@@ -2,7 +2,7 @@ from django.utils import simplejson
 
 def json_property(field):
     def getter(obj):
-        assert getattr(obj, field.name), "Object %s did not have a value for %s" % (self.key().name(), field.name)
+        assert getattr(obj, field.name), "Object %s did not have a value for %s" % (obj.key().name(), field.name)
         json = getattr(obj, field.name)
         result = simplejson.loads(json)
         return result
@@ -10,7 +10,7 @@ def json_property(field):
     def setter(obj, value):
         json = simplejson.dumps(value)
         setattr(obj, field.name, json)
-        assert getattr(obj, field.name), "Object %s did not have a value for %s" % (self.key().name(), field.name)
+        assert getattr(obj, field.name), "Object %s did not have a value for %s" % (obj.key().name(), field.name)
 
     return property(getter, setter)
 
