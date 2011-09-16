8f8595584cd36fcd32b9e970dba21ca465394ba5
Mike Lambert <mlambert@gmail.com>
2011-09-16 19:05:11 +0000

    simplified mirror-ing system
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@896 67443d69-2279-40e3-8022-4dbfd14c1b02

--- mirror_events.sh
+++ mirror_events.sh
@@ -1,31 +1,13 @@
 rm bulkloader*
-rm local_data/events.db-new
-rm local_data/potentialevents.db-new
-rm local_data/fb_data.db-new
-rm local_data/city.db-new
-rm local_data/geocode.db-new
-rm local_data/locationmapping.db-new
-rm local_data/source.db-new
-appcfg.py download_data --application=dancedeets --kind="DBEvent" --url=http://dancedeets.appspot.com/_ah/remote_api --filename=local_data/events.db-new
-mv local_data/events.db{-new,}
-appcfg.py download_data --application=dancedeets --kind="PotentialEvent" --url=http://dancedeets.appspot.com/_ah/remote_api --filename=local_data/potentialevents.db-new
-mv local_data/potentialevents.db{-new,}
-appcfg.py download_data --application=dancedeets --kind="FacebookCachedObject" --url=http://dancedeets.appspot.com/_ah/remote_api --filename=local_data/fb_data.db-new
-mv local_data/fb_data.db{-new,}
-#appcfg.py download_data --application=dancedeets --kind="City" --url=http://dancedeets.appspot.com/_ah/remote_api --filename=local_data/city.db-new
-#mv local_data/city.db{-new,}
-appcfg.py download_data --application=dancedeets --kind="Geocode" --url=http://dancedeets.appspot.com/_ah/remote_api --filename=local_data/geocode.db-new
-mv local_data/geocode.db{-new,}
-appcfg.py download_data --application=dancedeets --kind="LocationMapping" --url=http://dancedeets.appspot.com/_ah/remote_api --filename=local_data/locationmapping.db-new
-mv local_data/locationmapping.db{-new,}
-appcfg.py download_data --application=dancedeets --kind="Source" --url=http://dancedeets.appspot.com/_ah/remote_api --filename=local_data/source.db-new
-mv local_data/source.db{-new,}
+db_names="DBEvent PotentialEvent Geocode LocationMapping Source FacebookCachedObject City"
+for name in $db_names ; do
+  echo "downloading $name"
+  rm local_data/$name-new
+  appcfg.py download_data --application=dancedeets --kind="$name" --url=http://dancedeets.appspot.com/_ah/remote_api --filename=local_data/$name.db-new
+  mv local_data/$name.db{-new,}
+done
+rm bulkloader*
+for name in $db_names ; do
+  #appcfg.py upload_data --application=dancedeets --kind="$name" --url=http://127.0.0.1:8080/_ah/remote_api --filename=local_data/$name.db
+done
 
-#appcfg.py upload_data --application=dancedeets --kind="DBEvent" --url=http://127.0.0.1:8080/_ah/remote_api --filename=events.db
-#appcfg.py upload_data --application=dancedeets --kind="PotentialEvent" --url=http://127.0.0.1:8080/_ah/remote_api --filename=potentialevents.db
-#appcfg.py upload_data --application=dancedeets --kind="FacebookCachedObject" --url=http://127.0.0.1:8080/_ah/remote_api --filename=fb_data.db
-#appcfg.py upload_data --application=dancedeets --kind="City" --url=http://127.0.0.1:8080/_ah/remote_api --filename=city.db
-#appcfg.py upload_data --application=dancedeets --kind="GeoCode" --url=http://127.0.0.1:8080/_ah/remote_api --filename=city.db
-#appcfg.py upload_data --application=dancedeets --kind="LocationMapping" --url=http://127.0.0.1:8080/_ah/remote_api --filename=city.db
-#appcfg.py upload_data --application=dancedeets --kind="Source" --url=http://127.0.0.1:8080/_ah/remote_api --filename=city.db
-rm bulkloader-*
