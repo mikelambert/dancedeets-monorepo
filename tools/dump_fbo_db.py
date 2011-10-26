44310d0288a475bc19706476f3535c79f3098c37
Mike Lambert <mlambert@gmail.com>
2011-10-26 00:22:32 +0000

    write out a bunch of downloaded-data to the test_data directory for our fb_api_stuby backend
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@1049 67443d69-2279-40e3-8022-4dbfd14c1b02

---
+++ tools/dump_fbo_db.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import sqlite3
+import logging
+#import pprint
+
+import sys
+sys.path += ['/Users/lambert/Projects/googleappengine-read-only/python']
+sys.path += ['/Users/lambert/Projects/googleappengine-read-only/python/lib/yaml/lib']
+sys.path += ['/Users/lambert/Projects/googleappengine-read-only/python/lib/webob']
+sys.path += ['/Users/lambert/Projects/googleappengine-read-only/python/lib/fancy_urllib']
+sys.path += ['/Users/lambert/Projects/googleappengine-read-only/python/lib/django']
+sys.path += ['.']
+
+from google.appengine.datastore import entity_pb
+from google.appengine.api import datastore
+
+conn = sqlite3.connect('local_data/FacebookCachedObject.db', isolation_level=None)
+cursor = conn.cursor()
+cursor.execute('select id, value from result')
+for entity_id, entity in cursor:
+    entity_proto = entity_pb.EntityProto(contents=entity)
+    e = datastore.Entity._FromPb(entity_proto)
+    if str(entity_id).endswith('OBJ_EVENT'):
+        real_id = str(entity_id).split(':')[-1]
+        if 'json_data' in e:
+            f = open('test_data/FacebookCachedObject/%s' % real_id, 'w')
+            f.write(e['json_data'])
+            f.close()
