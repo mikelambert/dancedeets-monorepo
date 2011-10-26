1e1fc6c83b7e51e4969b8a63ccdb2163ec0a03e5
Mike Lambert <mlambert@gmail.com>
2011-10-26 00:21:51 +0000

    basic stub for fb_api, to load off disk. will need to carefully choose which on-disk records to submit to the repository as test_data
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@1047 67443d69-2279-40e3-8022-4dbfd14c1b02

---
+++ fb_api_stub.py
@@ -0,0 +1,36 @@
+import json
+import logging
+
+from google.appengine.datastore import entity_pb
+from google.appengine.api import datastore
+
+import fb_api
+
+class FileBackedBatchLookup(fb_api.BatchLookup):
+    def _fetch_object_keys(self, keys):
+        fetched_objects = {}
+        for key in keys:
+            fetched_objects[key] = json.loads(open('test_data/FacebookCachedObject/%s' % self._string_key(key)).read())
+        return fetched_objects
+
+    def lookup_venue(self, venue_id):
+        pass
+
+    def data_for_venue(self, venue_id):
+        return {'deleted': True}
+
+
+class Stub(object):
+    def activate(self):
+        self.original_batch_lookup = fb_api.BatchLookup
+        fb_api.BatchLookup = FileBackedBatchLookup
+        import locations
+        dummy_location = {
+            'latlng': (0, 0),
+            'address': '',
+            'city': '',
+        }
+        locations.get_geocoded_location = lambda x: dummy_location
+
+    def deactivate(self):
+        fb_api.BatchLookup = self.original_batch_lookup
