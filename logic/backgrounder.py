1c0ebb8f0b02abce7c0161c1eb22a3fc6c2e9a63
Mike Lambert <mlambert@gmail.com>
2011-12-22 17:34:02 +0000

    do one-by-one friend-loads-of-events since some friends are huge and blow memory limits and we want to isolate them, at least until we figure out a better way to solve the problem
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@1115 67443d69-2279-40e3-8022-4dbfd14c1b02

--- logic/backgrounder.py
+++ logic/backgrounder.py
@@ -37,7 +37,7 @@ def load_potential_events_for_users(fb_uids, allow_cache=True, **kwargs):
 
 
 def load_potential_events_for_friends(user_id, fb_uids, allow_cache=True, **kwargs):
-    task_size = 10
+    task_size = 1 # 10 seemed to break things with too-large memory sizes
     allow_cache_arg = (allow_cache and '1' or '0')
     for i in range(0, len(fb_uids), task_size):
         taskqueue.add(method='GET', url='/tasks/load_potential_events_for_user?' + urllib.urlencode(dict(user_id=user_id, user_ids=','.join(str(x) for x in fb_uids[i:i+task_size]), allow_cache=allow_cache_arg)), queue_name='slow-queue', **kwargs)
