573677039a43f489323da27c94b7973eaa7a60f8
Mike Lambert <mlambert@gmail.com>
2012-01-03 17:31:16 +0000

    support google-searches for the given location. also do better links to the graph pages (should add tokens?) and owners for events and sources
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@1143 67443d69-2279-40e3-8022-4dbfd14c1b02

--- base_servlet.py
+++ base_servlet.py
@@ -38,6 +38,8 @@ class BareBaseRequestHandler(RequestHandler):
         # functions, add these to some base display setup
         self.display['format_html'] = text.format_html
         self.display['format_js'] = text.format_js
+        self.display['urlencode'] = lambda x: urllib.quote_plus(x.encode('utf8'))
+
         self.display['date_format'] = text.date_format
         self.display['format'] = text.format
         self.display['next'] = ''
