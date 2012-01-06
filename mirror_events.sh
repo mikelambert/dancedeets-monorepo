717cd8a1052827ac8d299f354cdaafee8e0ed2bf
Mike Lambert <mlambert@gmail.com>
2012-01-06 08:57:08 +0000

    fix up mirroring code
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@1162 67443d69-2279-40e3-8022-4dbfd14c1b02

--- mirror_events.sh
+++ mirror_events.sh
@@ -3,12 +3,12 @@ db_names="DBEvent PotentialEvent Source LocationMapping FacebookCachedObject" # 
 for name in $db_names ; do
   echo "downloading $name"
   rm local_data/$name-new
-  appcfg.py download_data --application=dancedeets --kind="$name" --url=http://dancedeets.appspot.com/_ah/remote_api --filename=local_data/$name.db-new
+  appcfg.py download_data --application=s~dancedeets-hrd --kind="$name" --url=http://dancedeets-hrd.appspot.com/_ah/remote_api --filename=local_data/$name.db-new
   mv local_data/$name.db{-new,}
 done
 rm bulkloader*
 for name in $db_names ; do
   echo "uploading $name"
-  #appcfg.py upload_data --application=dancedeets --kind="$name" --url=http://127.0.0.1:8080/_ah/remote_api --filename=local_data/$name.db
+  #appcfg.py upload_data --application=dancedeets-hrd --kind="$name" --url=http://127.0.0.1:8080/_ah/remote_api --filename=local_data/$name.db
 done
 
