1258a0d74d3f2d1b804d3a968c14735a2356b50f
Mike Lambert <mlambert@gmail.com>
2012-01-10 20:38:50 +0000

    fix up cities, countries, and continents listed on the logged-out homepage
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@1217 67443d69-2279-40e3-8022-4dbfd14c1b02

--- servlets/login.py
+++ servlets/login.py
@@ -44,15 +44,11 @@ class LoginHandler(base_servlet.BaseRequestHandler):
 
         self.display['user_message'] = self.get_cookie('User-Message')
 
-        city_rankings = rankings.get_thing_ranking(rankings.get_city_by_event_rankings(), rankings.ALL_TIME)
-        top_na_rankings = [x for x in city_rankings if 'United States' in x['key'] or 'Canada' in x['key'] or 'Mexico' in x['key']][:10]
+        city_rankings = rankings.get_thing_ranking(rankings.get_city_by_event_rankings(), rankings.LAST_MONTH)
+        top_na_rankings = [x for x in city_rankings if 'United States' in x['key'] or 'Canada' in x['key'] or 'Mexico' in x['key']][:20]
         self.display['top_cities'] = [(x['key'], x['key'].split(', ')[0]) for x in top_na_rankings]
-        self.display['top_continents'] = [
-            ('paris', 'Europe'),
-            ('beijing', 'Asia'),
-            ('sydney', 'Australia'),
-            ('sao paolo', 'South America'),
-        ]
+        self.display['top_european_countries'] = ['Czech Republic', 'Finland', 'France', 'Germany', 'Ireland', 'Italy', 'Norway', 'Poland', 'Spain', 'Sweden', 'Switzerland', 'United Kingdom', 'Portugal']
+        self.display['top_continents'] = ['Asia', 'Africa', 'Australia', 'South America']
 
                 self.display['defaults'] = {
                         'distance': '50',
