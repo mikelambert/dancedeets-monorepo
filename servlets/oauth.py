c974f41a0fa2ff858b0bc81c119753fd751f0f98
Mike Lambert <mlambert@gmail.com>
2012-01-19 14:41:05 +0000

    set up the oauth support so we can grab tokens from the db if necessary
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@1295 67443d69-2279-40e3-8022-4dbfd14c1b02

---
+++ servlets/oauth.py
@@ -0,0 +1,40 @@
+import httplib2
+import os.path
+
+from oauth2client.appengine import oauth2decorator_from_clientsecrets
+from oauth2client.client import AccessTokenRefreshError
+from google.appengine.api import memcache
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+CLIENT_SECRETS = os.path.join(os.path.dirname(__file__), '../client_secrets.json')
+
+http = httplib2.Http(memcache)
+decorator = oauth2decorator_from_clientsecrets(
+    CLIENT_SECRETS,
+    'https://www.googleapis.com/auth/prediction',
+    'Fill out %s' % CLIENT_SECRETS)
+
+class OAuthSetupHandler(webapp.RequestHandler):
+  @decorator.oauth_required
+  def get(self):
+    user = users.get_current_user()
+    try:
+      http = decorator.http()
+      self.response.out.write('oauth set up for user id %r!' % user.user_id())
+    except AccessTokenRefreshError, e:
+      self.response.out.write('oauth error! %s' % e)
+
+
+def main():
+  application = webapp.WSGIApplication(
+      [
+       ('/oauth_setup', OAuthSetupHandler),
+      ],
+      debug=True)
+  run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+  main()
