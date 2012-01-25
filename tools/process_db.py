ff89da18deb69c8d2e56b1cbb863706d6ce75128
Mike Lambert <mlambert@gmail.com>
2012-01-25 15:01:18 +0000

    better error handling
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@1315 67443d69-2279-40e3-8022-4dbfd14c1b02

--- tools/process_db.py
+++ tools/process_db.py
@@ -7,17 +7,19 @@ sys.path += ['/Users/lambert/Projects/googleappengine-read-only/python/lib/fancy
 sys.path += ['/Users/lambert/Projects/googleappengine-read-only/python/lib/django']
 sys.path += ['.']
 
+import csv
 import datetime
+import os
+import sqlite3
 import sys
 import time
-import csv
-import sqlite3
 from google.appengine.datastore import entity_pb
 from google.appengine.api import datastore
 
 def flatten_output(db_name, out_name, process_func):
     ct = 0
-    csvwriter = csv.writer(open(out_name, 'w'))
+    print 'Processing %s' % db_name
+    csvwriter = csv.writer(open(out_name+".new", 'w'))
     conn = sqlite3.connect(db_name, isolation_level=None)
     cursor = conn.cursor()
     cursor.execute('select id, value from result')
@@ -27,17 +29,20 @@ def flatten_output(db_name, out_name, process_func):
         try:
             result = process_func(f)
         except Exception, e:
-            print "Error processing row %s: %s: %s" % (f.key().name(), e, f)
-        csvwriter.writerow(result)
+            print "Error processing row %s: %r: %s" % (f.key().name(), e, f)
+        if result is not None:
+            csvwriter.writerow(result)
 
         ct += 1
         if ct % 20000 == 0:
             print "Ct is %s" % ct
             sys.stdout.flush()
+    os.rename(out_name, out_name+".bak")
+    os.rename(out_name+".new", out_name)
     print "done"
 
 def event_list(x):
-    start_time = x.get('start_time') or datetime.datetime.min
+    start_time = x.get('start_time') or datetime.datetime(1970, 1, 1)
     return [
         x.key().name(),
         str(x['owner_fb_uid']),
@@ -45,9 +50,15 @@ def event_list(x):
         start_time.strftime('%Y%m%d')
     ]
 
+def json_data(x):
+    if 'json_data' in x:
+        return [x.key().name(), x['json_data']]
+    else:
+        return None
+
 flatten_output("local_data/DBEvent.db", "local_data/DBEvent.csv", event_list)
-flatten_output("local_data/PotentialEvent.db", "local_data/PotentialEvent.csv", lambda x: [x.key().name()])
-flatten_output("local_data/FacebookCachedObject.db", "local_data/FacebookCachedObject.csv", lambda x: [x.key().name(), x['json_data']])
+#flatten_output("local_data/PotentialEvent.db", "local_data/PotentialEvent.csv", lambda x: [x.key().name()])
+flatten_output("local_data/FacebookCachedObject.db", "local_data/FacebookCachedObject.csv", json_data)
 
 # count characters
 count_characters = False
