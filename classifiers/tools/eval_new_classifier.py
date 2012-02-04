579d1f4cb477a930d86abd6f604f5f3fc055c23c
Mike Lambert <mlambert@gmail.com>
2012-02-04 13:02:38 +0000

    some tweaks to the tool
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@1330 67443d69-2279-40e3-8022-4dbfd14c1b02

--- classifiers/tools/eval_new_classifier.py
+++ classifiers/tools/eval_new_classifier.py
@@ -31,7 +31,7 @@ def all_fb_data():
 
 
 START_EVENT = 0
-END_EVENT = 1000000
+END_EVENT = 0
 def partition_ids(classifier=event_classifier.ClassifiedEvent):
     success = set()
     fail = set()
@@ -39,16 +39,17 @@ def partition_ids(classifier=event_classifier.ClassifiedEvent):
         if not i % 10000: print 'Processing ', i
         if i < START_EVENT:
             continue
-        if i > END_EVENT:
+        if END_EVENT and i > END_EVENT:
             break
         result = classifier(fb_event)
         result.classify()
         if result.is_dance_event():
             success.add(id)
         else:
-            if id in good_ids:
-                print id, fb_event['info'].get('name')
-                print result.found_dance_matches, result.found_event_matches, result.found_wrong_matches
+            # To print out failures, to see if there's any way we can better detect them
+            #if id in good_ids:
+            #    print id, fb_event['info'].get('name')
+            #    print result.found_dance_matches, result.found_event_matches, result.found_wrong_matches
             fail.add(id)
     return fail, success
 
@@ -65,9 +66,7 @@ true_negative = fail.intersection(potential_and_bad_ids)
 print 'false negatives', len(false_negative)
 print 'true negatives', len(true_negative)
 
-sys.exit(0)
-
-print '--- using new filter ---'
+print '--- using old filter ---'
 fail2, succeed2 = partition_ids(classifier=event_classifier2.ClassifiedEvent)
 false_negative2 = fail2.difference(potential_and_bad_ids)
 true_negative2 = fail2.intersection(potential_and_bad_ids)
@@ -75,7 +74,9 @@ print 'false negatives', len(false_negative2)
 print 'true negatives', len(true_negative2)
 
 print 'list of used-to-be-positive now-negative dance events'
-for id in false_negative2.difference(false_negative):
+for id in false_negative.difference(false_negative2):
+    print id
+    continue
     fb_event = get_fb_event(id)
     print 'F', id, fb_event['info'].get('owner', {}).get('name'), fb_event['info']['name']
     old = event_classifier.ClassifiedEvent(fb_event).is_dance_event()
@@ -86,7 +87,9 @@ print ''
 print ''
 
 print 'list of used-to-be-negative now-positive non-dance events'
-for id in true_negative.difference(true_negative2):
+for id in true_negative2.difference(true_negative):
+    print id
+    continue
     fb_event = get_fb_event(id)
     print 'F', id, fb_event['info'].get('owner', {}).get('name'), fb_event['info']['name']
     old = event_classifier.ClassifiedEvent(fb_event).is_dance_event()
