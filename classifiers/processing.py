41c5f0ff51d10601112f0be9c473905df4a98157
Mike Lambert <mlambert@gmail.com>
2012-02-28 13:02:24 +0000

    use subsetted file and cjson for faster processing. cjson was minimal
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@1364 67443d69-2279-40e3-8022-4dbfd14c1b02

--- classifiers/processing.py
+++ classifiers/processing.py
@@ -1,5 +1,8 @@
 import csv
-import json
+import cjson
+
+# grep '^701004.[0-9]*.OBJ_EVENT,' local_data/FacebookCachedObject.csv > local_data/FacebookCachedObjectEvent.csv
+
 
 def load_ids():
     result = {}
@@ -29,10 +32,10 @@ def load_ids():
 
 def all_fb_data(combined_ids):
     csv.field_size_limit(1000000000)
-    for row in csv.reader(open('local_data/FacebookCachedObject.csv')):
+    for row in csv.reader(open('local_data/FacebookCachedObjectEvent.csv')):
         source_id, row_id, row_type = row[0].split('.')
         if source_id == "701004" and row_type == 'OBJ_EVENT' and row_id in combined_ids:
-            fb_event = json.loads(row[1])
+            fb_event = cjson.decode(row[1])
             if fb_event and not fb_event['deleted'] and fb_event['info']['privacy'] == 'OPEN':
                 yield row_id, fb_event
 
