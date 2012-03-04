5ac8b48b44406808455f2c557b0391a4461713ad
Mike Lambert <mlambert@gmail.com>
2012-03-04 16:38:35 +0000

    rewrote bits to allow for better debugging display realtime during classification
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@1374 67443d69-2279-40e3-8022-4dbfd14c1b02

--- classifiers/tools/eval_new_classifier.py
+++ classifiers/tools/eval_new_classifier.py
@@ -4,7 +4,6 @@ import sys
 sys.path += ['.']
 import time
 from logic import event_classifier
-from logic import event_classifier2
 from classifiers import processing
 
 ids_info = processing.load_ids()
@@ -14,9 +13,15 @@ good_ids = ids_info['good_ids']
 bad_ids = ids_info['bad_ids']
 combined_ids = ids_info['combined_ids']
 
+def handle_new_false_negative(id, ec):
+    print "fn", [(k, v) for (k, v) in ec.__dict__.iteritems() if k not in ['search_text', 'times']]
+def handle_new_false_positive(id, ec):
+    print "fp", [(k, v) for (k, v) in ec.__dict__.iteritems() if k not in ['search_text', 'times']]
+
+
 START_EVENT = 0
 END_EVENT = 0
-def partition_ids(classifier=event_classifier.ClassifiedEvent):
+def partition_ids(old_true_positive, old_true_negative, classifier=event_classifier.ClassifiedEvent):
     success = set()
     fail = set()
     a = time.time()
@@ -30,12 +35,16 @@ def partition_ids(classifier=event_classifier.ClassifiedEvent):
         result.classify()
         if result.is_dance_event():
             success.add(id)
+            if id in old_true_negative:
+                handle_new_false_positive(result)
         else:
             # To print out failures, to see if there's any way we can better detect them
             #if id in good_ids:
             #    print id, fb_event['info'].get('name')
             #    print result.found_dance_matches, result.found_event_matches, result.found_wrong_matches
             fail.add(id)
+            if id in old_true_positive:
+                handle_new_false_negative(result)
     print 'Time per event: %s' % (1.0 * (time.time() - a) / (max(END_EVENT, i) - START_EVENT))
     return fail, success
 
@@ -48,8 +57,20 @@ opts = dict(opt_data[0])
 if not opts.get('-i'):
     opts['-i'] = 'old_eval'
 
+
+print '--- using old filter ---'
+if opts.get('-i'):
+    old_fail = frozenset(x.strip() for x in open(opts['-i']+".fail").readlines())
+    old_succeed = frozenset(x.strip() for x in open(opts['-i']+".succeed").readlines())
+old_true_positive = old_succeed.intersection(good_ids)
+old_false_positive = old_succeed.intersection(bad_ids)
+old_false_negative = old_fail.intersection(good_ids)
+old_true_negative = old_fail.intersection(bad_ids)
+print 'false negatives', len(old_false_negative)
+print 'true negatives', len(old_true_negative)
+
 print '---'
-fail, succeed = partition_ids()
+fail, succeed = partition_ids(old_true_positive, old_true_negative)
 if opts.get('-o'):
     open(opts['-o']+".fail", 'w').writelines([x+"\n" for x in fail])
     open(opts['-o']+".succeed", 'w').writelines([x+"\n" for x in succeed])
@@ -60,44 +81,22 @@ true_negative = fail.intersection(bad_ids)
 print 'false negatives', len(false_negative)
 print 'true negatives', len(true_negative)
 
-print '--- using old filter ---'
-if opts.get('-i'):
-    fail2 = set(x.strip() for x in open(opts['-i']+".fail").readlines())
-    succeed2 = set(x.strip() for x in open(opts['-i']+".succeed").readlines())
-#fail2, succeed2 = partition_ids(classifier=event_classifier2.ClassifiedEvent)
-true_positive2 = succeed2.intersection(good_ids)
-false_positive2 = succeed2.intersection(bad_ids)
-false_negative2 = fail2.intersection(good_ids)
-true_negative2 = fail2.intersection(bad_ids)
-print 'false negatives', len(false_negative2)
-print 'true negatives', len(true_negative2)
 
 print '-----'
-print "Events we helped find:", len(true_positive.difference(true_positive2))
-print "Events we will miss:", len(false_negative.difference(false_negative2))
-print "Events we will waste time on:", len(false_positive.difference(false_positive2))
-print "Events we saved time on:", len(true_negative.difference(true_negative2))
+print "Events we helped find:", len(true_positive.difference(old_true_positive))
+print "Events we will miss:", len(false_negative.difference(old_false_negative))
+print "Events we will waste time on:", len(false_positive.difference(old_false_positive))
+print "Events we saved time on:", len(true_negative.difference(old_true_negative))
 
 print 'list of used-to-be-positive now-negative dance events (things we will miss)'
-for id in false_negative.difference(false_negative2):
+for id in false_negative.difference(old_false_negative):
     print id
     continue
-    fb_event = get_fb_event(id)
-    print 'F', id, fb_event['info'].get('owner', {}).get('name'), fb_event['info']['name']
-    old = event_classifier.ClassifiedEvent(fb_event).is_dance_event()
-    new = event_classifier2.ClassifiedEvent(fb_event).is_dance_event()
-    print '  ', old, new
-
+    
 print ''
 print ''
 
 print 'list of used-to-be-negative now-positive non-dance events (extra useless work)'
-for id in false_positive.difference(false_positive2):
+for id in false_positive.difference(old_false_positive):
     print id
-    continue
-    fb_event = get_fb_event(id)
-    print 'F', id, fb_event['info'].get('owner', {}).get('name'), fb_event['info']['name']
-    old = event_classifier.ClassifiedEvent(fb_event).is_dance_event()
-    new = event_classifier2.ClassifiedEvent(fb_event).is_dance_event()
-    print '  ', old, new
 
