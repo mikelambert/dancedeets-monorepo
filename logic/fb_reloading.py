1119a80d72c01a05d48238d0fd966e72e9b5c5aa
Mike Lambert <mlambert@gmail.com>
2012-05-15 01:26:00 +0000

    extract out re-usable filtered-input-reader
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@1409 67443d69-2279-40e3-8022-4dbfd14c1b02

--- logic/fb_reloading.py
+++ logic/fb_reloading.py
@@ -2,15 +2,13 @@ import logging
 
 from google.appengine.datastore import datastore_query
 
-from mapreduce import util
-from mapreduce import input_readers
-
 from events import eventdata
 import fb_api
 from logic import email_events
 from logic import potential_events
 from util import fb_mapreduce
 from util import timings
+from util import mr_helper
 
 BROKEN_UIDS = []#"1400235949", "1268158596"]
 
@@ -92,34 +90,11 @@ def yield_load_fb_user(batch_lookup, user):
 map_load_fb_user = fb_mapreduce.mr_user_wrap(yield_load_fb_user)
 load_fb_user = fb_mapreduce.nomr_wrap(yield_load_fb_user)
 
-class FilteredInputReader(input_readers.DatastoreInputReader):
-
-    def _iter_key_range(self, k_range):
-        cursor = None
-        while True:
-            query = k_range.make_ascending_query(
-                util.for_name(self._entity_kind))
-            self.filter_query(query)
-            if cursor:
-                query.with_cursor(cursor)
-
-            results = query.fetch(limit=self._batch_size)
-            if not results:
-                break
-
-            for model_instance in results:
-                key = model_instance.key()
-                yield key, model_instance
-            cursor = query.cursor()
-
-    def filter_query(self, query):
-        raise NotImplementedError("filter_query() not implemented in %s" % self.__class__)
-
-class FutureEventInputReader(FilteredInputReader):
+class FutureEventInputReader(mr_helper.FilteredInputReader):
     def filter_query(self, query):
         query.filter('search_time_period =', eventdata.TIME_FUTURE)
 
-class PastEventInputReader(FilteredInputReader):
+class PastEventInputReader(mr_helper.FilteredInputReader):
     def filter_query(self, query):
         query.filter('search_time_period =', eventdata.TIME_PAST)
 
