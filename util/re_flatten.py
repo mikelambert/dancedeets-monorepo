f92a23f588d9f2924dd0768a353e3342a425feb9
Mike Lambert <mlambert@gmail.com>
2012-05-23 09:23:54 +0000

    fix bug in quantifier-logic and nested-parens logic for our re-flattener, along with basic test change
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@1442 67443d69-2279-40e3-8022-4dbfd14c1b02

--- util/re_flatten.py
+++ util/re_flatten.py
@@ -20,7 +20,7 @@ def _sub_alternation(tree):
     else:
         return u'(?:%s)' % '|'.join(regexes)
 
-QUANTIFIER = '(?:[+*?]|{\d+(?:,\d+)?})?\??'
+QUANTIFIER = '(?:[+*?]|{(?:\d+(?:,\d+)|,\d+)})?\??'
 def tokenize_regex(original_r):
     r = original_r
     tokenized = []
@@ -41,7 +41,7 @@ def tokenize_regex(original_r):
             r = r[match.end():]
             continue
         if r.startswith('('):
-            return original_r
+            return tuple([original_r])
         raise Exception("Cannot parse regex: %r" % r)
     return tuple(tokenized)
 
@@ -49,6 +49,7 @@ if __name__ == '__main__':
     assert ('b', 'o', 'n', 'n', 'i', 'e', '\\s*', '(?:and|&)', '\\s*', 'c', 'l', 'y', 'd', 'e') == tokenize_regex(r"bonnie\s*(?:and|&)\s*clyde")
     assert ('p', 'o', 'p', '\\W{0,3}', 'l', 'o', 'c', 'k', '(?:ing?|er[sz]?)?') == tokenize_regex(r"pop\W{0,3}lock(?:ing?|er[sz]?)?")
     assert ('0', '[ -]?', 'n', 'a', '[ -]?', '0') == tokenize_regex("0[ -]?na[ -]?0"),tokenize_regex("0[ -]?na[ -]?0")
+    assert ('((a))',) == tokenize_regex("((a))")
     import logic.event_classifier
     manual_dance_keywords = logic.event_classifier.get_manual_dance_keywords()
     t = pytrie.Trie(dict((tokenize_regex(x), True) for x in manual_dance_keywords))
