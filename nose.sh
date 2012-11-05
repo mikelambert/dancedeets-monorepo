005a9529a57228b3f6c8616cdf72d0546f9e0a55
Mike Lambert <mlambert@gmail.com>
2012-11-05 14:42:22 +0000

    first pass of support on symlinked-to-other-projects to get things working on new machine
    git-svn-id: file:///Users/lambert/Projects/svn/dancesite@1530 67443d69-2279-40e3-8022-4dbfd14c1b02

--- nose.sh
+++ nose.sh
@@ -1,9 +1,10 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 nosetests --with-gae --exclude="mapreduce|gdata|atom|httplib2" $1 $2 $3 $4 $5
 
 # needed to change nose/case.py's MethodTestCase.__init__ as follows.
 # Otherwise the attempt to create cls() without a parameter fails due to runTest missing
+# ValueError: no such test method in <class 'XXX'>: runTest
 #        if self.test is None:
 #            method_name = self.method.__name__
 #            self.inst = self.cls(method_name)
