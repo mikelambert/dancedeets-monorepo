62ea53c01e552097b26dae0280c22f4ece419eb9
Mike Lambert <mlambert@gmail.com>
2014-02-01 21:34:22 +0800

    make json outputting stable/consistent, so we can compare it on old-vs-new

--- util/properties.py
+++ util/properties.py
@@ -8,7 +8,8 @@ def json_property(field):
         return result
 
     def setter(obj, value):
-        json_data = json.dumps(value)
+        # We sort so that strings are compare-able for changes
+        json_data = json.dumps(value, sort_keys=True)
         setattr(obj, field.name, json_data)
         assert getattr(obj, field.name), "Object %s did not have a value for %s" % (obj.key().name(), field.name)
 
