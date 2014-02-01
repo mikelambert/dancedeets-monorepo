4a7a43c8406ec1b4ccf5167dc0686f1ad674f2bc
Mike Lambert <mlambert@gmail.com>
2014-02-01 21:33:23 +0800

    dont have controllers poll so frequently

--- appengine_config.py
+++ appengine_config.py
@@ -3,3 +3,7 @@ def webapp_add_wsgi_middleware(app):
     app = recording.appstats_wsgi_middleware(app)
     return app
 
+# We don't need such real-time statistics (normally 1 second) on the mapreduce job.
+# More of an optimization to save on the associated database Get/Put every second.
+mapreduce__CONTROLLER_PERIOD_SEC = 5
+
