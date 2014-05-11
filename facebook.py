206372ab5a00552643528b31c45ae18876406a61
Mike Lambert <mlambert@gmail.com>
2014-05-11 03:11:15 -0400

    deleted old code, added missing error

--- facebook.py
+++ facebook.py
@@ -35,7 +35,6 @@ usage of this module might look like this:
 
 import cgi
 import hashlib
-import time
 import urllib
 
 # Find a JSON parser
@@ -201,6 +200,9 @@ def urlsafe_b64decode(str):
     return base64.urlsafe_b64decode(str.ljust(l+pl, "="))
 
 
+class SignedRequestError(Exception):
+    pass
+
 def parse_signed_request(signed_request, secret):
     """
     Parse signed_request given by Facebook (usually via POST),
@@ -319,29 +321,3 @@ def get_user_from_cookie(cookies, app_id, app_secret):
         access_token_expires = expires_time,
     )
 
-def old_get_user_from_cookie(cookies, app_id, app_secret):
-    """Parses the cookie set by the official Facebook JavaScript SDK.
-
-    cookies should be a dictionary-like object mapping cookie names to
-    cookie values.
-
-    If the user is logged in via Facebook, we return a dictionary with the
-    keys "uid" and "access_token". The former is the user's Facebook ID,
-    and the latter can be used to make authenticated requests to the Graph API.
-    If the user is not logged in, we return None.
-
-    Download the official Facebook JavaScript SDK at
-    http://github.com/facebook/connect-js/. Read more about Facebook
-    authentication at http://developers.facebook.com/docs/authentication/.
-    """
-    cookie = cookies.get("fbs_" + app_id, "")
-    if not cookie: return None
-    args = dict((k, v[-1]) for k, v in cgi.parse_qs(cookie.strip('"')).items())
-    payload = "".join(k + "=" + args[k] for k in sorted(args.keys())
-                      if k != "sig")
-    sig = hashlib.md5(payload + app_secret).hexdigest()
-    expires = int(args["expires"])
-    if sig == args.get("sig") and (expires == 0 or time.time() < expires):
-        return args
-    else:
-        return None
