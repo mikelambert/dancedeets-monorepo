a54b1fcea5397596a1b017790bea6c8368f463ed
Mike Lambert <mlambert@gmail.com>
2014-11-26 12:57:21 -0500

    Unindent, add coment.

--- logic/user_creation.py
+++ logic/user_creation.py
@@ -9,46 +9,47 @@ from events import users
 from logic import backgrounder
 
 def create_user_with_fbuser(fb_uid, fb_user, access_token, access_token_expires, city, send_email=False, referer=None, client=None):
-        user = users.User(key_name=str(fb_uid))
-        user.fb_access_token = access_token
-        user.fb_access_token_expires = access_token_expires
-        user.location = city
+    user = users.User(key_name=str(fb_uid))
+    user.fb_access_token = access_token
+    user.fb_access_token_expires = access_token_expires
+    user.location = city
 
-        # grab the cookie to figure out who referred this user
-        logging.info("Referer was: %s", referer)
-        if referer:
-            user.inviting_fb_uid = int(referer)
-        user.clients = [client]
+    # grab the cookie to figure out who referred this user
+    logging.info("Referer was: %s", referer)
+    if referer:
+        user.inviting_fb_uid = int(referer)
+    user.clients = [client]
 
-        user.send_email = send_email
-        user.distance = '50'
-        user.distance_units = 'miles'
-        user.min_attendees = 0
+    user.send_email = send_email
+    user.distance = '50'
+    user.distance_units = 'miles'
+    user.min_attendees = 0
 
-        user.creation_time = datetime.datetime.now()
+    user.creation_time = datetime.datetime.now()
 
-        user.login_count = 1
-        user.last_login_time = user.creation_time
+    user.login_count = 1
+    user.last_login_time = user.creation_time
 
-        user.compute_derived_properties(fb_user)
-        logging.info("Saving user with name %s", user.full_name)
-        user.put()
+    user.compute_derived_properties(fb_user)
+    logging.info("Saving user with name %s", user.full_name)
+    user.put()
 
-        logging.info("Requesting background load of user's friends")
-        # Must occur after User is put with fb_access_token
-        taskqueue.add(method='GET', url='/tasks/track_newuser_friends?' + urllib.urlencode({'user_id': fb_uid}), queue_name='slow-queue')
-        # Now load their potential events, to make "add event page" faster (and let us process/scrape their events)
-        #fb_reloading.load_potential_events_for_user_ids(fbl, [fb_uid])
-        backgrounder.load_potential_events_for_users([fb_uid])
+    logging.info("Requesting background load of user's friends")
+    # Must occur after User is put with fb_access_token
+    taskqueue.add(method='GET', url='/tasks/track_newuser_friends?' + urllib.urlencode({'user_id': fb_uid}), queue_name='slow-queue')
+    # Now load their potential events, to make "add event page" faster (and let us process/scrape their events)
+    #fb_reloading.load_potential_events_for_user_ids(fbl, [fb_uid])
+    backgrounder.load_potential_events_for_users([fb_uid])
 
 
 def create_user(access_token, access_token_expires, city, send_email=False, referer=None, client=None):
-        # Build a cache-less lookup
-        fbl = fb_api.FBLookup(None, access_token)
-        fb_user = fbl.get(fb_api.LookupUser, 'me')
-        fb_uid = fb_user['info']['id']
+    #TODO(lambebrt): move to servlets/api.py code, if this is repeated code
+    # Build a cache-less lookup
+    fbl = fb_api.FBLookup(None, access_token)
+    fb_user = fbl.get(fb_api.LookupUser, 'me')
+    fb_uid = fb_user['info']['id']
 
-        fbl = fb_api.FBLookup(fb_uid, access_token)
-        fb_user = fbl.get(fb_api.LookupUser, fb_uid)
-        return create_user(fb_uid, fb_user, access_token, access_token_expires, city, send_email=send_email, referer=referer, client=client)
+    fbl = fb_api.FBLookup(fb_uid, access_token)
+    fb_user = fbl.get(fb_api.LookupUser, fb_uid)
+    return create_user(fb_uid, fb_user, access_token, access_token_expires, city, send_email=send_email, referer=referer, client=client)
 
