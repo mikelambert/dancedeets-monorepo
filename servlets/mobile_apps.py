81abecfa6e52b6d2d10b6120be331d50d50b8c10
Mike Lambert <mlambert@gmail.com>
2015-01-14 21:26:47 -0500

    Add the bouncing penguin.

--- servlets/mobile_apps.py
+++ servlets/mobile_apps.py
@@ -35,6 +35,9 @@ class MobileAppsHandler(base_servlet.BaseRequestHandler):
             self.render_page()
 
     def render_page(self, message=None, error=None):
+        total_time = 10
+        self.display['total_time'] = total_time
+        self.display['walkout_animation'] = self.get_walkout_animation(total_time, -1000)
         self.display['android_url'] = ANDROID_URL
         self.display['ios_url'] = IOS_URL
         if message:
@@ -51,3 +54,38 @@ class MobileAppsHandler(base_servlet.BaseRequestHandler):
             phone = self.request.get('phone')
             sms.send_email_link(phone)
             self.render_page(message="Thank you, your SMS should be arriving shortly. Just open the link on your phone to download the DanceDeets app.")
+
+    def get_walkout_animation(self, total_time, total_distance):
+        jumps = int(total_time / 0.5)
+        if jumps * 0.5 != total_time:
+            raise ValueError("total_time(%s) must be multiple of 0.5" % total_time)
+        loop_pct = 100.0 / jumps
+        half_loop_pct = loop_pct / 2
+
+        jump_distance = total_distance / jumps
+
+        stages = []
+        for i in range(jumps):
+            this_loop_pct = i*loop_pct
+            this_half_loop_pct = this_loop_pct + half_loop_pct
+            this_distance = i*jump_distance
+            animation_stage = """
+            %(half_loop_pct)s%% {
+                left: %(distance)spx;
+            }
+            %(loop_pct)s%% {
+                left: %(distance)spx;
+                -webkit-animation-timing-function: linear;
+                animation-timing-function: linear;
+            }
+            """ % dict(half_loop_pct=this_half_loop_pct, loop_pct=this_loop_pct, distance=this_distance)
+            stages.append(animation_stage)
+
+        stages.append("""
+            100%% {
+                left: %(distance)spx;
+            }
+        """ % dict(distance=total_distance))
+        keyframes = ''.join(stages)
+        animations = "@-webkit-keyframes walkOut {%s}\n@keyframes walkOut {%s}" % (keyframes, keyframes)
+        return animations
