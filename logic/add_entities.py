00dbadd889e5ae94c55abc1669aa4ccb84f529a5
Mike Lambert <mlambert@gmail.com>
2015-01-18 08:08:50 -0500

    Make we grab the correct facebook token, to use when posting to the facebook wall (might have grabbed a twitter token before, doh)
    Better error messaging and logging for posting to twitter/fb. Catch twitter errors (such as duplicate status posts).

--- logic/add_entities.py
+++ logic/add_entities.py
@@ -1,5 +1,7 @@
 import logging
 
+import twitter
+
 import fb_api
 from events import eventdata
 from logic import event_classifier
@@ -42,20 +44,27 @@ def add_update_event(event_id, user_id, fbl, remapped_address=None, override_add
     if newly_created:
         logging.info("New event, publishing to twitter/facebook")
         # When we want to support complex queries on many types of events, perhaps we should use Prospective Search.
-        auth_tokens = pubsub.OAuthToken.query(pubsub.OAuthToken.user_id=="701004", pubsub.OAuthToken.token_nickname=="BigTwitter").fetch(1)
+        auth_tokens = pubsub.OAuthToken.query(pubsub.OAuthToken.user_id=="701004", pubsub.OAuthToken.application==pubsub.APP_TWITTER, pubsub.OAuthToken.token_nickname=="BigTwitter").fetch(1)
         if auth_tokens:
-            pubsub.twitter_post(auth_tokens[0], e, fb_event)
+            try:
+                pubsub.twitter_post(auth_tokens[0], e, fb_event)
+            except twitter.TwitterError as e:
+                logging.error("Twitter Post Error: %s", e)
         else:
             logging.error("Could not find Mike's BigTwitter OAuthToken")
-        auth_tokens = pubsub.OAuthToken.query(pubsub.OAuthToken.user_id=="701004").fetch(1)
+        auth_tokens = pubsub.OAuthToken.query(pubsub.OAuthToken.user_id=="701004", pubsub.OAuthToken.application==pubsub.APP_FACEBOOK).fetch(5)
         if auth_tokens:
-            auth_token = auth_tokens[0]
-            if auth_token.token_nickname in ["1613128148918160", "1375421172766829"]:
+            filtered_auth_tokens = [x for x in auth_tokens if x.token_nickname in ["1613128148918160", "1375421172766829"]]
+            if filtered_auth_tokens:
+                auth_token = filtered_auth_tokens[0]
                 result = pubsub.facebook_post(auth_token, e, fb_event)
+                logging.info("Facebook result was %s", result)
                 if 'error' in result:
-                    logging.error("Error posting to FB wall: %s" % result)
+                    logging.error("Facebook Post Error: %s", result)
+            else:
+                logging.error("Couldn't find good Facebook tokens to pubulish to: %s", auth_tokens)
         else:
-            logging.error("Could not find Mike's Facebook Page to post to")
+            logging.error("Could not find a Facebook token")
 
     potential_event = potential_events.make_potential_event_without_source(event_id, fb_event, fb_event_attending)
     classified_event = event_classifier.get_classified_event(fb_event, potential_event.language)
