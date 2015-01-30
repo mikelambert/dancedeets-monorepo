1527758901782d02ae640904df1095dd90b06f49
Mike Lambert <mlambert@gmail.com>
2015-01-29 21:22:48 -0500

    Move all our libraries into /lib, and adjust our two most common endpoints needing these libraries.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), 'lib'))
 
 import logging
 logging.info("Begin modules")
--- setup.sh
+++ setup.sh
@@ -3,20 +3,31 @@
 set -e
 
 BASE_DIR=$(pwd)
-mkdir -p download/
 
-cd $BASE_DIR/download
-curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py
-#python get-pip.py
-sudo python get-pip.py
+TMP_DIR=/tmp/dancedeets-download/
+mkdir -p $TMP_DIR
+
+cd $TMP_DIR
+curl https://bootstrap.pypa.io/get-pip.py --output $TMP_DIR/get-pip.py
+sudo python $TMP_DIR/get-pip.py
 
 # For testing, just install them locally. Depends on pip being installed.
 sudo pip install nosegae
 sudo pip install nose
 sudo pip install webtest
 
+pip install --upgrade -t $BASE_DIR/lib GoogleAppEngineCloudStorageClient
+pip install --upgrade -t $BASE_DIR/lib iso3166
+pip install --upgrade -t $BASE_DIR/lib twitter
+pip install --upgrade -t $BASE_DIR/lib python-oauth2
+pip install --upgrade -t $BASE_DIR/lib twilio
+pip install --upgrade -t $BASE_DIR/lib GoogleAppEngineMapReduce
+pip install --upgrade -t $BASE_DIR/lib google-api-python-client
+pip install --upgrade -t $BASE_DIR/lib gdata
+pip install --upgrade -t $BASE_DIR/lib python-gflags
+
 function install-git() {
-	cd $BASE_DIR/download
+	cd $TMP_DIR
 	REPO_PATH=$1
 	shift
 	echo
@@ -31,12 +42,12 @@ function install-git() {
 	for MODULE in $*
 	do
 		[ -h $BASE_DIR/$(basename $MODULE) ] && rm $BASE_DIR/$(basename $MODULE)
-		cp -Rf $MODULE $BASE_DIR/
+		cp -Rf $MODULE $BASE_DIR/lib/
 	done
 }
 
 function install-hg() {
-	cd $BASE_DIR/download
+	cd $TMP_DIR
 	REPO_PATH=$1
 	shift
 	echo
@@ -50,12 +61,12 @@ function install-hg() {
 	for MODULE in $*
 	do
 		[ -h $BASE_DIR/$(basename $MODULE) ] && rm $BASE_DIR/$(basename $MODULE)
-		cp -Rf $MODULE $BASE_DIR/
+		cp -Rf $MODULE $BASE_DIR/lib/
 	done
 }
 
 function install-gcode-svn() {
-	cd $BASE_DIR/download
+	cd $TMP_DIR
 	REPO_PATH=$1
 	shift
 	echo
@@ -69,27 +80,27 @@ function install-gcode-svn() {
 	for MODULE in $*
 	do
 		[ -h $BASE_DIR/$(basename $MODULE) ] && rm $BASE_DIR/$(basename $MODULE)
-		cp -Rf $MODULE $BASE_DIR/
+		cp -Rf $MODULE $BASE_DIR/lib/
 	done
 }
 
 # OAUTH2 (for authorizing twitter)
-install-git https://github.com/deactivated/python-iso3166.git iso3166
-install-git https://github.com/sixohsix/twitter.git twitter
-install-git https://github.com/simplegeo/python-oauth2.git oauth2
-install-git https://github.com/twilio/twilio-python.git twilio
-install-git https://github.com/simplejson/simplejson.git simplejson
-install-git https://github.com/GoogleCloudPlatform/appengine-mapreduce.git python/src/mapreduce
-
 # SIX (for TWILIO)
-install-hg https://mikelambert@bitbucket.org/gutworth/six/ six.py
-install-hg https://code.google.com/p/google-api-python-client/ apiclient oauth2client uritemplate
-install-hg https://code.google.com/p/httplib2/ python2/httplib2
-install-hg https://code.google.com/p/gdata-python-client/ src/atom src/gdata
+#install-git https://github.com/deactivated/python-iso3166.git iso3166
+#install-git https://github.com/sixohsix/twitter.git twitter
+#install-git https://github.com/simplegeo/python-oauth2.git oauth2
+#install-git https://github.com/twilio/twilio-python.git twilio
+#install-git https://github.com/simplejson/simplejson.git simplejson
+#install-git https://github.com/GoogleCloudPlatform/appengine-mapreduce.git python/src/mapreduce
 
-install-gcode-svn spitfire spitfire
-install-gcode-svn python-gflags gflags.py gflags_validators.py
-install-gcode-svn graphy graphy
+#install-hg https://mikelambert@bitbucket.org/gutworth/six/ six.py
+#install-hg https://code.google.com/p/google-api-python-client/ apiclient oauth2client uritemplate
+#install-hg https://code.google.com/p/httplib2/ python2/httplib2
+#install-hg https://code.google.com/p/gdata-python-client/ src/atom src/gdata
+
+install-gcode-svn spitfire 	
+#install-gcode-svn python-gflags gflags.py gflags_validators.py
+#install-gcode-svn graphy graphy
 
 # Build our code
 cd $BASE_DIR
