cf38eec9fdec0acd25cdabfd45ce3c3fbc32c1a8
Mike Lambert <mlambert@gmail.com>
2015-02-09 12:26:58 -0500

    Move keys out of gmaps.py. Also setup a backup key, in case we make a mistake and actually blow our daily maps key quota. (for example, what hhappened when I ran tests, since they aren't hitting any cache.)

--- loc/gmaps.py
+++ loc/gmaps.py
@@ -5,6 +5,13 @@ import json
 import logging
 import urllib
 
+import keys
+
+USE_PRIVATE_KEY = False
+
+google_maps_private_key = keys.get("google_maps_private_key")
+google_server_key = keys.get("google_server_key")
+
 def fetch_raw(address=None, latlng=None, language=None, region=None, components=None):
     params = {}
     if address is not None:
@@ -13,21 +20,24 @@ def fetch_raw(address=None, latlng=None, language=None, region=None, components=
         params['latlng'] = '%s,%s' % latlng
     assert params
     params['sensor'] = 'false'
-    params['client'] = 'free-dancedeets'
     if language is not None:
         params['language'] = language
     if region is not None:
         params['region'] = region
     if components is not None:
         params['components'] = components
-    unsigned_url_path = "/maps/api/geocode/json?%s" % urllib.urlencode(params)
-    private_key = 'zj918QnslsoOQHl4kLjv-ZCgsDE='
-    decoded_key = base64.urlsafe_b64decode(private_key)
-    signature = hmac.new(decoded_key, unsigned_url_path, hashlib.sha1)
-    encoded_signature = base64.urlsafe_b64encode(signature.digest())
 
-    url = "http://maps.google.com%s&signature=%s" % (unsigned_url_path, encoded_signature)
-    print url
+    if USE_PRIVATE_KEY:
+        params['client'] = 'free-dancedeets'
+        unsigned_url_path = "/maps/api/geocode/json?%s" % urllib.urlencode(params)
+        private_key = google_maps_private_key
+        decoded_key = base64.urlsafe_b64decode(private_key)
+        signature = hmac.new(decoded_key, unsigned_url_path, hashlib.sha1)
+        encoded_signature = base64.urlsafe_b64encode(signature.digest())
+        url = "https://maps.google.com%s&signature=%s" % (unsigned_url_path, encoded_signature)
+    else:
+        unsigned_url_path = "/maps/api/geocode/json?%s" % urllib.urlencode(params)
+        url = "https://maps.google.com%s&key=%s" % (unsigned_url_path, google_server_key)
 
     logging.info('geocoding url: %s', url)
     result = urllib.urlopen(url).read()
