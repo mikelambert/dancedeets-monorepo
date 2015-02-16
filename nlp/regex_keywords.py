20f459b9e7308f12440a9315c4c770946cbf288a
Mike Lambert <mlambert@gmail.com>
2015-02-16 14:01:48 -0500

    Function renames.

--- nlp/regex_keywords.py
+++ nlp/regex_keywords.py
@@ -13,7 +13,22 @@ else:
     re.set_fallback_notification(re.FALLBACK_WARNING)
 from util import re_flatten
 
-def _prep_regex(regex_string, matching=False, word_boundaries=True, match_cjk=False, wrapper='%s'):
+def flatten_regex(strings):
+    try:
+        return re_flatten.construct_regex(strings)
+    except UnicodeDecodeError as e:
+        logging.exception('e %s', e)
+        # When we compile a gigantic regex and fail, let's try to compile the component pieces and see where things fall apart
+        for line in strings:
+            try:
+                line.encode('ascii')
+            except UnicodeDecodeError:
+                logging.error("failed to compile: %r: %s", line, line)
+                raise
+        logging.fatal("Error constructing regexes")
+        raise
+
+def _wrap_regex(regex_string, matching=False, word_boundaries=True, match_cjk=False, wrapper='%s'):
     if matching:
         regex_string = u'(' + regex_string + u')'
     else:
@@ -28,7 +43,7 @@ def _prep_regex(regex_string, matching=False, word_boundaries=True, match_cjk=Fa
         regex_string = wrapper % regex_string
     return regex_string
 
-def _make_regex_raw(regex_string, flags=0):
+def _compile_regex(regex_string, flags=0):
     if re2:
         # default max_mem is 8<<20 = 8*1000*1000
         return re.compile(regex_string, max_mem=60*1000*1000, flags=flags)
@@ -36,25 +51,12 @@ def _make_regex_raw(regex_string, flags=0):
         return re.compile(regex_string, flags=flags)
 
 def make_regexes(strings, matching=False, wrapper='%s', flags=0):
-    try:
-        regex_string = re_flatten.construct_regex(strings)
-    except UnicodeDecodeError as e:
-        logging.exception('e %s', e)
-        # When we compile a gigantic regex and fail, let's try to compile the component pieces and see where things fall apart
-        for line in strings:
-            try:
-                line.encode('ascii')
-            except UnicodeDecodeError:
-                logging.error("failed to compile: %r: %s", line, line)
-                raise
-        logging.fatal("Error constructing regexes")
-        return
-    return make_regexes_raw(regex_string, matching=matching, wrapper=wrapper, flags=flags)
+    return make_regexes_raw(flatten_regex(strings), matching=matching, wrapper=wrapper, flags=flags)
 
 NO_WORD_BOUNDARIES = 0
 WORD_BOUNDARIES = 1
 def make_regexes_raw(regex_string, matching=False, wrapper='%s', flags=0):
     a = [None] * 2
-    a[NO_WORD_BOUNDARIES] = _make_regex_raw(_prep_regex(regex_string, match_cjk=True, wrapper=wrapper), flags=flags)
-    a[WORD_BOUNDARIES] = _make_regex_raw(_prep_regex(regex_string, match_cjk=False, wrapper=wrapper), flags=flags)
+    a[NO_WORD_BOUNDARIES] = _compile_regex(_wrap_regex(regex_string, match_cjk=True, wrapper=wrapper), flags=flags)
+    a[WORD_BOUNDARIES] = _compile_regex(_wrap_regex(regex_string, match_cjk=False, wrapper=wrapper), flags=flags)
     return tuple(a)
