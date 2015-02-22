9ea6a5d7347104028cd7877bebb90e37cf4250f4
Mike Lambert <mlambert@gmail.com>
2015-02-22 06:21:16 -0500

    Add in the rest of our user-editable models to the admin site.

--- admin.py
+++ admin.py
@@ -3,16 +3,26 @@ from flask.ext.admin import Admin
 
 from flask.ext.admin.contrib import appengine
 
-from events import eventdata
-from topics import topic_db
+from events.cities import City
+from events.eventdata import DBEvent
+from events.users import User, UserFriendsAtSignup, UserMessage
+from fb_api import FacebookCachedObject
+from loc.gmaps_cached import CachedGeoCode
+from loc.gmaps_bwcompat import GeoCode
+from logic.event_locations import LocationMapping
+from logic.potential_events import PotentialEvent
+from logic.pubsub import OAuthToken
+from logic.thing_db import Source
+from topics.topic_db import Topic
 
 app = Flask(__name__)
 app.debug = True
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 admin = Admin(app, name="Admin")
-admin.add_view(appengine.ModelView(topic_db.Topic))
-admin.add_view(appengine.ModelView(eventdata.DBEvent))
+for model in [CachedGeoCode, City, DBEvent, FacebookCachedObject, GeoCode, LocationMapping, OAuthToken, PotentialEvent, Source, Topic, User, UserFriendsAtSignup, UserMessage]:
+    admin.add_view(appengine.ModelView(model))
+
 
 if __name__ == '__main__':
 
