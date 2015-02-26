7f4efbc5e9d0d43b20e545de58099909f81deba1
Mike Lambert <mlambert@gmail.com>
2015-02-26 14:32:06 -0500

    ndb.toplevel is too aggressive, and wants to finish the mapreduce iterators, even though we've intentionally broken out of the generator. Let's eliminate that portions of toplevel and run it ourself.

--- appengine_config.py
+++ appengine_config.py
@@ -1,6 +1,28 @@
-def webapp_add_wsgi_middleware(app):
+def tasklets_toplevel(func):
+    """A sync tasklet that sets a fresh default Context.
+
+    Use this for toplevel view functions such as
+    webapp.RequestHandler.get() or Django view functions.
+    """
     from google.appengine.ext.ndb import tasklets
-    app = tasklets.toplevel(app)
+    synctaskletfunc = tasklets.synctasklet(func)    # wrap at declaration time.
+    @tasklets.utils.wrapping(func)
+    def add_context_wrapper(*args, **kwds):
+        tasklets._state.clear_all_pending()
+        # Create and install a new context.
+        ctx = tasklets.make_default_context()
+        try:
+            tasklets.set_context(ctx)
+            return synctaskletfunc(*args, **kwds)
+        finally:
+            tasklets.set_context(None)
+            tasklets._state.clear_all_pending()
+    return add_context_wrapper
+
+
+def webapp_add_wsgi_middleware(app):
+    #from google.appengine.ext.ndb import tasklets
+    app = tasklets_toplevel(app)
 
     from google.appengine.ext.appstats import recording
     app = recording.appstats_wsgi_middleware(app)
