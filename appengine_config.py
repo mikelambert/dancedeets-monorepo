06cb26ab13d2adeb399a897ee14627c7801cc87f
Mike Lambert <mlambert@gmail.com>
2015-02-28 03:01:20 -0500

    Fix our NDB leftover-state clearing logic to work in the presence of generators. Add a way to enable NDB logging. Also attempt to fix the RPC ordering issue on these super-long generators wasting time in mapreduces.

--- appengine_config.py
+++ appengine_config.py
@@ -1,3 +1,5 @@
+import logging
+
 def tasklets_toplevel(func):
     """A sync tasklet that sets a fresh default Context.
 
@@ -5,29 +7,70 @@ def tasklets_toplevel(func):
     webapp.RequestHandler.get() or Django view functions.
     """
     from google.appengine.ext.ndb import tasklets
-    synctaskletfunc = tasklets.synctasklet(func)    # wrap at declaration time.
+    from google.appengine.ext.ndb import eventloop
     @tasklets.utils.wrapping(func)
     def add_context_wrapper(*args, **kwds):
         tasklets._state.clear_all_pending()
-        # Create and install a new context.
         ctx = tasklets.make_default_context()
         try:
             tasklets.set_context(ctx)
-            return synctaskletfunc(*args, **kwds)
+            result = func(*args, **kwds)
+            # We may be surrounding a generator (like the appstats middleware)
+            # So let's be sure we properly re-yield, so that the finally clause
+            # runs correctly at the end of the yields, and not when we return a
+            # generator ourselves (which then leaves it un-cleaned-up at the end).
+            if result is not None:
+                for value in result:
+                    yield value
         finally:
+            # Clean up everything we can. Emptying the context and pending Futures is the most important.
             tasklets.set_context(None)
             tasklets._state.clear_all_pending()
+            # But we clean out the event_loop early for good measure,
+            # since it would happen on the next request to this thread anyway.
+            if eventloop._state.event_loop:
+                eventloop._state.event_loop.clear()
+                eventloop._state.event_loop = None
     return add_context_wrapper
 
 
 def webapp_add_wsgi_middleware(app):
     #from google.appengine.ext.ndb import tasklets
-    app = tasklets_toplevel(app)
-
     from google.appengine.ext.appstats import recording
+
     app = recording.appstats_wsgi_middleware(app)
 
+    app = tasklets_toplevel(app)
+
+    # Should only use this in cases of serialized execution of requests in a multi-threaded processes.
+    # So setdeploy manually, and test from there. Never a live server, as it would be both broken *and* slow.
+    # import memory_leaks
+    #app = memory_leaks.leak_middleware(app)
+
     return app
+
+NDB_LOGGING_LEVEL = 0
+if NDB_LOGGING_LEVEL > 0:
+    from google.appengine.ext.ndb import utils
+    utils.logging_debug = logging.info
+    from google.appengine.ext.ndb import eventloop
+    eventloop._logging_debug = logging.info
+    if NDB_LOGGING_LEVEL > 1:
+        from google.appengine.ext.ndb import tasklets
+        tasklets._logging_debug = logging.info
+
+# This was a failed attempt at trying to keep a deque-style FIFO ordering to the event_loop.rpcs
+RPC_ORDERING = False
+if RPC_ORDERING:
+    import collections
+    from google.appengine.ext.ndb import eventloop
+    orig_get_event_loop = eventloop.get_event_loop
+    def new_get_event_loop():
+        event_loop = orig_get_event_loop()
+        event_loop.rpcs = collections.OrderedDict()
+        return event_loop
+    #eventloop.get_event_loop = new_get_event_loop
+
 
 # We don't need such real-time statistics (normally 1 second) on the mapreduce job.
 # More of an optimization to save on the associated database Get/Put every second.
