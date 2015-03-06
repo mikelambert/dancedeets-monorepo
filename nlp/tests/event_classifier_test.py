1c8ff82ed92ab8e6b4a180394db152e1dd5aee2f
Mike Lambert <mlambert@gmail.com>
2015-03-05 22:10:44 -0500

    Fix test (it had depended on an unsubmitted nlp/keywords.py change)

--- nlp/tests/event_classifier_test.py
+++ nlp/tests/event_classifier_test.py
@@ -4,6 +4,7 @@
 import unittest
 
 from nlp import event_classifier
+from nlp import keywords
 from nlp import grammar
 
 class TestSoulSessionsOslo(unittest.TestCase):
@@ -28,20 +29,27 @@ class TestKeywordLoader(unittest.TestCase):
         result = grammar.FileBackedKeyword._parse_keywords(['abcdefghijklmnopqrstuvwxyz#', 'ab(cd)ef #()', 'ab\(cd\)ef ###'])
         self.assertEqual(result[0], ['abcdefghijklmnopqrstuvwxyz', 'ab(cd)ef', 'ab\(cd\)ef'])
 
-class TestCJK(unittest.TestCase):
+class TestCJKAndWordBreaks(unittest.TestCase):
     def runTest(self):
-        fb_event = dict(info=dict(name="Blah", description="""
-╔════════════════════════════╗
+        string_processor = event_classifier.StringProcessor(u'the blocking dance')
+        self.assertFalse(string_processor.get_tokens(keywords.STYLE_LOCK))
 
-•´´¯`••. .•´´¯`••
-lockdanceevent
-╚════════════════════════════╝
-•´´¯`••. •´´¯`••.
-█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
-►
-"""))
-        classified_event = event_classifier.get_classified_event(fb_event)
-        self.assertFalse(classified_event.is_dance_event())
+        string_processor = event_classifier.StringProcessor(u'the locking dance')
+        self.assertTrue(string_processor.get_tokens(keywords.STYLE_LOCK))
+
+        string_processor = event_classifier.StringProcessor(u'今日はblockingです')
+        self.assertFalse(string_processor.get_tokens(keywords.STYLE_LOCK))
+
+        string_processor = event_classifier.StringProcessor(u'今日はlockingです')
+        self.assertTrue(string_processor.get_tokens(keywords.STYLE_LOCK))
+
+        string_processor = event_classifier.StringProcessor(u'今日はロックイングです')
+        self.assertTrue(string_processor.get_tokens(keywords.STYLE_LOCK))
+
+        string_processor = event_classifier.StringProcessor(u'今日はブロックイングです')
+        # Ideally we'd like this to return false,
+        # but word segmentation is near-impossible with cjk (and japanese katakana phrases)
+        self.assertTrue(string_processor.get_tokens(keywords.STYLE_LOCK))
 
 if __name__ == '__main__':
     print unittest.main()
