6fb9ec1f447cf5e9887c0f70b858dfff64cf289d
Mike Lambert <mlambert@gmail.com>
2015-03-07 07:20:30 -0500

    Remove another LookupEvent reference.

--- servlets/event_proxy.py
+++ servlets/event_proxy.py
@@ -1,24 +1,15 @@
 
-import fb_api
 import webapp2
 
 from events import eventdata
-from users import users
 from util import fetch
 
 class ImageProxyHandler(webapp2.RequestHandler):
     """Proxies images for use by twitter, where it doesn't need to respect the FB cache server's /robots.txt."""
 
     def get(self, event_id):
-        user = users.User.get_by_key_name(fb_api.USERLESS_UID)
-        fbl = fb_api.FBLookup(None, user.fb_access_token)
-        try:
-            event = fbl.get(fb_api.LookupEvent, event_id)
-        except fb_api.NoFetchedDataException:
-            self.response.set_status(404)
-            return
-
-        cover = eventdata.get_largest_cover(event)
+        db_event = eventdata.DBEvent.get_by_id(event_id)        
+        cover = eventdata.get_largest_cover(db_event.fb_event)
         if not cover:
             self.response.set_status(404)
             return
