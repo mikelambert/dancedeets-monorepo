7fbd0f93b9577fc1036f5794ed059d66207c8cb1
Mike Lambert <mlambert@gmail.com>
2015-03-08 16:18:09 -0400

    Dedupe before calling auto_add.classify_events

--- event_scraper/event_pipeline.py
+++ event_scraper/event_pipeline.py
@@ -24,8 +24,7 @@ def process_discovered_events(fbl, discovered_list):
     finally:
         fbl.allow_cache = orig_allow_cache
 
-    filtered_pe_events = []
-    filtered_fb_events = []
+    potential_events_added = []
     for fb_event, discovered in zip(fb_events, discovered_list):
         if not fb_event or fb_event['empty']:
             continue
@@ -36,10 +35,12 @@ def process_discovered_events(fbl, discovered_list):
         # makes a potential event, with scored information. transactions. one. by. one.
         discovered = potential_events.DiscoveredEvent(event_id, discovered.source, discovered.source_field)
         pe_event = potential_events.make_potential_event_with_source(fb_event, discovered)
-        filtered_pe_events.append(pe_event)
-        filtered_fb_events.append(fb_event)
+        potential_events_added.append(pe_event)
     # TODO: Create new sources, update source feed values, etc? done in make_potential_events_with_source, but need more that's not done there
 
+    fb_lookup = dict((x['info']['id'], x) for x in fb_events))
+    pe_lookup = dict((x.fb_event_id, x) for x in potential_events_added))
+    potential_event_ids = pe_lookup.keys()
     logging.info("Going to classify the %s potential events", len(discovered_list))
     # Classify events on the fly as we add them as potential events, instead of waiting for the mapreduce
-    auto_add.classify_events(fbl, filtered_pe_events, filtered_fb_events)
+    auto_add.classify_events(fbl, [pe_lookup[x] for x in potential_event_ids], [fb_lookup[x] for x in potential_event_ids])
