f8ff55b8bb6f977e6590e04066e53651d7824b07
Mike Lambert <mlambert@gmail.com>
2015-08-20 02:28:51 -0400

    Fix str-int issue when comparing ids for equality. Also speed up our mapreduce.

--- users/user_event_tasks.py
+++ users/user_event_tasks.py
@@ -12,8 +12,10 @@ def update_user_qualities(user):
 
     user.num_auto_added_events     = len([x for x in added_events if x and x.creating_method == eventdata.CM_AUTO])
     user.num_auto_added_own_events = len([x for x in added_events if x and x.creating_method == eventdata.CM_AUTO and x.owner_fb_uid == user.fb_uid])
-    user.num_hand_added_events     = len([x for x in added_events if x and x.creating_method == eventdata.CM_USER and x.creating_fb_uid == user.fb_uid])
-    user.num_hand_added_own_events = len([x for x in added_events if x and x.creating_method == eventdata.CM_USER and x.creating_fb_uid == user.fb_uid and x.owner_fb_uid == user.fb_uid])
+    #STR_ID_MIGRATE
+    user.num_hand_added_events     = len([x for x in added_events if x and x.creating_method == eventdata.CM_USER and str(x.creating_fb_uid) == user.fb_uid])
+    #STR_ID_MIGRATE
+    user.num_hand_added_own_events = len([x for x in added_events if x and x.creating_method == eventdata.CM_USER and str(x.creating_fb_uid) == user.fb_uid and x.owner_fb_uid == user.fb_uid])
 
 def map_compute_user_stats(user):
     update_user_qualities(user)
@@ -26,7 +28,7 @@ class RecomputeUserStatsHandler(base_servlet.BaseTaskRequestHandler):
             reader_spec='mapreduce.input_readers.DatastoreInputReader',
             handler_spec='users.user_event_tasks.map_compute_user_stats',
             mapper_parameters={'entity_kind': 'users.users.User'},
-            queue_name='slow-queue',
+            queue_name='fast-queue',
             shard_count=5,
         )
     post=get
