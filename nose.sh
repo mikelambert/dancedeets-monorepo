72917c425367ea62847607f6ccc4e7af2a6eed28
Mike Lambert <mlambert@gmail.com>
2015-09-10 06:18:43 -0400

    Update nose.sh with the latest modules we shouldn't test.

--- nose.sh
+++ nose.sh
@@ -3,5 +3,5 @@
 # We use without-sandbox, so that we properly load all the modules/tests
 cat app.yaml | sed 's/runtime: vm/runtime: python27/' > app-nose.yaml
 rm -rf lib/tests # this is pulled in via twilio, and messes with our excludes
-PYTHONPATH=lib nosetests --with-gae --gae-application=app-nose.yaml --exclude="flask_admin|werkzeug|flask|markupsafe|jinja2|oauth2|mapreduce|gdata|atom|httplib2|unittest2|simplejson|graphy|requests|rsa" $1 $2 $3 $4 $5
+PYTHONPATH=lib nosetests --with-gae --gae-application=app-nose.yaml --exclude="mock|flask_admin|werkzeug|flask|markupsafe|jinja2|oauth2|mapreduce|gdata|atom|httplib2|unittest2|simplejson|graphy|requests|rsa|pbr|wtforms" $1 $2 $3 $4 $5
 
