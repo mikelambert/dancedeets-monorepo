267f42fb8ce5bf44dc0da27ac1709e6fc5ea9dfa
Mike Lambert <mlambert@gmail.com>
2015-09-13 00:58:07 -0400

    Add support for visible_to_fb_uids to our event reloading pipeline.
    If an event fails to load with our public 701004 id, then let's do our fallback,
    searching through visible_to_fb_uids to grab a user token which works.
    
    Then process the events the same way within our fallback.

--- events/event_reloading_tasks.py
+++ events/event_reloading_tasks.py
@@ -1,14 +1,46 @@
 import logging
 
+from google.appengine.ext import deferred
 from mapreduce import context
 
 import base_servlet
 import fb_api
-from util import dates
 from util import fb_mapreduce
 from util import timings
+from users import users
 from . import eventdata
 from . import event_updates
+
+def add_event_tuple_if_updating(events_to_update, fbl, db_event, only_if_updated):
+    fb_event = fbl.fetched_data(fb_api.LookupEvent, db_event.fb_event_id, only_if_updated=only_if_updated)
+    # This happens when an event moves from TIME_FUTURE into TIME_PAST
+    if event_updates.need_forced_update(db_event):
+        fb_event = fbl.fetched_data(fb_api.LookupEvent, db_event.fb_event_id)
+    # If we have an event in need of updating, record that
+    if fb_event:
+        events_to_update.append((db_event, fb_event))
+
+def load_fb_events_using_backup_tokens(event_ids, only_if_updated, update_geodata):
+    db_events = eventdata.DBEvent.get_by_ids(event_ids)
+    events_to_update = []
+    for db_event in db_events:
+        processed = False
+        logging.info("Looking for event id %s with user ids %s", db_event.fb_event_id, db_event.visible_to_fb_uids)
+        for user in users.User.get_by_ids(db_event.visible_to_fb_uids):
+            fbl = fb_api.FBLookup(user.fb_uid, user.fb_access_token)
+            real_fb_event = fbl.get(fb_api.LookupEvent, db_event.fb_event_id)
+            if real_fb_event['empty'] != fb_api.EMPTY_CAUSE_INSUFFICIENT_PERMISSIONS:
+                add_event_tuple_if_updating(events_to_update, fbl, db_event, only_if_updated)
+                processed = True
+        # If we didn't process, it means none of our access_tokens are valid.
+        if not processed:
+            # Now mark our event as lacking in valid access_tokens, so that our pipeline can pick it up and look for a new one
+            db_event.visible_to_fb_uids = []
+            db_event.put()
+            # Let's update the DBEvent as necessary (note, this uses the last-updated FBLookup)
+            add_event_tuple_if_updating(events_to_update, fbl, db_event, only_if_updated)
+    event_updates.update_and_save_events(events_to_update, update_geodata=update_geodata)
+
 
 @timings.timed
 def yield_load_fb_event(fbl, db_events):
@@ -25,15 +57,19 @@ def yield_load_fb_event(fbl, db_events):
     else:
         update_geodata = True
         only_if_updated = True
+    empty_fb_event_ids = []
     for db_event in db_events:
         try:
-            fb_event = fbl.fetched_data(fb_api.LookupEvent, db_event.fb_event_id, only_if_updated=only_if_updated)
-            if event_updates.need_forced_update(db_event):
-                fb_event = fbl.fetched_data(fb_api.LookupEvent, db_event.fb_event_id)
-            if fb_event:
-                events_to_update.append((db_event, fb_event))
+            real_fb_event = fbl.fetched_data(fb_api.LookupEvent, db_event.fb_event_id)
+            if real_fb_event['empty'] != fb_api.EMPTY_CAUSE_INSUFFICIENT_PERMISSIONS:
+                empty_fb_event_ids.append(db_event.fb_event_id)
+            else:
+                add_event_tuple_if_updating(events_to_update, fbl, db_event, only_if_updated)
         except fb_api.NoFetchedDataException, e:
             logging.info("No data fetched for event id %s: %s", db_event.fb_event_id, e)
+    # Now trigger off a background reloading of empty fb_events
+    deferred.defer(load_fb_events_using_backup_tokens, empty_fb_event_ids, only_if_updated=only_if_updated, update_geodata=update_geodata)
+    # And then re-save all the events in here
     event_updates.update_and_save_events(events_to_update, update_geodata=update_geodata)
 map_load_fb_event = fb_mapreduce.mr_wrap(yield_load_fb_event)
 load_fb_event = fb_mapreduce.nomr_wrap(yield_load_fb_event)
