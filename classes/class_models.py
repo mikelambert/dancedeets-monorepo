d884c4d084acdb589e571fff5a7f27e08bb5c750
Mike Lambert <mlambert@gmail.com>
2015-10-09 17:49:21 -0700

    Turn off unnecessary indices.

--- classes/class_models.py
+++ classes/class_models.py
@@ -5,16 +5,16 @@ class StudioClass(ndb.Model):
     recurrence_id = ndb.StringProperty()
 
     studio_name = ndb.StringProperty()
-    source_page = ndb.StringProperty()
-    style = ndb.StringProperty()
-    teacher = ndb.StringProperty()
-    teacher_link = ndb.StringProperty()
+    source_page = ndb.StringProperty(indexed=False)
+    style = ndb.StringProperty(indexed=False)
+    teacher = ndb.StringProperty(indexed=False)
+    teacher_link = ndb.StringProperty(indexed=False)
     start_time = ndb.DateTimeProperty(indexed=False)
     end_time = ndb.DateTimeProperty(indexed=False)
 
     auto_categories = ndb.StringProperty(repeated=True)
 
-    latitude = ndb.FloatProperty()
-    longitude = ndb.FloatProperty()
+    latitude = ndb.FloatProperty(indexed=False)
+    longitude = ndb.FloatProperty(indexed=False)
 
-    scrape_time = ndb.DateTimeProperty()
+    scrape_time = ndb.DateTimeProperty(indexed=False)
