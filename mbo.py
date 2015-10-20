0705968e5cf2ab05b43e68446e95054bb189dc40
Mike Lambert <mlambert@gmail.com>
2015-10-20 02:59:42 -0400

    Refactor to fit naming conventions.

--- mbo.py
+++ mbo.py
@@ -1,4 +1,4 @@
-# mbo.py
+#!/usr/bin/env python
 
 import datetime
 import suds.client
@@ -15,66 +15,62 @@ USER_PASSWORD = "apitest1234"
 
 SITE_IDS = [-99]
 
-def BuildWsdlUrl(serviceName):
-    return "https://api.mindbodyonline.com/0_5/" + serviceName + "Service.asmx?wsdl"
+def get_client(service_name):
+    url = "https://api.mindbodyonline.com/0_5/" + service_name + "Service.asmx?wsdl"
+    return suds.client.Client(url)
 
-def GetActivationLink():
-    service = suds.client.Client(BuildWsdlUrl("Site"))
-    request = GetRequest(service, "GetActivationCode")
+def fill_credentials(service, request, siteIDs):
+    source_creds = service.factory.create('SourceCredentials')
+    source_creds.SourceName = SOURCE_NAME
+    source_creds.Password = SOURCE_PASSWORD
+    source_creds.SiteIDs.int = siteIDs
+
+    user_creds = service.factory.create('UserCredentials')
+    user_creds.Username = USER_NAME
+    user_creds.Password = USER_PASSWORD
+    user_creds.SiteIDs.int = siteIDs
+
+    request.XMLDetail = 'Full'
+    request.SourceCredentials = source_creds
+    request.UserCredentials = user_creds
+
+
+def get_request(service, request_name):
+    request = service.factory.create(request_name)
+    if hasattr(request, 'Request'):
+        request = request.Request
+    fill_credentials(service, request, siteIDs)
+    return request
+
+def get_activation_link():
+    service = get_client("Site")
+    request = get_request(service, "GetActivationCode")
     result = service.service.GetActivationCode(request)
     return result.ActivationLink
 
 
-def FillDefaultCredentials(service, request, siteIDs):
-    sourceCreds = service.factory.create('SourceCredentials')
-    sourceCreds.SourceName = SOURCE_NAME
-    sourceCreds.Password = SOURCE_PASSWORD
-    sourceCreds.SiteIDs.int = siteIDs
-
-    userCreds = service.factory.create('UserCredentials')
-    userCreds.Username = USER_NAME
-    userCreds.Password = USER_PASSWORD
-    userCreds.SiteIDs.int = siteIDs
-
-    request.SourceCredentials = sourceCreds
-    request.UserCredentials = userCreds
-    request.XMLDetail = "Full"
-    request.PageSize = 200
-    request.CurrentPageIndex = 0
-
-    return request
-
-def GetRequest(service, requestName):
-    request = service.factory.create(requestName)
-    if hasattr(request, 'Request'):
-        request = request.Request
-    return FillDefaultCredentials(service, request, siteIDs)
-
-print GetActivationLink()
-
-
-def GetClasses(
+def get_classes(
     startDateTime,
     endDateTime,
     hideCanceledClasses,
     pageSize):
-    service = suds.client.Client(BuildWsdlUrl("Class"))
-
-    request = GetRequest(service, "GetClasses")
+    service = get_client("Class")
+    request = get_request(service, "GetClasses")
 
     request.StartDateTime = startDateTime
     request.EndDateTime = endDateTime
     request.HideCanceledClasses = hideCanceledClasses
 
-    # This improves speed from 3 seconds to 2 seconds, so the additional debugging we get from not using it is worth it
+    # This improves speed from 3 seconds to 2 seconds, so the additional debugging advantages outweigh the speed benefits
     #request.XMLDetail = 'Bare'
     #request.Fields = BasicRequestHelper.FillArrayType(service, ["Classes.ClassDescription.Name", "Classes.StartDateTime", "Classes.EndDateTime", "Classes.Staff.Name"], "String")
     return service.service.GetClasses(request)
 
+print get_activation_link()
 
 start = datetime.datetime.combine(datetime.date.today(), datetime.time())
 end = start + datetime.timedelta(days=10)
-result = GetClasses(startDateTime=start, endDateTime=end, hideCanceledClasses=True, pageSize=200)
+result = get_classes(startDateTime=start, endDateTime=end, hideCanceledClasses=True, pageSize=200)
 if result.Status != 'Success':
     print result.Status
     print result.Message
