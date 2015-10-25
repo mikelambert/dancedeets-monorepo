91e5202faba5e112da5c524df96a270ef3969616
Mike Lambert <mlambert@gmail.com>
2015-10-25 04:10:52 -0400

    Make sure we reindex after scraping.

--- scrape_all.sh
+++ scrape_all.sh
@@ -1,1 +1,4 @@
 cat classes/scrapy/spiders/*.py | grep '^\sname = ' | grep -o "'.*'" | xargs -I{} ./scrapy.py crawl {}
+curl http://www.dancedeets.com/classes/reindex
+curl http://dev.dancedeets.com:8080/classes/reindex
+
