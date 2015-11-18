db505077b37ae4b1fcb707141ff04aae2ad9e904
Mike Lambert <mlambert@gmail.com>
2015-11-17 22:25:37 -0500

    Post new classes to both dev and prod servers.

--- classes/scrapy/pipelines.py
+++ classes/scrapy/pipelines.py
@@ -11,15 +11,19 @@ import urllib2
 
 DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 
-SERVER = 'www.dancedeets.com'
-#SERVER = 'dev.dancedeets.com:8080'
+PROD_SERVER = 'www.dancedeets.com'
+DEV_SERVER = 'dev.dancedeets.com:8080'
 
-def make_request(path, params):
+def make_request(server, path, params):
     data = json.dumps(params)
     quoted_data = urllib.quote_plus(data)
-    f = urllib2.urlopen('http://%s/%s' % (SERVER, path), quoted_data)
+    f = urllib2.urlopen('http://%s/%s' % (server, path), quoted_data)
     result = f.read()
     return result
+
+def make_requests(path, params):
+    make_request(PROD_SERVER, path, params)
+    make_request(DEV_SERVER, path, params)
 
 class SaveStudioClassPipeline(object):
 
@@ -30,7 +34,7 @@ class SaveStudioClassPipeline(object):
         params = {
             'studio_name': spider.name,
         }
-        result = make_request('classes/finish_upload', params)
+        result = make_requests('classes/finish_upload', params)
         if result:
             print 'Upload returned: ', result
 
@@ -39,6 +43,7 @@ class SaveStudioClassPipeline(object):
         for key in ['start_time', 'end_time', 'scrape_time']:
             new_item[key] = new_item[key].strftime(DATETIME_FORMAT)
         new_item['auto_categories'] = [x.index_name for x in new_item['auto_categories']]
-        result = make_request('classes/upload', new_item)
+        print new_item
+        result = make_requests('classes/upload', new_item)
         if result:
             print 'Upload returned: ', result
