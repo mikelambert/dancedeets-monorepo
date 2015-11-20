02c3b4e2b5a137ee2eed1a9ae5e9b3e8e86d0779
Mike Lambert <mlambert@gmail.com>
2015-11-20 15:42:00 -0500

    Add a setup.py so we can push to ScrapingHub.

---
+++ setup.py
@@ -0,0 +1,26 @@
+# Automatically created by: shub deploy
+
+from setuptools import setup, find_packages
+import glob
+
+setup(
+    name         = 'project',
+    version      = '1.0',
+    packages     = find_packages(),
+    install_requires = [
+        'icalendar',
+        'dateparser',
+    ],
+    py_modules = [
+        'event_types',
+        'keys',
+        'mindbody',
+        'styles',
+    ],
+    data_files   = [
+        ('dance_keywords', glob.glob('dance_keywords/*.txt')),
+        ('', ['keys.yaml']),
+    ],
+    include_package_data=True,
+    entry_points = {'scrapy': ['settings = classes.scrapy.settings']},
+)
