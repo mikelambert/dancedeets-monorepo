363709896705f1115090e6cf50e7f644ed1f7d69
Mike Lambert <mlambert@gmail.com>
2015-11-20 04:23:23 -0500

    Fix scraping script so it can be safely called from a crontab (or any cwd)

--- scrape_all.sh
+++ scrape_all.sh
@@ -1,4 +1,5 @@
-cat classes/scrapy/spiders/*.py | grep '^\sname = ' | grep -o "'.*'" | xargs -I{} ./scrapy.py crawl {}
+path=$(dirname $0)
+cat $path/classes/scrapy/spiders/*.py | grep '^\sname = ' | grep -o "'.*'" | xargs -I{} $path/scrapy.sh crawl {}
 curl http://www.dancedeets.com/classes/reindex
 curl http://dev.dancedeets.com:8080/classes/reindex
 
