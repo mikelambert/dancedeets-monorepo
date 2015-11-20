8fe57f2927496b462ceac9e2767229cf3620c051
Mike Lambert <mlambert@gmail.com>
2015-11-20 15:41:29 -0500

    Load keys.yaml file relative to our module.

--- keys.py
+++ keys.py
@@ -1,7 +1,8 @@
 # For loading private keys
+import os
 import yaml
 
-__KEY_CONFIG = yaml.load(file('keys.yaml', 'r'))
+__KEY_CONFIG = yaml.load(file(os.path.join(os.path.dirname(__file__), 'keys.yaml'), 'r'))
 
 def get(key):
     return __KEY_CONFIG[key]
