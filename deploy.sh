5f3a2891fe0abda40bf3717b9f8e45183d413943
Mike Lambert <mlambert@gmail.com>
2015-11-22 19:43:08 -0500

    Update deployment scripts.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
-DEFAULT_VERSION="$(gcloud preview app modules list | grep \* | awk '{print $2}')"
-gcloud --verbosity info preview app deploy app.yaml --set-default
-gcloud preview app modules stop default --version $DEFAULT_VERSION
+time yes | gcloud preview app deploy app.yaml --set-default --docker-build=remote
+
+#DEFAULT_VERSION="$(gcloud preview app modules list | grep \* | awk '{print $2}')"
+#gcloud --verbosity info preview app deploy app.yaml --set-default
+#gcloud preview app modules stop default --version $DEFAULT_VERSION
