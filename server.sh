876f45b7ec28c303b5f742f5ea54ea8834698aa7
Mike Lambert <mlambert@gmail.com>
2015-12-01 17:17:31 -0500

    Improvements to scripts and excludes in light of new gcloud behavior.

--- server.sh
+++ server.sh
@@ -1,6 +1,6 @@
-cat app.yaml | sed 's/runtime: vm/runtime: python27/' > app-devserver.yaml
+cat app.yaml | sed 's/runtime: custom/runtime: python27/' > app-devserver.yaml
 echo 'libraries:
 - name: ssl
   version: "latest"
 ' >> app-devserver.yaml
-dev_appserver.py app-devserver.yaml --storage_path ~/Projects/dancedeets-storage/
+dev_appserver.py app-devserver.yaml --storage_path ~/Projects/dancedeets-storage/ "$@"
