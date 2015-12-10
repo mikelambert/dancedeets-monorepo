5a0e1a99ce42785f595f1d0503c797a90da19c1e
Mike Lambert <mlambert@gmail.com>
2015-12-10 17:47:07 -0500

    Fix dev server by avoiding native-but-not-really-native socket lib.

--- server.sh
+++ server.sh
@@ -1,4 +1,4 @@
-cat app.yaml | sed 's/runtime: custom/runtime: python27/' > app-devserver.yaml
+cat app.yaml | sed 's/runtime: custom/runtime: python27/' | sed 's/GAE_USE_SOCKETS_HTTPLIB/x_disabled_GAE_USE_SOCKETS_HTTPLIB/' > app-devserver.yaml
 echo 'libraries:
 - name: ssl
   version: "latest"
