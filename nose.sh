ac97fba0bf27eeb099a7072632876eeb78e83bb9
Mike Lambert <mlambert@gmail.com>
2015-12-13 06:05:49 -0500

    Do a better job of future-proofing nose.sh, to handle whatever new modules might pop into the lib/ dir.

--- nose.sh
+++ nose.sh
@@ -3,5 +3,5 @@
 # We use without-sandbox, so that we properly load all the modules/tests
 cat app.yaml | sed 's/runtime: vm/runtime: python27/' > app-nose.yaml
 rm -rf lib/tests # this is pulled in via twilio, and messes with our excludes
-PYTHONPATH=lib nosetests --with-gae --gae-application=app-nose.yaml --exclude="mock|flask_admin|werkzeug|flask|markupsafe|jinja2|oauth2|mapreduce|gdata|atom|httplib2|unittest2|simplejson|graphy|requests|rsa|pbr|wtforms" $1 $2 $3 $4 $5
-
+MODULES=$(find lib -depth 1 | grep -v info | cut -f2- -d/ | sed 's/\.py//' | paste -s -d "|" -)
+PYTHONPATH=lib nosetests --with-gae --gae-application=app-nose.yaml --exclude="$MODULES" $1 $2 $3 $4 $5
