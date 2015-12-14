2845124d507ee1b12aad95d012846a45a6a3eba3
Mike Lambert <mlambert@gmail.com>
2015-12-14 17:39:58 -0500

    An attempt to fix our dev-appserver setup to work with the latest gcloud sdk (required me to comment-out the "raise errors.DockerfileError" code in the gcloud sdk, though)

--- server.sh
+++ server.sh
@@ -1,6 +1,2 @@
 cat app.yaml | sed 's/runtime: custom/runtime: python27/' | sed 's/GAE_USE_SOCKETS_HTTPLIB/x_disabled_GAE_USE_SOCKETS_HTTPLIB/' > app-devserver.yaml
-echo 'libraries:
-- name: ssl
-  version: "latest"
-' >> app-devserver.yaml
 dev_appserver.py app-devserver.yaml --storage_path ~/Projects/dancedeets-storage/ "$@"
