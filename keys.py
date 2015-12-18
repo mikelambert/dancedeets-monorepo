dba2c0f4f79d130cfed6d53e6f9ab2f81002644c
Mike Lambert <mlambert@gmail.com>
2015-12-18 05:44:24 -0500

    Make keys more forgiving of missing keys.yaml.

--- keys.py
+++ keys.py
@@ -1,8 +1,14 @@
 # For loading private keys
+import logging
 import os
 import yaml
 
-__KEY_CONFIG = yaml.load(file(os.path.join(os.path.dirname(__file__), 'keys.yaml'), 'r'))
+_filename = os.path.join(os.path.dirname(__file__), 'keys.yaml')
+if os.path.exists(_filename):
+    __KEY_CONFIG = yaml.load(file(_filename, 'r'))
+else:
+    logging.error('Cannot find keys.yaml, using an empty keys file instead.')
+    __KEY_CONFIG = {}
 
 def get(key):
     return __KEY_CONFIG[key]
