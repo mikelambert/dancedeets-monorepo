6236eb3bc1d82dd29ec396382b249f4a4176abcc
Mike Lambert <mlambert@gmail.com>
2015-12-18 02:48:33 -0500

    Use a more cross-platform find call.

--- nose.sh
+++ nose.sh
@@ -23,5 +23,5 @@ done
 # We use without-sandbox, so that we properly load all the modules/tests
 cat app.yaml | sed 's/runtime: vm/runtime: python27/' > app-nose.yaml
 rm -rf lib/tests # this is pulled in via twilio, and messes with our excludes
-MODULES=$(find lib -depth 1 | grep -v info | cut -f2- -d/ | sed 's/\.py//' | paste -s -d "|" -)
+MODULES=$(find lib -maxdepth 1 | grep -v info | cut -f2- -d/ | sed 's/\.py//' | paste -s -d "|" -)
 PYTHONPATH=lib $COVERAGE_PREFIX `which nosetests` --with-gae --gae-application=app-nose.yaml --exclude="$MODULES" $1 $2 $3 $4 $5
