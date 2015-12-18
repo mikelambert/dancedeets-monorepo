bcdb4c65f6e73e318cb0370f3c1801be4afbc697
Mike Lambert <mlambert@gmail.com>
2015-12-18 06:17:43 -0500

    Use facebook-dev.yaml explicitly.

--- facebook.py
+++ facebook.py
@@ -32,11 +32,38 @@ import hashlib
 import urllib
 import yaml
 
-prod_mode = 'SERVER_SOFTWARE' in os.environ and not os.environ['SERVER_SOFTWARE'].startswith('Dev')
-if prod_mode:
+
+def is_appengine():
+    return (is_local_appengine() or
+            is_prod_appengine() or
+            is_prod_appengine_mvms())
+
+
+def is_appengine_sandbox():
+    return is_appengine() and not is_prod_appengine_mvms()
+
+
+def is_local_appengine():
+    return ('APPENGINE_RUNTIME' in os.environ and
+            'Development/' in os.environ['SERVER_SOFTWARE'])
+
+
+def is_prod_appengine():
+    return ('APPENGINE_RUNTIME' in os.environ and
+            'Google App Engine/' in os.environ['SERVER_SOFTWARE'] and
+            not is_prod_appengine_mvms())
+
+
+def is_prod_appengine_mvms():
+    return os.environ.get('GAE_VM', False) == 'true'
+
+if is_local_appengine():
+    filename = 'facebook-dev.yaml'
+elif is_prod_appengine() or is_prod_appengine_mvms():
     filename = 'facebook-prod.yaml'
 else:
-    filename = 'facebook.yaml'
+    filename = 'facebook-test.yaml'
+
 FACEBOOK_CONFIG = yaml.load(file(filename, 'r'))
 
 # This new code pulled from: https://gist.github.com/1190267
