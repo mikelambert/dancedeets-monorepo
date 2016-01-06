680ab60a8d11cacc3bd4decee19e9d95080d4655
Mike Lambert <mlambert@gmail.com>
2016-01-06 08:16:21 -0500

    Plan out two days of notifications (in case we have some mapreduce failures on our 8-hour-ly run like today). And don't try to notify about events that have already started.

--- notifications/rsvped_events.py
+++ notifications/rsvped_events.py
@@ -27,22 +27,15 @@ def setup_reminders(fb_uid, fb_user_events):
         start_notify_window = start_time - datetime.timedelta(hours=1, minutes=10)
 
         # I think 30 days is the limit for appengine tasks with ETA set, but it gets trickier with all the timezones.
-        # And really, we run this code multiple times a day, so don't need to plan out more than a single day.
-        # Otherwise if the user un-rsvp's for an event, we'll still have a zombie task waiting to notify them.
-        # (And AppEngine doesn't like us removing-and-readding a named task item anyway)
+        # And really, we run this code multiple times a day, so don't need to plan out more than a day or two.
         now = datetime.datetime.now(start_time.tzinfo)
-        future_cutoff = now + datetime.timedelta(days=1)
-
-        if 'end_time' in event:
-            end_notify_window = parser.parse(event['end_time'])
-        else:
-            end_notify_window = start_time
+        future_cutoff = now + datetime.timedelta(days=2)
 
         # Any time after start_notify_window, we could have sent a notification.
         # If we try to re-add the taskqueue after that timestamp has passed,
         # we may re-add a task that has already completed, and re-notify a user.
         # Double-check in our logs that this does not occur....(it shouldn't!)
-        #end_notify_window = start_notify_window
+        end_notify_window = start_notify_window
 
         # Ignore events that started in the past
         if end_notify_window < now:
@@ -61,8 +54,8 @@ def setup_reminders(fb_uid, fb_user_events):
                     user_id=fb_uid,
                     event_id=event['id'])),
             )
-        except (taskqueue.TaskAlreadyExistsError, taskqueue.TombstonedTaskError):
-            pass
+        except (taskqueue.TaskAlreadyExistsError, taskqueue.TombstonedTaskError) as e:
+            logging.info("Error adding task: %s", e)
 
 def _is_attending(user, event_id):
     fbl = user.get_fblookup()
@@ -78,8 +71,6 @@ def _is_attending(user, event_id):
         return False
     return True
 
-#TODO: Delete old space eventually
-@app.route('/tasks/notify_user')
 @app.route('/tasks/remind_user')
 class RemindUserHandler(base_servlet.BaseTaskRequestHandler):
     def get(self):
