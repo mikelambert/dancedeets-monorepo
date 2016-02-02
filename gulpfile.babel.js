d7415f7f4b316074f225af18ff81b995db264e00
Mike Lambert <mlambert@gmail.com>
2016-02-03 04:15:19 +0800

    Grab a bunch more useful stuff from the web starter kit: https://github.com/google/web-starter-kit/

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,32 +1,44 @@
-var autoprefixer = require('autoprefixer');
-var browserify = require('browserify');
-var buffer     = require('vinyl-buffer');
+
+'use strict';
+
+// This gulpfile makes use of new JavaScript features.
+// Babel handles this without us having to do anything. It just works.
+// You can read more about the new JavaScript features here:
+// https://babeljs.io/docs/learn-es2015/
+
+
+import autoprefixer from 'autoprefixer';
+import browserify from 'browserify';
+import buffer from 'vinyl-buffer';
+import del from 'del';
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import gutil from 'gutil';
+import os from 'os';
+import path from 'path';
+import reactify from 'reactify';
+import runSequence from 'run-sequence';
+import source from 'vinyl-source-stream';
+import watchify from 'watchify';
+import username from 'username';
+import {output as pagespeed} from 'psi';
+
+import gulpif from 'gulp-if';
 var concat = require('gulp-concat');
 var cssnano = require('gulp-cssnano');
-var del = require('del');
-var gulp = require('gulp');
-var gulpif = require('gulp-if');
-var gutil = require('gutil');
 var lodash = require('lodash')
-var os = require("os");
-var path = require('path');
-var reactify   = require('reactify');
 var rename = require('gulp-rename');
 var responsive = require('gulp-responsive-images');
-var runSequence = require('run-sequence');
 var postcss = require('gulp-postcss');
 var sass = require('gulp-sass');
 var shell = require('gulp-shell');
-var source     = require('vinyl-source-stream');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 var uncss = require('gulp-uncss');
-var watchify = require('watchify');
-const username = require('username')
 
+const $ = gulpLoadPlugins();
 
 var baseAssetsDir = '/Users/' + username.sync() + '/Dropbox/dancedeets-art/build-assets/'
-
 
 var src  = './';
 var dest = './dist';
@@ -73,23 +85,41 @@ var config = {
   }
 };
 
+
+// Lint JavaScript
+gulp.task('lint', () => {
+  return gulp.src('assets/js/**/*.js')
+    .pipe($.eslint())
+    .pipe($.eslint.format())
+    .pipe($.eslint.failAfterError())
+});
+
+// Do we want this?
+//    .pipe($.newer('.tmp/styles'))
 function compileCssTo(destDir, destFilename) {
-    return function() {
+    return () => {
         return gulp.src(config.css.sourceFiles)
-            .pipe(sourcemaps.init({loadMaps: true}))
-                .pipe(gulpif('*.{sass,scss}', sass().on('error', sass.logError)))
-                .pipe(postcss([ autoprefixer({ browsers: ['> 2%'] }) ]))
-                .pipe(gulpif(destFilename != null, concat(destFilename || 'dummyArgSoConstructorPasses')))
+            .pipe($.sourcemaps.init())
+                .pipe($.sass({
+                    'precision': 10
+                }).on('error', $.sass.logError))
+                .pipe($.autoprefixer({
+                    browsers: ['> 2%']
+                }))
+                .pipe($.if(destFilename != null, concat(destFilename || 'dummyArgSoConstructorPasses')))
                 .pipe(gulp.dest(destDir))
-                .pipe(uncss(config.css.uncssArgs))
-                .pipe(rename({ extname: '.trim.css' }))
+                .pipe($.uncss(config.css.uncssArgs))
+                .pipe($.rename({ extname: '.trim.css' }))
                 .pipe(gulp.dest(destDir))
-                .pipe(cssnano({sourcemap: true}))
-                .pipe(rename({ extname: '.min.css', basename: path.basename(config.css.comboFile, '.css') }))
+                .pipe($.cssnano())
+                .pipe($.size({title: 'styles'}))
+                .pipe($.rename({ extname: '.min.css', basename: path.basename(config.css.comboFile, '.css') }))
                 .pipe(gulp.dest(destDir))
-            .pipe(sourcemaps.write('.'));
+            .pipe($.sourcemaps.write('.'));
     }
 }
+
+
 
 gulp.task('compile-css', compileCssTo(config.css.dest, 'main.css'));
 gulp.task('compile-css-individual-debug', compileCssTo(config.css.destDebug));
@@ -98,7 +128,7 @@ gulp.task('compile-js', compileJavascript(false));
 gulp.task('watchify', compileJavascript(true));
 
 function compileJavascript(watch) {
-  return function (callback) {
+  return function(callback) {
     var files = config.javascript.rootFiles;
 
     files.forEach(function (file) {
@@ -120,13 +150,14 @@ function compileJavascript(watch) {
         return b.bundle()
           .pipe(source(file.dest))
           .pipe(buffer())
-
           .pipe(gulp.dest(config.javascript.dest))
-          .pipe(sourcemaps.init({loadMaps: true}))
-            .pipe(uglify())
+          .pipe($.sourcemaps.init({loadMaps: true}))
+            .pipe($.babel())
+            .pipe($.uglify())
             .on('error', gutil.log)
-            .pipe(rename({ extname: '.min.js' }))
-          .pipe(sourcemaps.write('.'))
+            .pipe($.rename({ extname: '.min.js' }))
+            .pipe($.size({title: 'scripts'}))
+          .pipe($.sourcemaps.write('.'))
 
           .pipe(gulp.dest(config.javascript.dest));
       }
@@ -140,9 +171,9 @@ function compileJavascript(watch) {
   };
 }
 
-gulp.task('compile-images-resize', function () {
+gulp.task('compile-images-resize', () => {
   return gulp.src(baseAssetsDir + 'img/**/*.{png,jpg}')
-    .pipe(responsive({
+    .pipe($.responsiveImages({
       '{location,style}-*.jpg': [{
         width: 450,
         height: 300,
@@ -159,39 +190,56 @@ gulp.task('compile-images-resize', function () {
       'deets-head-and-title-on-black.png': [{
       }],
     }))
+    .pipe($.cache($.imagemin({
+      progressive: true,
+      interlaced: true
+    })))
     .pipe(gulp.dest('dist/img'));
 });
 
 gulp.task('compile-images', ['compile-images-resize', 'compile-svg', 'compile-icons']);
 
 // gets deets-activity svg files
-gulp.task('compile-svg', function () {
+gulp.task('compile-svg', () => {
   return gulp.src(baseAssetsDir + 'img/*.svg')
     .pipe(gulp.dest('dist/img'));
 });
 
-gulp.task('compile-icons', function () {
+gulp.task('compile-icons', () => {
   return gulp.src('assets/img/icons/social/*.png')
     .pipe(gulp.dest('dist/img/icons/social/'));
 });
 
-gulp.task('compile-fonts', function () {
+gulp.task('compile-fonts', () => {
   return gulp.src('bower_components/font-awesome/fonts/*.*')
     .pipe(gulp.dest('dist/fonts/'));
 });
 
+
+// Run PageSpeed Insights
+gulp.task('pagespeed', cb =>
+  // Update the below URL to the public URL of your site
+  pagespeed('www.dancedeets.com', {
+    strategy: 'mobile'
+    // By default we use the PageSpeed Insights free (no API key) tier.
+    // Use a Google Developer API key if you have one: http://goo.gl/RkN0vE
+    // key: 'YOUR_API_KEY'
+  }, cb)
+);
+
+
 gulp.task('compile', ['compile-js', 'compile-css', 'compile-images', 'compile-fonts']);
 
-gulp.task('clean', function() {
+gulp.task('clean', () => {
   return del.sync('dist');
 })
 
-gulp.task('test', shell.task(['./nose.sh']));
+gulp.task('test', $.shell.task(['./nose.sh']));
 
 gulp.task('clean-build-test', function (callback) {
-    runSequence('clean', 'compile', 'test', callback);
+    runSequence('clean', 'compile', 'lint', 'test', callback);
 });
 
 // TODO: someday we may want something more elaborate like:
 // https://github.com/gulpjs/gulp/blob/master/docs/recipes/automate-release-workflow.md
-gulp.task('deploy', ['clean-build-test'], shell.task(['./deploy.sh']));
+gulp.task('deploy', ['clean-build-test'], $.shell.task(['./deploy.sh']));
