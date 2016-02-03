f8f8f896c4bdf2f3cb0b93e4d841aa601cc2f08c
Mike Lambert <mlambert@gmail.com>
2016-02-03 14:59:22 +0800

    Fix some lint errors.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,40 @@
 global.jQuery = global.$ = require('jquery');
 require('jquery.backstretch');
 require('bootstrap');
-//var WOW = require('wow.js');
 var App = require('./app');
 
-//from old site: jquery.cookie@1.4.1,momentjs@2.10.6,jquery.lazyload@1.9.3
+// From old site: jquery.cookie@1.4.1,momentjs@2.10.6,jquery.lazyload@1.9.3
 
-jQuery(document).ready(function() {
-    App.init()
-//    new WOW.WOW().init(live=false);
+$(document).ready(function() {
+  App.init()
 
-    // background-image rotation
-    var images = [
-        "dist/img/background-show-locking.jpg",
-        "dist/img/background-class-overhead.jpg",
-        "dist/img/background-club-turntable.jpg",
-        "dist/img/background-show-awards.jpg",
+  // background-image rotation
+  var images = [
+    'dist/img/background-show-locking.jpg',
+    'dist/img/background-class-overhead.jpg',
+    'dist/img/background-club-turntable.jpg',
+    'dist/img/background-show-awards.jpg',
 
-        "dist/img/background-class-kids.jpg",
-        "dist/img/background-show-pose.jpg",
-        "dist/img/background-club-smoke-cypher.jpg",
-        "dist/img/background-class-rocking.jpg",
-        "dist/img/background-club-hustle.jpg",
-        "dist/img/background-show-dj.jpg",
-        "dist/img/background-club-headspin.jpg",
-    ]
-    if ($(document).width() > 900) {
-        images = images.map(function(x) {
-            return x.replace('.jpg', '@2x.jpg');
-        });
-    }
-    $(".fullscreen-static-image").backstretch(images, {duration: 8000, fade: 1500});
-
-    // animate-on-hover
-    $(".animate-on-hover").hover(function(){
-        $(this).addClass('animated ' + $(this).data('action'));
+    'dist/img/background-class-kids.jpg',
+    'dist/img/background-show-pose.jpg',
+    'dist/img/background-club-smoke-cypher.jpg',
+    'dist/img/background-class-rocking.jpg',
+    'dist/img/background-club-hustle.jpg',
+    'dist/img/background-show-dj.jpg',
+    'dist/img/background-club-headspin.jpg',
+  ];
+  if ($(document).width() > 900) {
+    images = images.map(function(x) {
+      return x.replace('.jpg', '@2x.jpg');
     });
-    $(".animate-on-hover").bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",function(){
-        $(this).removeClass('animated ' + $(this).data('action'));
-    });
+  }
+  $('.fullscreen-static-image').backstretch(images, {duration: 8000, fade: 1500});
+
+  // animate-on-hover
+  $('.animate-on-hover').hover(function() {
+    $(this).addClass('animated ' + $(this).data('action'));
+  });
+  $('.animate-on-hover').bind('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', function() {
+    $(this).removeClass('animated ' + $(this).data('action'));
+  });
 });
-
-
