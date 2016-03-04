9398dfe501a7780f092619058f9fbaea8c195c20
Mike Lambert <mlambert@gmail.com>
2016-03-04 13:41:50 +0800

    Let pip work as user without sudo.

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ mkdir -p $TMP_DIR
 
 cd $TMP_DIR
 curl https://bootstrap.pypa.io/get-pip.py --output $TMP_DIR/get-pip.py
-sudo python $TMP_DIR/get-pip.py
+python $TMP_DIR/get-pip.py --user
 
 # For testing, just install them locally. Depends on pip being installed.
 # Install jinja2 library directly, so nlp/ libraries can be used directly
