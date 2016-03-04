37bbbb375b1683b9443ab54f40664f18e1c7499c
Mike Lambert <mlambert@gmail.com>
2016-03-04 18:24:06 +0800

    Lint cleanups

--- hacks/fixed_mapreduce_util.py
+++ hacks/fixed_mapreduce_util.py
@@ -1,13 +1,12 @@
 """We monkey patch this function, so that mapreduce jobs are always run on the latest uploaded code version."""
 
-import os
 from mapreduce import util as mapreduce_util
-from pipeline import pipeline as pipeline_pipeline
 from pipeline import util as pipeline_util
 
 
 def _get_task_host():
     return None
+
 
 def patch_function():
     mapreduce_util._get_task_host = _get_task_host
