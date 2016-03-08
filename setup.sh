dacb3a0fc5cd43e339ecbbbfc6d2ca0fde65c8de
Mike Lambert <mlambert@gmail.com>
2016-03-09 02:09:12 +0800

    travis 5

--- setup.sh
+++ setup.sh
@@ -9,7 +9,6 @@ mkdir -p $TMP_DIR
 
 cd $TMP_DIR
 curl https://bootstrap.pypa.io/get-pip.py --output $TMP_DIR/get-pip.py
-python $TMP_DIR/get-pip.py --user
 
 # For testing, just install them locally. Depends on pip being installed.
 USER_FLAG='--user'
@@ -17,6 +16,9 @@ echo "Travis is $TRAVIS"
 if [ "$TRAVIS" == true ]; then
   USER_FLAG=''
 fi
+
+python $TMP_DIR/get-pip.py $USER_FLAG
+
 pip install --upgrade $USER_FLAG -r $BASE_DIR/test-requirements.txt
 
 pip install --upgrade -t $BASE_DIR/lib -r $BASE_DIR/setup-requirements.txt
