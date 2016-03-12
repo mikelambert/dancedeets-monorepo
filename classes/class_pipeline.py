4a033a690ffba687bb1a119c4cac2f4b5a2fb482
Mike Lambert <mlambert@gmail.com>
2016-03-13 06:16:05 +0800

    Lint cleanups

--- classes/class_pipeline.py
+++ classes/class_pipeline.py
@@ -18,8 +18,10 @@ from util import fixed_pipelines
 
 def get_spiders():
     return [
-        'PMT', 'Evolution', 'Peridance', 'BDC', # NY
-        'NeighborhoodStudio', 'Millenium', 'EDGE', 'DebbieReynolds', 'EXPG', 'Boogiezone', 'IDA', 'mL', 'Quest', # LA
+        # NY
+        'PMT', 'Evolution', 'Peridance', 'BDC',
+        # LA
+        'NeighborhoodStudio', 'Millenium', 'EDGE', 'DebbieReynolds', 'EXPG', 'Boogiezone', 'IDA', 'mL', 'Quest',
     ]
     # This depends on Twisted, which depends on zope.interface and lxml. And that whole ball of wax fails when run in the appengine dev sandbox.
     # We can't import any of classes/scrapers/ (since it all ultimately depends on scrapy), so there's no great way to get a list of classes.
@@ -122,7 +124,6 @@ class EmailErrors(fixed_pipelines.Pipeline):
 @app.route('/tasks/crawl_and_index_classes')
 class CrawlAndIndexClassesHandler(base_servlet.BaseTaskRequestHandler):
     def get(self):
-
         pipeline = CrawlAndIndexClassesJob()
         pipeline.start(queue_name='slow-queue')
         self.response.out.write('OK')
