706b8f59aaf43095d625e7bbbd10f10291241867
Mike Lambert <mlambert@gmail.com>
2016-03-13 05:37:42 +0800

    Add Quest scraper using MINDBODY too.

---
+++ classes/scraper/spiders/la_quest.py
@@ -0,0 +1,9 @@
+from .. import browser_scraper
+
+
+class Quest(browser_scraper.MindBodyBrowserScraper):
+    name = 'Quest'
+    latlong = (33.8727865, -118.079369)
+    address = '11976 Artesia Blvd, Artesia CA'
+
+    mindbody_studio_id = 110557
