9df33bf00266c4c5538cafad3c120d9e1a4cd060
Mike Lambert <mlambert@gmail.com>
2016-03-13 06:23:51 +0800

    Add html2text to our dependencies

--- setup.py
+++ setup.py
@@ -8,8 +8,11 @@ setup(
     version='1.0',
     packages=find_packages(),
     install_requires=[
+        # I'm not sure this really does anything more than documentation,
+        # as our our dependencies need to be installed as eggs directly on dash.scrapinghub.com
         'icalendar',
         'dateparser',
+        'html2text',
     ],
     py_modules=[
         'event_types',
