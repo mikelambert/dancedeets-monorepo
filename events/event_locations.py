2c4e02e439ec6a7dd8906a12d564f4d1a6d63370
Mike Lambert <mlambert@gmail.com>
2016-03-21 01:15:34 +0800

    Lint cleanups and remove deprecated code.

--- events/event_locations.py
+++ events/event_locations.py
@@ -7,6 +7,8 @@ from loc import gmaps_api
 from loc import formatting
 from util import abbrev
 
+ONLINE_ADDRESS = 'ONLINE'
+
 # TODO: do some auto-classification of ripley grier, pearl studios, champion studios, etc. People always type horrible addresses.
 # UCI
 # UCLB
@@ -16,8 +18,10 @@ from util import abbrev
 
 # TODO(lambert): when we get an un-geocodable address, stick with last-known-address in the DBEvent, and do not overwrite with None
 
+
 class LocationMapping(db.Model):
     remapped_address = db.StringProperty(indexed=False)
+
 
 def city_for_fb_location(location):
     state = abbrev.states_full2abbrev.get(location.get('state'), location.get('state'))
@@ -29,6 +33,7 @@ def city_for_fb_location(location):
     else:
         return None
 
+
 def _get_latlng_from_event(fb_event):
     venue = fb_event['info'].get('venue')
     if venue and venue.get('latitude') and venue.get('longitude'):
@@ -37,6 +42,7 @@ def _get_latlng_from_event(fb_event):
     # We don't appear to get this any more, so we can eliminate our event-dependent venue lookup code.
     # Maybe we get an id and no lat/lng still, but looking up the latest venue information shouldn't give a lat/lng either.
     return None
+
 
 def get_address_for_fb_event(fb_event):
     event_info = fb_event['info']
@@ -56,18 +62,20 @@ def get_address_for_fb_event(fb_event):
     # many geocodes have a couple trailing digits, a la "VIA ROMOLO GESSI 14"
     return re.sub(r' \d{,3}$', '', final_address)
 
+
 def _get_remapped_address_for(address):
     address = (address or '').strip()
     if not address:
         return None
     # map locations to corrected locations for events that have wrong or incomplete info
-    #TODO(lambert): How about we have a sharded-memcache-key based on first hexadecimal character of md5-hash of address. this key-value would store all re-mappings with that prefix, and could be db-and-memcached easily.
-    #TODO(lambert): Write a mapreduce which goes through events looking for unnecessary mappings to clear out the mapping space.
+    # TODO(lambert): How about we have a sharded-memcache-key based on first hexadecimal character of md5-hash of address. this key-value would store all re-mappings with that prefix, and could be db-and-memcached easily.
+    # TODO(lambert): Write a mapreduce which goes through events looking for unnecessary mappings to clear out the mapping space.
     location_mapping = LocationMapping.get_by_key_name(address)
     if location_mapping:
         return location_mapping.remapped_address
     else:
         return None
+
 
 def _save_remapped_address_for(original_address, new_remapped_address):
     original_address = (original_address or '').strip()
@@ -87,6 +95,7 @@ def _save_remapped_address_for(original_address, new_remapped_address):
             if location_mapping:
                 location_mapping.delete()
 
+
 def update_remapped_address(fb_event, new_remapped_address):
     new_remapped_address = new_remapped_address or None
     location_info = LocationInfo(fb_event)
@@ -95,16 +104,16 @@ def update_remapped_address(fb_event, new_remapped_address):
         _save_remapped_address_for(location_info.fb_address, new_remapped_address)
 
 
-ONLINE_ADDRESS = 'ONLINE'
-
 def online_address(address):
     return address == ONLINE_ADDRESS
+
 
 def get_geocode(address):
     if online_address(address):
         return None
     else:
         return gmaps_api.get_geocode(address=address)
+
 
 class LocationInfo(object):
     def __init__(self, fb_event, db_event=None, debug=False):
@@ -141,26 +150,10 @@ class LocationInfo(object):
                 self.online = True
             logging.info("Final address is %r", final_address)
             self.geocode = db_event.get_geocode() if has_geocode else get_geocode(address=final_address)
-            # TODO(lambert): cleanup: delete this try/except and contents
-            try:
-                self.final_city = formatting.format_geocode(self.geocode)
-            except TypeError:
-                logging.error("1format_geocode had error, with geocode %s", self.geocode)
-                logging.error("1geocode has data %r", self.geocode.__dict__)
-                logging.error("1dbevent had geocode %r", (db_event.get_geocode().__dict__ if has_geocode else None))
-                gmaps_api.delete(address=final_address)
+            self.final_city = formatting.format_geocode(self.geocode)
 
         if self.geocode:
-            # TODO(lambert): cleanup: delete this if statement and try/except (but not the contents)
-            if self.geocode.lookup_kwargs and isinstance(self.geocode.json_data, (str, unicode)):
-                gmaps_api.delete(**self.geocode.lookup_kwargs)
-            try:
-                self.final_city = formatting.format_geocode(self.geocode)
-            except TypeError:
-                logging.error("2format_geocode had error, with geocode %s", self.geocode)
-                logging.error("2geocode has data %r", self.geocode.__dict__)
-                logging.error("2dbevent had geocode %r", (db_event.get_geocode().__dict__ if has_geocode else None))
-                logging.error("2looked up final address %s", final_address)
+            self.final_city = formatting.format_geocode(self.geocode)
             self.final_latlng = self.geocode.latlng()
         elif self.online:
             self.final_city = ONLINE_ADDRESS
@@ -189,4 +182,3 @@ class LocationInfo(object):
             return None, None
         else:
             return self.final_latlng
-
