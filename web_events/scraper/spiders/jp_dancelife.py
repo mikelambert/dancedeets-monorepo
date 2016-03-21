444c65181c7c537b070be41a23da4eb7ec030825
Mike Lambert <mlambert@gmail.com>
2016-03-22 05:10:39 +0800

    Strip out markdown text as being fluff in our description, and do a better job of grabbing the locations.

--- web_events/scraper/spiders/jp_dancelife.py
+++ web_events/scraper/spiders/jp_dancelife.py
@@ -1,7 +1,6 @@
 # -*-*- encoding: utf-8 -*-*-
 import datetime
 import re
-import urlparse
 
 import scrapy
 
@@ -9,12 +8,13 @@ from events import namespaces
 from loc import japanese_addresses
 from .. import items
 from .. import jp_spider
+from util import strip_markdown
 
 
 class TokyoDanceLifeScraper(items.WebEventScraper):
     name = 'TokyoDanceLife'
     allowed_domains = ['www.tokyo-dancelife.com']
-    namespace = namespaces.JAPAN_DL
+    namespace = namespaces.JAPAN_TDL
 
     def start_requests(self):
         yield scrapy.Request('http://www.tokyo-dancelife.com/event/')
@@ -60,16 +60,13 @@ class TokyoDanceLifeScraper(items.WebEventScraper):
 
         category = response.css('div.event-detail-koumoku').xpath('./img/@alt').extract()[0]
         # Because dt otherwise remains flush up against the end of the previous dd, we insert manual breaks.
-        full_description = items._format_text(response.xpath('//dl').extract()[0].replace('<dt>', '<dt><br><br>'))
+        full_description = strip_markdown.strip(items._format_text(response.xpath('//dl').extract()[0].replace('<dt>', '<dt><br><br>')))
         item['description'] = '%s\n\n%s' % (category, full_description)
 
         jp_addresses = japanese_addresses.find_addresses(item['description'])
-        venue = items.get_line_after(item['description'], ur'場所|会場')
+        venue = items.get_line_after(strip_markdown.strip(item['description']), ur'場所|会場|LOCATION')
         jp_spider.setup_location(venue, jp_addresses, item)
 
         item['start_time'], item['end_time'] = self.parseDateTimes(response)
-
-        #item['latitude'] = latlng['lat']
-        #item['longitude'] = latlng['lng']
 
         yield item
