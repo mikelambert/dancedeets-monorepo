eedf5bebf47dd2a7bbc2da05ad27ed9c73420f40
Mike Lambert <mlambert@gmail.com>
2016-03-22 23:13:36 +0800

    Some URLs have unicode in them, so let's normalize that. This was broken on prod appengine, but dev appengine handled it fine.

--- util/fetch.py
+++ util/fetch.py
@@ -2,7 +2,7 @@ import urllib2
 
 
 def fetch_data(url):
-    req = urllib2.Request(url)
+    req = urllib2.Request(url.encode('utf-8'))
     response = urllib2.urlopen(req)
     mimetype = response.info().getheader('Content-Type')
     return mimetype, response.read()
