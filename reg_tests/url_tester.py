a31252974319aa6123660b38f092603191243037
Mike Lambert <mlambert@gmail.com>
2016-04-01 07:47:53 +0800

    Add some regtests, in particular the beginnings of our very own url tester!

---
+++ reg_tests/url_tester.py
@@ -0,0 +1,58 @@
+import logging
+import json
+import urllib2
+
+URLS = [
+    'http://www.dancedeets.com/login?next=%2F%3F',
+    'http://www.dancedeets.com/?location=USA',
+    'http://www.dancedeets.com/?location=Europe',
+    'http://www.dancedeets.com/?location=Asia',
+    'http://www.dancedeets.com/events/386287078085795/', # no amp page
+    'http://www.dancedeets.com/events/1171204409564075/',
+    'http://www.dancedeets.com/events/1171204409564075/?amp=1',
+    'http://www.dancedeets.com/events/dance-delight:20554/',
+    'http://www.dancedeets.com/events/dance-delight:20554/?amp=1',
+    'http://www.dancedeets.com/events/street-dance-korea:190/',
+    'http://www.dancedeets.com/events/street-dance-korea:190/?amp=1',
+    'http://www.dancedeets.com/events/image_proxy/dance-delight:20554/',
+    'http://www.dancedeets.com/events/image_proxy/1459851027584883',
+    'http://www.dancedeets.com/events/image_proxy/1459851027584883/',
+]
+API_URLS = [
+    'http://www.dancedeets.com/api/v1.2/search?location=USA&time_period=UPCOMING',
+    'http://www.dancedeets.com/api/v1.2/search?location=Europe&time_period=UPCOMING',
+    'http://www.dancedeets.com/api/v1.2/search?location=Asia&time_period=UPCOMING',
+    'http://www.dancedeets.com/api/v1.2/events/1171204409564075',
+    'http://www.dancedeets.com/api/v1.2/events/dance-delight:20554',
+]
+
+
+def fetch(url):
+    print url
+    req = urllib2.Request(url.encode('utf-8'))
+    try:
+        response = urllib2.urlopen(req)
+    except urllib2.HTTPError as e:
+        return e
+    return response
+
+
+def error(url, error):
+    logging.error('URL Error: %s', url)
+    logging.error('  %s', error)
+
+for url in URLS:
+    response = fetch(url)
+    if isinstance(response, Exception):
+        error(url, response)
+    elif response.code != 200:
+        error(url, response.read())
+
+for url in API_URLS:
+    response = fetch(url)
+    if isinstance(response, Exception):
+        error(url, response)
+    else:
+        data = json.loads(response.read())
+        if 'errors' in data:
+            error(url, data['errors'])
