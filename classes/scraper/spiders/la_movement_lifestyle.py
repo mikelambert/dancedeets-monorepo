637dfd4085937abf55a0fadc96b01837ad09a7ba
Mike Lambert <mlambert@gmail.com>
2016-04-01 07:46:05 +0800

    Reclassify most of the generic classes on mL as hiphop/streetjazz.

--- classes/scraper/spiders/la_movement_lifestyle.py
+++ classes/scraper/spiders/la_movement_lifestyle.py
@@ -1,3 +1,6 @@
+import re
+
+import event_types
 from .. import browser_scraper
 
 
@@ -8,3 +11,10 @@ class MovementLifestyle(browser_scraper.MindBodyBrowserScraper):
 
     mindbody_studio_id = 133521
     mindbody_tab_id = 7
+
+    def _get_auto_categories(self, studio_class):
+        categories = super(MovementLifestyle, self)._get_auto_categories(studio_class)
+        lower_style = studio_class['style'].lower()
+        if re.search(r'\b(?:class|ml)\b', lower_style):
+            categories.append(event_types.HIPHOP)
+        return list(set(categories))
