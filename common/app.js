0919fb67266ccc7eb01e515852d24696c35ed168
Mike Lambert <mlambert@gmail.com>
2016-04-14 15:12:50 +0800

    Get things working on iOS and Android.

---
+++ common/app.js
@@ -0,0 +1,72 @@
+
+import React, {
+  Component,
+  ListView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+import { EventListView } from './events';
+
+class DancedeetsReact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  render() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <View
+        style={styles.container}>
+        <EventListView dataSource={this.state.dataSource} />
+      </View>
+    );
+  }
+
+  renderLoadingView() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading events...
+        </Text>
+      </View>
+    );
+  }
+
+  fetchData() {
+    fetch("http://www.dancedeets.com/api/v1.2/search?location=Taipei&time_period=UPCOMING")
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log(responseData);
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData.results),
+          loaded: true,
+        });
+      })
+      .done();
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#000',
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
+
+module.exports = DancedeetsReact;
