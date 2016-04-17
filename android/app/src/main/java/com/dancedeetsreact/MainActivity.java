7206459fac661da1be3e8ddb71377bc647191fcb
Mike Lambert <mlambert@gmail.com>
2016-04-17 22:52:35 +0800

    The result of running rnpm link.

--- android/app/src/main/java/com/dancedeetsreact/MainActivity.java
+++ android/app/src/main/java/com/dancedeetsreact/MainActivity.java
@@ -1,6 +1,8 @@
 package com.dancedeetsreact;
 
 import com.facebook.react.ReactActivity;
+import com.kevinejohn.RNMixpanel.RNMixpanel;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
@@ -34,7 +36,9 @@ public class MainActivity extends ReactActivity {
     @Override
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
-            new MainReactPackage()
+            new MainReactPackage(),
+            new RNMixpanel(),
+            new FBSDKPackage()
         );
     }
 }
