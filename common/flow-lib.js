f98fcee730bfcfc856264d472c764a98de6fdef3
Mike Lambert <mlambert@gmail.com>
2016-04-19 19:05:21 +0800

    Handle the last lint error we have.

---
+++ common/flow-lib.js
@@ -0,0 +1,24 @@
+/**
+ * Copyright 2016 DanceDeets.
+ *
+ * @flow
+ */
+
+'use strict';
+
+/* eslint no-unused-vars:0 */
+declare var jest: any;
+declare var jasmine: any;
+declare var describe: (name: string, callback: () => void) => void;
+declare var it: (name: string, callback: () => void) => void;
+declare var expect: any;
+
+declare module 'react-native-linear-gradient' {
+  declare var exports: any;
+}
+
+declare module 'react-native-push-notification' {
+  declare var exports: any;
+}
+
+declare var requestAnimationFrame: (callback: () => void) => number;
