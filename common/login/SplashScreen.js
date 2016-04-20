ac95793db77d25cd805a17d8b7d0af501887adb2
Mike Lambert <mlambert@gmail.com>
2016-04-21 00:22:31 +0800

    Refactor the function for more clarity in the process/flow.

--- common/login/SplashScreen.js
+++ common/login/SplashScreen.js
@@ -70,56 +70,72 @@ class SplashScreen extends React.Component {
 }
 export default connect(select)(SplashScreen);
 
+
+async function isLoggedOut() {
+  const accessToken = await AccessToken.getCurrentAccessToken();
+  return Boolean(accessToken);
+}
+
+async function isRecentlyLoggedIn() {
+  const accessToken = await AccessToken.getCurrentAccessToken();
+  if (accessToken != null) {
+    var howLongAgo = Math.round((Date.now() - accessToken.lastRefreshTime) / 1000);
+    return (howLongAgo < 60 * 60);
+  } else {
+    // This shouldn't happen, since we check isLoggedOut() before isRecentlyLoggedIn().
+    // But let's handle it correctly anyway.
+    return false;
+  }
+}
+
+async function refreshFullToken() {
+  // If we need to refresh, there's a lot of things that can go wrong:
+  // Errors refreshing, errors getting additional permissions, etc.
+  // If they happen, let's log them out and send them back in through the flow above.
+  try {
+    // First refresh the token. This returns a list of permissions approved/declined,
+    // which we don't care about using as such.
+    await AccessToken.refreshCurrentAccessTokenAsync();
+    // Let's grab the actual access token (which should now be cached from the refresh).
+    // This has an easier API to work with too.
+    const newAccessToken = await AccessToken.getCurrentAccessToken();
+    console.log('Refreshed Token result:', newAccessToken);
+    // Now check if this token has user_events permission (our most important permission)
+    // For awhile many iOS users were being approved without this permission due to a bug.
+    // So this requests they log in again to explicitly grab that permission.
+    //
+    // NOTE: We intentionally use != instead of !== due to the need to protect against undefined:
+    // described more in http://flowtype.org/docs/nullable-types.html
+    // This != fixes Flow, but then flags with ESLint!
+    if (newAccessToken != null && !newAccessToken.getPermissions().includes('user_events')) {
+      await loginOrLogout();
+    }
+  } catch (exc) {
+    // Something strange happened!
+    // Let's log them out, and send them back in from the top without a token.
+    // This effectively drops them back in the onboarding flow.
+    console.log('Exception refreshing or logging in:', exc);
+    LoginManager.logOut();
+  }
+}
+
 async function performLoginTransitions(dispatch: Dispatch) {
   // When they open the app, check for their existing FB token.
-  const accessToken = await AccessToken.getCurrentAccessToken();
-  // If they have none, they are logged out, so start the onboarding process.
-  if (!accessToken) {
+  if (isLoggedOut()) {
     console.log('Wait for onboarding!');
     return dispatch(loginStartOnboard());
+  // Now let's check how old the token is. We want to refresh old tokens,
+  // but not delay/block users who have recently refreshed.
+  } else if (isRecentlyLoggedIn()) {
+    console.log('Fresh access token, completing login!');
+    return dispatch(loginComplete());
   } else {
-    // Now let's check how old the token is. We want to refresh old tokens,
-    // but not delay/block users who have recently refreshed.
-    var howLongAgo = Math.round((Date.now() - accessToken.lastRefreshTime) / 1000);
-    if (howLongAgo < 60 * 60) {
-      console.log('Good access token, finishing login!');
-      return dispatch(loginComplete());
-    } else {
-      // If we need to refresh, there's a lot of things that can go wrong:
-      // Errors refreshing, errors getting additional permissions, etc.
-      // If they happen, let's log them out and send them back in through the flow above.
-      try {
-        // First refresh the token. This returns a list of permissions approved/declined,
-        // which we don't care about using as such.
-        await AccessToken.refreshCurrentAccessTokenAsync();
-        // Let's grab the actual access token (which should now be cached from the refresh).
-        // This has an easier API to work with too.
-        const newAccessToken = await AccessToken.getCurrentAccessToken();
-        console.log('Refreshed Token result:', newAccessToken);
-        // Now check if this token has user_events permission (our most important permission)
-        // For awhile many iOS users were being approved without this permission due to a bug.
-        // So this requests they log in again to explicitly grab that permission.
-        //
-        // NOTE: We intentionally use != instead of !== due to the need to protect against undefined:
-        // described more in http://flowtype.org/docs/nullable-types.html
-        // This != fixes Flow, but then flags with ESLint!
-        if (newAccessToken != null && !newAccessToken.getPermissions().includes('user_events')) {
-          await loginOrLogout();
-        }
-      } catch (exc) {
-        // Something strange happened!
-        // Let's log them out, and send them back in from the top without a token.
-        // This effectively drops them back in the onboarding flow.
-        console.log('Exception refreshing or logging in:', exc);
-        LoginManager.logOut();
-      }
-      // Okay, now we've either refreshed with a new valid authtoken, or we've logged the user out.
-      // Let's send them back into the top of this function,
-      // which will either start their onboarding process, or start their use of the app.
-      //
-      // NOTE: It's possible this will trigger an infinte loop, in strange cases that "Shouldn't happen"
-      return performLoginTransitions(dispatch);
-    }
+    refreshFullToken();
+    // Okay, now we've either refreshed with a new valid authtoken, or we've logged the user out.
+    // Let's send them back into the flow, which will start onboarding or start the main app.
+    //
+    // NOTE: It's possible this will trigger an infinte loop, in strange cases that "Shouldn't happen".
+    return performLoginTransitions(dispatch);
   }
 }
 
