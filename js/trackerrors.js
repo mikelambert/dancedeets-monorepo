0ad80a3136ef21e5a626d53f7d4b700c69618ace
Mike Lambert <mlambert@gmail.com>
2016-05-08 20:49:25 -0400

    Don't track errors on dev.

--- js/trackerrors.js
+++ js/trackerrors.js
@@ -2,6 +2,11 @@ import trackjs from 'react-native-trackjs';
 import crashlytics_reporter from 'react-native-crashlytics-reporter';
 
 export default function init() {
+  if (__DEV__) {
+    // Don't send exceptions from __DEV__, it's way too noisy!
+    // Live reloading and hot reloading in particular lead to tons of noise...
+    return;
+  }
   trackjs.init({token: '77a8a7079d734df7a94150f8f0d7e16f'});
   crashlytics_reporter.init();
 }
