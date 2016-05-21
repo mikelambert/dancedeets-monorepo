f109d4757987ce8e7532566d7d4b4075945bb5cf
Mike Lambert <mlambert@gmail.com>
2016-05-20 21:50:48 -0400

    Start supporting enabled state on android (in here, instead of in the native component)
    This necessitates managing the disabled tintColor ourselves, as well as force-updating
    the native componet to avoid (or correctly!) update its internal state in response
    to the state stored and maintained in this component.

--- js/ui/SegmentedControl.js
+++ js/ui/SegmentedControl.js
@@ -13,6 +13,7 @@ import {
   SegmentedControlIOS,
 } from 'react-native';
 
+import normalizeColor from 'normalizeColor';
 import SegmentedControlAndroid from 'react-native-segmented-android';
 
 type Props = {
@@ -23,6 +24,27 @@ type Props = {
   tintColor: string,
   style: any,
 };
+
+function disabledColor(color: string, multiplier: number = 0.5) {
+  const colorNumber = normalizeColor(color);
+  if (colorNumber == null) {
+    // No color, give up and return existing color
+    return color;
+  }
+  const colorComponents = [
+    ((colorNumber & 0xff000000) >> 24) + 256, // r
+    (colorNumber & 0x00ff0000) >> 16,         // g
+    (colorNumber & 0x0000ff00) >> 8,          // b
+  ];
+  const disabledColorComponents = colorComponents.map((d) => d * multiplier);
+  const disabledNumber = (
+    (disabledColorComponents[0] << 16) |
+    (disabledColorComponents[1] << 8)  |
+    (disabledColorComponents[2] << 0)
+  );
+  const disabledHex = '#' + ('000000' + disabledNumber.toString(16)).substr(-6);
+  return disabledHex;
+}
 
 export default class SegmentedControl extends React.Component {
   state: {
@@ -42,24 +64,41 @@ export default class SegmentedControl extends React.Component {
 
   constructor(props: Props) {
     super(props);
-
     this.state = {
       selectedIndex: this.props.defaultIndex,
     };
   }
 
   async onChange(index: number) {
+    // If we're not enabled, don't actually attempt to do anything
+    // This is important for Android, which doesn't support enabled natively,
+    // and so will still call onChange.
     const oldIndex = this.state.selectedIndex;
-    this.setState({selectedIndex: index});
+    if (!this.props.enabled) {
+      // Make sure we trigger an 'update' even though nothing should happen while we're disabled.
+      // Triggering forceUpdate() isn't sufficient, since it recognizes there is no "change"
+      // to our rendered stated (selectedIndex did not change), and so does not update the native control,
+      // which then ends up with an out-of-sync native selectedIndex relative to this.props.selectedIndex.
+      //
+      // By changing the state back-and-forth, we force a native update and re-render,
+      // which ensures the native component will point at selectedIndex.
+      this.setState({selectedIndex: index});
+      this.setState({selectedIndex: oldIndex});
+      return;
+    }
+    // Only we're enabled, let's try our update (and rolling back, if there were any failures)
     if (this.props.tryOnChange) {
       try {
         await this.props.tryOnChange(index, oldIndex);
       } catch (error) {
-        console.warn('Undoing SegmentedControl due to rrror setting RSVP:', error);
+        console.warn('Undoing SegmentedControl due to error calling tryOnChange:', error);
         this.setState({selectedIndex: oldIndex});
       }
+    } else {
+      this.setState({selectedIndex: index});
     }
   }
+
 
   render() {
     if (Platform.OS === 'ios') {
@@ -69,16 +108,17 @@ export default class SegmentedControl extends React.Component {
         enabled={this.props.enabled}
         selectedIndex={this.state.selectedIndex}
         onChange={(event) => this.onChange(event.nativeEvent.selectedSegmentIndex)}
-        tintColor={this.props.tintColor}
+        tintColor={this.props.tintColor} // iOS handles the disabled color automatically
       />;
     } else {
+      const tintColor = this.props.enabled ? this.props.tintColor : disabledColor(this.props.tintColor);
       return <SegmentedControlAndroid
         style={this.props.style}
         childText={this.props.values}
         orientation="horizontal"
         selectedPosition={this.state.selectedIndex}
         onChange={(event) => this.onChange(event.selected)}
-        tintColor={[this.props.tintColor, '#000000ff']}
+        tintColor={[tintColor, '#000000ff']}
       />;
     }
   }
