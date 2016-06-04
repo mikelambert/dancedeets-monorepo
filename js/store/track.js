764d0caef45b2b9fbb2395802910c62da74a7c5b
Mike Lambert <mlambert@gmail.com>
2016-06-03 18:54:31 -0700

    Grab all the necessary bits we need for Login/Logout and tracking against Mixpanel.

--- js/store/track.js
+++ js/store/track.js
@@ -26,6 +26,8 @@
 
 import Mixpanel from 'react-native-mixpanel';
 import {AppEventsLogger} from 'react-native-fbsdk';
+import { AccessToken } from 'react-native-fbsdk';
+import { performRequest } from '../api/fb';
 
 import type {Action} from '../actions/types';
 import type {Event} from '../events/models';
@@ -68,14 +70,37 @@ function trackWithEvent(eventName: string, event: Event) {
   track(eventName, extraParams);
 }
 
+async function setupPersonProperties(token: AccessToken) {
+  Mixpanel.identify(token.userID);
+
+  const user = await performRequest('GET', 'me', {fields: 'id,name,first_name,last_name,gender,locale,timezone,email,link'});
+  Mixpanel.set('$first_name', user.first_name);
+  Mixpanel.set('$last_name', user.last_name);
+  Mixpanel.set('FB Gender', user.gender);
+  Mixpanel.set('FB Locale', user.locale);
+  Mixpanel.set('FB Timezone', user.timezone);
+  Mixpanel.set('$email', user.email);
+
+  const now = new Date().toISOString().slice(0,19); // Trim off the fractional seconds from our ISO?UTC time
+  Mixpanel.set('Last Login', now);
+  Mixpanel.setOnce('$created', now);
+}
+
 export default function trackDispatches(action: Action): void {
   switch (action.type) {
-    case 'LOGGED_IN':
+    case 'LOGIN_LOGGED_IN':
+      // We must call identify *first*, before calling setDeviceToken() or login().
+      // This ensures the latter functions operate against the correct user.
+      // TODO: Retrieve push token
+      //Mixpanel.addPushDeviceToken(...);
+      setupPersonProperties(action.token);
       track('Login');
       break;
 
-    case 'LOGGED_OUT':
+    case 'LOGIN_LOGGED_OUT':
       track('Logout');
+      Mixpanel.removePushToken();
+      Mixpanel.reset();
       break;
 
     case 'VIEW_EVENT':
