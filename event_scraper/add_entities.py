e7a735557a03bfb80045b241a48058d82f0a7db3
Mike Lambert <mlambert@gmail.com>
2016-06-06 04:19:07 -0400

    When we add an event, defer the event source's scraping til later. This speeds up our event adding significantly (and avoids potential sources of 500s when adding events)

--- event_scraper/add_entities.py
+++ event_scraper/add_entities.py
@@ -38,34 +38,42 @@ def add_update_event(fb_event, fbl, creating_uid=None, visible_to_fb_uids=None, 
     e.visible_to_fb_uids = visible_to_fb_uids
 
     event_updates.update_and_save_fb_events([(e, fb_event)])
-    thing_db.create_source_from_event(fbl, e)
 
     if newly_created:
         logging.info("New event, publishing to twitter/facebook")
         deferred.defer(pubsub.eventually_publish_event, e.fb_event_id)
 
-    #DISABLE_ATTENDING
-    fb_event_attending = None
-    potential_event = potential_events.make_potential_event_without_source(e.fb_event_id, fb_event, fb_event_attending)
-    classified_event = event_classifier.get_classified_event(fb_event, potential_event.language)
-    if potential_event:
-        for source_id in potential_event.source_ids:
-            #STR_ID_MIGRATE
-            source_id = str(source_id)
-            s = thing_db.Source.get_by_key_name(source_id)
-            if not s:
-                logging.warning("Couldn't find source %s when updating event %s", source_id, e.fb_event_id)
-                continue
-            #TODO(lambert): doesn't handle the case of the match score increasing from <0 to >0 in the future
-            if not classified_event.is_dance_event():
-                s.num_false_negatives = (s.num_false_negatives or 0) + 1
-            s.num_real_events = (s.num_real_events or 0) + 1
-            s.put()
+    fbl.clear_local_cache()
+    deferred.defer(crawl_event_source, fbl, e.fb_event_id)
 
     # like the fb page?
     deferred.defer(like_event_admin_pages, e.fb_event_id)
 
     return e
+
+
+def crawl_event_source(fbl, event_id):
+    fb_event = fbl.get(fb_api.LookupEvent, event_id)
+    e = eventdata.DBEvent.get_by_id(fb_event['info']['id'])
+    thing_db.create_source_from_event(fbl, e)
+
+    # DISABLE_ATTENDING
+    fb_event_attending = None
+    potential_event = potential_events.make_potential_event_without_source(e.fb_event_id, fb_event, fb_event_attending)
+    classified_event = event_classifier.get_classified_event(fb_event, potential_event.language)
+    if potential_event:
+        for source_id in potential_event.source_ids:
+            # STR_ID_MIGRATE
+            source_id = str(source_id)
+            s = thing_db.Source.get_by_key_name(source_id)
+            if not s:
+                logging.warning("Couldn't find source %s when updating event %s", source_id, e.fb_event_id)
+                continue
+            # TODO(lambert): doesn't handle the case of the match score increasing from <0 to >0 in the future
+            if not classified_event.is_dance_event():
+                s.num_false_negatives = (s.num_false_negatives or 0) + 1
+            s.num_real_events = (s.num_real_events or 0) + 1
+            s.put()
 
 
 def like_event_admin_pages(db_event_id):
