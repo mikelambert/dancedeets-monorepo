212ad01145fafc94ecb5f8caa5e985f7bdbc43b4
Mike Lambert <mlambert@gmail.com>
2016-06-08 23:31:34 -0400

    fixup! Use a longer timeout on our geolocation.

--- js/ui/AutocompleteList.js
+++ js/ui/AutocompleteList.js
@@ -142,7 +142,7 @@ export default class AutocompleteList extends React.Component {
         this._disableRowLoaders();
         console.warn(error.message);
       },
-      {enableHighAccuracy: highAccuracy, timeout: 5 * 1000, maximumAge: 10 * 60 * 1000}
+      {enableHighAccuracy: highAccuracy, timeout: 10 * 1000, maximumAge: 10 * 60 * 1000}
     );
   }
 
