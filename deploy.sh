5d410d5d2139cbbf30e67ab315e01857f72a8d6d
Mike Lambert <mlambert@gmail.com>
2016-06-09 23:49:11 -0400

    Fix some deprecation warnings, which I think allow us to push again.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 # pip installs some library files that don't upload at all, so delete them here:
 rm -f "lib/setuptools/script (dev).tmpl"
 
-time yes | gcloud preview app deploy app.yaml --docker-build=remote "$@"
+time yes | gcloud preview app deploy app.yaml "$@"
 
 #DEFAULT_VERSION="$(gcloud preview app modules list | grep \* | awk '{print $2}')"
 #gcloud --verbosity info preview app deploy app.yaml --set-default
