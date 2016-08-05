e7e8e422b301999ab9d2877e7ceff0d62cbccdf5
Mike Lambert <mlambert@gmail.com>
2016-08-04 20:12:32 -0400

    Set up a fully initialized screen on click, with proper access token and everything. Need to do a better job of passing that token through, though.

--- js/ScreenshotSlideshow.js
+++ js/ScreenshotSlideshow.js
@@ -1,33 +1,99 @@
-import React from 'react';
+/**
+ * Copyright 2016 DanceDeets.
+ *
+ * @flow
+ */
 
+'use strict';
+
+import React from 'react';
 import {
   TouchableHighlight,
   View,
 } from 'react-native';
+import { connect } from 'react-redux';
+import type { Dispatch } from './actions/types';
+import {disableTracking} from './store/track';
+import { loginComplete } from './actions';
+import {
+  AccessToken,
+} from 'react-native-fbsdk';
+import {
+  disableWrites
+} from './api/dancedeets';
+import {
+  updateLocation,
+  performSearch,
+} from './actions';
+import {
+  injectIntl,
+  defineMessages,
+} from 'react-intl';
 
-export default class ScreenshotSlideshow extends React.Component {
+const messages = defineMessages({
+  locations: {
+    id: 'search.autocompleteLocations',
+    defaultMessage: 'New York City, United States\nLos Angeles, United States\nSan Francisco, United States\nWashington DC, United States\nLondon, United Kingdom\nParis, France\nTokyo, Japan\nTaipei, Taiwan\nSeoul, South Korea',
+    description: 'A list of locations that we should show in our autocomplete',
+  },
+});
+
+class _ScreenshotSlideshow extends React.Component {
+  state: {
+    page: number;
+  };
+
   constructor(props) {
     super(props);
     this.state = {
       page: 0,
     };
-    this.transitionPage = this.transitionPage.bind(this);
+    (this: any).transitionPage = this.transitionPage.bind(this);
   }
 
-  transitionPage() {
+  componentWillMount() {
+    disableTracking();
+    disableWrites();
+    this.props.transitionPage(this);
+  }
+
+  async setupListView(dispatch) {
+    // TODO: Fill out dummy token better
+    AccessToken.setCurrentAccessToken({
+      accessToken: '',
+      applicationID: '2347064084',
+      userID: '701004',
+    });
+    const token = await AccessToken.getCurrentAccessToken();
+    if (!token) {
+      return;
+    }
+    await dispatch(loginComplete(token));
+    const firstLocation = this.props.intl.formatMessage(messages.locations).split('\n')[0];
+    await dispatch(updateLocation(firstLocation));
+    await dispatch(performSearch());
+  }
+
+  async transitionPage(dispatch) {
     switch (this.state.page) {
       case 0:
-        console.log('0');
+        await this.setupListView(dispatch);
         break;
       case 1:
-        console.log('1');
+        //await this.setupEventView(dispatch);
+        break;
+      case 2:
+        //await this.setupAddEventView(dispatch);
+        break;
+      case :
+        //await this.setupProfileView(dispatch);
         break;
     }
     this.setState({page: this.state.page + 1});
   }
 
   render() {
-    return <View>
+    return <View style={{flex: 1}}>
       {this.props.children}
       <TouchableHighlight
         style={{
@@ -37,10 +103,20 @@ export default class ScreenshotSlideshow extends React.Component {
           left: 0,
           right: 0,
         }}
-        onPress={this.transitionPage}
+        onPress={() => this.props.transitionPage(this)}
         testID="mainButton">
         <View />
       </TouchableHighlight>
     </View>;
   }
 }
+export default connect(
+  (state) => {
+    return {
+      translatedEvents: state.translate.events,
+    };
+  },
+  (dispatch: Dispatch) => ({
+    transitionPage: (obj) => obj.transitionPage(dispatch),
+  }),
+)(injectIntl(_ScreenshotSlideshow));
