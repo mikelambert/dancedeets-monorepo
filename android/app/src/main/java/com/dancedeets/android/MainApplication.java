9c268c891fc6f1ce466990ef88eac8843d01cd8e
Mike Lambert <mlambert@gmail.com>
2016-09-17 02:01:43 -0400

    Add Firebase to our Android build.

--- android/app/src/main/java/com/dancedeets/android/MainApplication.java
+++ android/app/src/main/java/com/dancedeets/android/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.react.ReactApplication;
+import org.jall.reactnative.firebase.FirebasePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -47,6 +48,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                 new MainReactPackage(),
+                new FirebasePackage(),
                 new RCTNativeEnvPackage(BuildConfig.class),
                 new RCTLocalePackage(),
                 new CodePush(BuildConfig.CODEPUSH_KEY, MainApplication.this, BuildConfig.DEBUG),
