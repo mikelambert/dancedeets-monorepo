c1771248285f641005c0642f8dc548fb87b7a5ad
Mike Lambert <mlambert@gmail.com>
2016-09-20 02:52:42 -0400

    Add react-native-permissions package.

--- android/app/src/main/java/com/dancedeets/android/MainApplication.java
+++ android/app/src/main/java/com/dancedeets/android/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.react.ReactApplication;
+import com.joshblour.reactnativepermissions.ReactNativePermissionsPackage;
 import org.jall.reactnative.firebase.FirebasePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -48,6 +49,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                 new MainReactPackage(),
+            new ReactNativePermissionsPackage(),
                 new FirebasePackage(),
                 new RCTNativeEnvPackage(BuildConfig.class),
                 new RCTLocalePackage(),
