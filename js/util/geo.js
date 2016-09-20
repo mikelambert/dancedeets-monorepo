d1c3579dd3c990acce765d29e76bf62ec1d8ccf1
Mike Lambert <mlambert@gmail.com>
2016-09-20 02:53:08 -0400

    Rework our geo api to ask permission before making a call-with-timeout, so the user's popup dialog doesn't count towards the timeout.

--- js/util/geo.js
+++ js/util/geo.js
@@ -8,8 +8,9 @@ import type { Address } from '../events/formatAddress';
 import { Platform } from 'react-native';
 import Geocoder from '../api/geocoder';
 import { format } from '../events/formatAddress';
+import Permissions from 'react-native-permissions';
 
-export function getPosition() {
+function getCurrentPosition() {
   return new Promise((resolve, reject) => {
     const highAccuracy = Platform.OS == 'ios';
     navigator.geolocation.getCurrentPosition(resolve, reject,
@@ -18,7 +19,34 @@ export function getPosition() {
   });
 }
 
+export async function getPosition() {
+//  Permissions.openSettings();
+  if (Platform.OS === 'ios') {
+    const status = await Permissions.requestPermission('location');
+    if (status !== 'authorized') {
+      // No location permission. Let's ignore it for now since the app will work fine
+      // But if we ever change our mind, we can prompt and run:
+      // Permissions.openSettings();
+      throw new Error('No location permissions');
+    }
+    // Otherwise have authorized permissions now, let's get their location!
+  }
+  return await getCurrentPosition();
+}
+
 export async function getAddress() {
+  if (Platform.OS === 'ios') {
+    const status = await Permissions.requestPermission('location');
+    if (status !== 'authorized') {
+      // No location permission. Let's ignore it for now since the app will work fine
+      // But if we ever change our mind, we can prompt and run:
+      // Permissions.openSettings();
+      return '';
+    }
+    // Otherwise have authorized permissions now, let's get their location!
+  }
+  // This has atimeout, so we need to do the user action (requesting permissions) above
+  // to ensure this call doesn't unnecessarily time out.
   const position = await getPosition();
   const newCoords = { lat: position.coords.latitude, lng: position.coords.longitude };
   const address: Address = await Geocoder.geocodePosition(newCoords);
