109f45469b7a2bb0339edfc4dab75e06dbd8b937
Mike Lambert <mlambert@gmail.com>
2016-10-04 06:55:08 -0400

    Smooth out our Android launch screen, by making it a no-op that shows the android application background automatically. This essentially gives up on the nice cypher image as an android background image.

--- js/login/LoginFlow.js
+++ js/login/LoginFlow.js
@@ -7,6 +7,10 @@
 'use strict';
 
 import React from 'react';
+import {
+  Platform,
+  View,
+} from 'react-native';
 import LaunchScreen from './LaunchScreen';
 import OnboardingFlow from './OnboardingFlow';
 import { connect } from 'react-redux';
@@ -30,10 +34,20 @@ class LoginFlow extends React.Component {
   render() {
     if (this.props.isOnboarding) {
       return <OnboardingFlow />;
-    } else {
-      // This is used when we're starting up, before we know
-      // whether to drop them into the <OnboardingFlow/> process.
+    }
+
+    // This is used when we're starting up, before we know
+    // whether to drop them into the <OnboardingFlow/> process.
+
+    if (Platform.OS === 'ios') {
+      // On iOS, we can do some easy scale-to-fit-window on our launch screen,
+      // so let's use our good launch image that can match the LaunchScreen.xib.
       return <LaunchScreen />;
+    } else if (Platform.OS === 'android') {
+      // On Android, background drawables are limited, and we can really only center things.
+      // So instead, let's draw a no-op, and let the application background show through,
+      // which ends up being a continuation of our launch screen as the app loads.
+      return <View />;
     }
   }
 }
