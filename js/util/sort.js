61f3229310f08c0ccc5736b200d7d49073672aaf
Mike Lambert <mlambert@gmail.com>
2016-10-26 20:57:53 -0400

    Fix sorting function. Was dropping events that shared a start time. :(

--- js/util/sort.js
+++ js/util/sort.js
@@ -9,8 +9,13 @@
 export default function sort<T>(list: Array<T>, computeFunction: (value: T) => any) {
   const sortValues = {};
   for (let origValue of list) {
-    sortValues[computeFunction(origValue)] = origValue;
+    const key = computeFunction(origValue);
+    if (!sortValues[key]) {
+      sortValues[key] = [];
+    }
+    sortValues[key].push(origValue);
   }
-  const origSorted = Object.keys(sortValues).sort().map((sortValue) => sortValues[sortValue]);
+  const origSortedDeep = Object.keys(sortValues).sort().map((sortValue) => sortValues[sortValue]);
+  const origSorted = [].concat.apply([], origSortedDeep);
   return origSorted;
 }
