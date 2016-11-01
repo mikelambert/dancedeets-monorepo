24d79dafa4342d058d01acc22db8e188e71c0606
Mike Lambert <mlambert@gmail.com>
2016-11-01 15:04:38 -0400

    First pass at running npm start *and* gunicorn app at the same time on our servers, with a custom Dockerfile and runtime.
    Unfortunately, this doesn't yet start gunicorn properly. despite deriving from the python-compat Dockerfile:
    ./run-servers.sh: line 2: /env/bin/gunicorn: No such file or directory
    
    Maybe it's not in the env, or maybe the shell script is running differently? Need to investigate still.

--- appengine_config.py
+++ appengine_config.py
@@ -3,50 +3,7 @@ import os
 import sys
 sys.path = [os.path.join(os.path.dirname(__file__), 'lib')] + sys.path
 
-from hacks import fixed_jinja2  # noqa: ignore=E402
-from hacks import fixed_ndb  # noqa: ignore=E402
-from hacks import fixed_mapreduce_util  # noqa: ignore=E402
-from requests_toolbelt.adapters import appengine as appengine_adapter  # noqa: ignore=E402
-from requests.packages.urllib3.contrib import appengine as appengine_manager  # noqa: ignore=E402
-
-try:
-    from google.appengine.ext.vmruntime import middlewares
-except:
-    logging.error("Failed to import google.appengine.ext.vmruntime.middlewares")
-else:
-    middlewares.MAX_CONCURRENT_REQUESTS = 30 # Normally 501
-
-# Disabled for now
-fixed_ndb.patch_logging(0)
-
-# Fix our runaway mapreduces
-fixed_ndb.fix_rpc_ordering()
-
-# Improve jinja2 stacktraces
-fixed_jinja2.fix_stacktraces()
-
-# Fix mapreduce to not require a certain version
-fixed_mapreduce_util.patch_function()
-
-# Make requests work with AppEngine's URLFetch
-if appengine_manager.is_local_appengine():
-    appengine_adapter.monkeypatch()
-
-
-def webapp_add_wsgi_middleware(app):
-    # Disable appstats since it may be resulting in NDB OOM issues
-    # from google.appengine.ext.appstats import recording
-    # app = recording.appstats_wsgi_middleware(app)
-
-    # Clean up per-thread NDB memory "leaks", though don't try to finish all RPCs calls (which includes runaway iterators)
-    app = fixed_ndb.tasklets_toplevel(app)
-
-    # Should only use this in cases of serialized execution of requests in a multi-threaded processes.
-    # So setdeploy manually, and test from there. Never a live server, as it would be both broken *and* slow.
-    # from hacks import memory_leaks
-    # app = memory_leaks.leak_middleware(app)
-
-    return app
+os.environ['GAE_USE_SOCKETS_HTTPLIB'] = '1'
 
 # We don't need such real-time statistics (normally 1 second) on the mapreduce job.
 # More of an optimization to save on the associated database Get/Put every second.
--- main.py
+++ main.py
@@ -5,6 +5,61 @@ import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), 'lib'))
 
+
+prod_mode = 'SERVER_SOFTWARE' in os.environ and not os.environ['SERVER_SOFTWARE'].startswith('Dev')
+
+# Make python-twitter work in the sandbox (not yet sure about prod...)
+if not prod_mode:
+    from google.appengine.tools.devappserver2.python import sandbox
+    sandbox._WHITE_LIST_C_MODULES += ['_ssl']
+
+
+from hacks import fixed_jinja2  # noqa: ignore=E402
+from hacks import fixed_ndb  # noqa: ignore=E402
+from hacks import fixed_mapreduce_util  # noqa: ignore=E402
+from requests_toolbelt.adapters import appengine as appengine_adapter  # noqa: ignore=E402
+from requests.packages.urllib3.contrib import appengine as appengine_manager  # noqa: ignore=E402
+
+try:
+    from google.appengine.ext.vmruntime import middlewares
+except:
+    logging.error("Failed to import google.appengine.ext.vmruntime.middlewares")
+else:
+    middlewares.MAX_CONCURRENT_REQUESTS = 30 # Normally 501
+
+# Disabled for now
+fixed_ndb.patch_logging(0)
+
+# Fix our runaway mapreduces
+fixed_ndb.fix_rpc_ordering()
+
+# Improve jinja2 stacktraces
+fixed_jinja2.fix_stacktraces()
+
+# Fix mapreduce to not require a certain version
+fixed_mapreduce_util.patch_function()
+
+# Make requests work with AppEngine's URLFetch
+if appengine_manager.is_local_appengine():
+    appengine_adapter.monkeypatch()
+
+
+def webapp_add_wsgi_middleware(app):
+    # Disable appstats since it may be resulting in NDB OOM issues
+    # from google.appengine.ext.appstats import recording
+    # app = recording.appstats_wsgi_middleware(app)
+
+    # Clean up per-thread NDB memory "leaks", though don't try to finish all RPCs calls (which includes runaway iterators)
+    app = fixed_ndb.tasklets_toplevel(app)
+
+    # Should only use this in cases of serialized execution of requests in a multi-threaded processes.
+    # So setdeploy manually, and test from there. Never a live server, as it would be both broken *and* slow.
+    # from hacks import memory_leaks
+    # app = memory_leaks.leak_middleware(app)
+
+    return app
+
+
 logging.info("Begin modules")
 import webapp2
 from google.appengine.ext import ereporter
@@ -17,14 +72,6 @@ from google.appengine.ext.ndb import tasklets
 # http://bugs.python.org/issue7980
 # http://code-trick.com/python-bug-attribute-error-_strptime/
 import _strptime
-
-
-prod_mode = 'SERVER_SOFTWARE' in os.environ and not os.environ['SERVER_SOFTWARE'].startswith('Dev')
-
-# Make python-twitter work in the sandbox (not yet sure about prod...)
-if not prod_mode:
-    from google.appengine.tools.devappserver2.python import sandbox
-    sandbox._WHITE_LIST_C_MODULES += ['_ssl']
 
 # We import for the side-effects in adding routes to the wsgi app
 logging.info("Begin servlets")
