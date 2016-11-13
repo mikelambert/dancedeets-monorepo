0773cff42f9fffb0b9ecff0af085013c710d7473
Mike Lambert <mlambert@gmail.com>
2016-11-12 20:35:01 -0500

    Fix our local dev server script, now that we've moved to custom vms running an npm server.

--- server.sh
+++ server.sh
@@ -1,3 +1,3 @@
 ./killserver.sh
 cat app.yaml | sed 's/GAE_USE_SOCKETS_HTTPLIB/x_disabled_GAE_USE_SOCKETS_HTTPLIB/' > app-devserver.yaml
-dev_appserver.py app-devserver.yaml --storage_path ~/Projects/dancedeets-storage/ "$@"
+dev_appserver.py app-devserver.yaml --storage_path ~/Projects/dancedeets-storage/ --runtime=python-compat "$@"
