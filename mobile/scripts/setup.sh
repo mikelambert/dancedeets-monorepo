7496b31041683dd681657529b5b6e89281c18d0d
Mike Lambert <mlambert@gmail.com>
2016-11-30 18:53:04 -0500

    Set up the dancedeets-common symlink on mobile.

--- mobile/scripts/setup.sh
+++ mobile/scripts/setup.sh
@@ -8,3 +8,8 @@ npm install
 
 cd $BASE_DIR/ios
 pod install
+
+# Point directly to the adjacent common module, so we don't need to npm install it
+rm -rf $BASE_DIR/node_modules/dancedeets-common
+ln -s ../../common $BASE_DIR/node_modules/dancedeets-common
+
