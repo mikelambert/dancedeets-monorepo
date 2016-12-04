5789ffdc196ad74d96d533f933230b153e3c0e8b
Mike Lambert <mlambert@gmail.com>
2016-12-03 19:00:52 -0800

    Start properly generating an amp file off our text fixture event object. The event is barebones and doesn't exercise all of the event.jsx functionality, but it's a start!

--- server/amp/generate_amp_sources.py
+++ server/amp/generate_amp_sources.py
@@ -1,11 +1,14 @@
-import logging
+#!/usr/bin/python
+
 import sys
 sys.path.insert(0, 'lib')
 sys.path.insert(0, '/Users/lambert/google-cloud-sdk/platform/google_appengine')
 sys.path.insert(0, '/Users/lambert/google-cloud-sdk/lib/third_party')
 
+import logging
+import os
+from unittest import result
 from webtest import TestApp
-from webtest import utils
 
 from google.appengine.ext import testbed
 
@@ -19,15 +22,23 @@ app = TestApp(main.application)
 class TestEvent(unittest.TestCase):
     def runTest(self):
         event = fixtures.create_event()
-        logging.error(1)
-        result = app.get('/events/%s?amp=1' % event.fb_event_id)
-        logging.error(result)
+        r = app.get('/events/%s?amp=1' % event.fb_event_id)
+        path = os.path.join(os.path.dirname(__file__), './generated/%s.html' % event.fb_event_id)
+        f = open(path, 'w')
+        f.write(r.unicode_normal_body)
 
 def generateAmpPages():
     test = TestEvent()
     test.testbed = testbed.Testbed()
     test.testbed.activate()
-    test.run()
+    r = result.TestResult()
+    test.run(r)
+    if r.errors or r.failures:
+        logging.error('Error generating amp files:')
+        for test, error in r.errors:
+            logging.error('%s: %s', test, error)
+        for test, fail in r.failures:
+            logging.error('%s: %s', test, fail)
 
 if __name__ == '__main__':
     generateAmpPages()
