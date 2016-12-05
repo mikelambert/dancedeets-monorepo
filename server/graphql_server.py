5a8383a7d9b4ac19185ee2b4316e309084083358
Mike Lambert <mlambert@gmail.com>
2016-12-05 02:51:28 -0800

    Basic fixes for graphql server.

--- server/graphql_server.py
+++ server/graphql_server.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_graphql import GraphQL
 import graphene
-from graphene import relay
 import keys
 
 app = Flask(__name__)
@@ -11,9 +10,9 @@ app.secret_key = keys.get('flask_session_key')
 # http://dev.dancedeets.com:8080/graphql?query={rebels%20{name,ships(first:1%20after:%22YXJyYXljb25uZWN0aW9uOjA=%22){edges{cursor,node{name}}}}}
 #from starwars import schema
 
-schema = graphene.Schema(name='Nonexistant Schema')
+schema = graphene.Schema()
 class Query(graphene.ObjectType):
-    node = relay.NodeField()
+    hello = graphene.String(description='A typical hello world')
 
 schema.query = Query
 #schema.mutation = Mutation
