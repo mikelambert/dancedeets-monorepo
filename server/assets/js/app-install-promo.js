ecc185a67ca3b70afa6043c23f74ded003d7b828
Mike Lambert <mlambert@gmail.com>
2016-12-06 15:55:10 -0800

    Remove JS hacks and let our modal center naturally.

--- server/assets/js/app-install-promo.js
+++ server/assets/js/app-install-promo.js
@@ -2,16 +2,6 @@ const $ = require('jquery');
 
 /* center modal */
 function initAppPromos(window) {
-  function centerModals() {
-    $('.modal').each((/* i */) => {
-      const clone = $(this).clone().css('display', 'block').appendTo('body');
-      let top = Math.round((clone.height() - clone.find('.modal-content').height()) / 2);
-      top = top > 0 ? top : 0;
-      clone.remove();
-      $(this).find('.modal-content').css('margin-top', top);
-    });
-  }
-
   function hideAppPromo() {
     $.cookie('ap-closed', '1', { expires: 4 });
   }
@@ -20,9 +10,6 @@ function initAppPromos(window) {
   if ($('#app-install')) {
     const appUsed = $.cookie('ap-used');
     if (appUsed) {
-      $('.modal').on('show.bs.modal', centerModals);
-      $(window).on('resize', centerModals);
-
       const appPromoClosed = $.cookie('ap-closed');
       if (!appPromoClosed) {
         $('#app-install').modal({});
