a6fec713b82cc854bf744636796637d856e55e14
Mike Lambert <mlambert@gmail.com>
2016-12-08 18:27:02 -0500

    Fix lint warning.

--- server/assets/js/intl.js
+++ server/assets/js/intl.js
@@ -7,8 +7,13 @@ import {
 class _Message extends React.Component {
   props: {
     message: {
+      /* eslint-disable react/no-unused-prop-types */
+      // intlShape defines its formatMessage as func.isRequired,
+      // but we know that it actually takes a few required parameters
+      // which we include here to ensure our callers pass them in.
       id: string;
       defaultMessage: string;
+      /* eslint-enable react/no-unused-prop-types */
     };
     values?: Object;
 
