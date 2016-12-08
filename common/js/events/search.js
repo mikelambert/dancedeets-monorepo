4f48681cd4ff7bbb18e2f14d2011183849f26336
Mike Lambert <mlambert@gmail.com>
2016-12-07 16:27:18 -0800

    Clarify our Search API returns better.

--- common/js/events/search.js
+++ common/js/events/search.js
@@ -14,6 +14,7 @@ export type TimePeriod = 'UPCOMING' | 'ONGOING' | 'PAST' | 'ALL_FUTURE';
 export type SearchQuery = {
   location: string;
   keywords: string;
+  locale: string;
   timePeriod: TimePeriod;
 };
 
@@ -22,14 +23,29 @@ export type Onebox = {
   title: string;
 };
 
+type LatLong = {
+  latitude: number;
+  longitude: number;
+};
+
+// API Requests 1.x
 export type SearchResults = {
   onebox_links: Array<Onebox>;
   results: Array<Event>;
   query: SearchQuery;
 };
 
+// API Requests 2.0+
 export type NewSearchResults = {
   onebox_links: Array<Onebox>;
   results: Array<SearchEvent>;
   query: SearchQuery;
+  // These are technically in SearchResults too, but they weren't necessary there.
+  // They aren't necessary here (yet), but this helps document the schema.
+  title: string;
+  location: string;
+  location_box?: {
+    southwest: LatLong;
+    northeast: LatLong;
+  };
 };
