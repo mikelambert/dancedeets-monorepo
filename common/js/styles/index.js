6a7e7581954c5076e2b1eec569972ea47beb04f4
Mike Lambert <mlambert@gmail.com>
2016-12-18 03:37:31 -0500

    Add ids used in our URLs.

--- common/js/styles/index.js
+++ common/js/styles/index.js
@@ -25,42 +25,49 @@ export type Style = {
 
 export default {
   break: {
+    id: 'break',
     title: 'Bboy / Bgirl',
     thumbnail: require('./images/break.png'),
     width: 505,
     height: 512,
   },
   hiphop: {
+    id: 'hiphop',
     title: 'Hip-Hop',
     thumbnail: require('./images/hiphop.png'),
     width: 278,
     height: 512,
   },
   pop: {
+    id: 'pop',
     title: 'Popping',
     thumbnail: require('./images/pop.png'),
     width: 283,
     height: 512,
   },
   lock: {
+    id: 'lock',
     title: 'Locking',
     thumbnail: require('./images/lock.png'),
     width: 339,
     height: 512,
   },
   house: {
+    id: 'house',
     title: 'House Dance',
     thumbnail: require('./images/house.png'),
     width: 381,
     height: 512,
   },
   krump: {
+    id: 'krump',
     title: 'Krump',
     thumbnail: require('./images/krump.png'),
     width: 420,
     height: 512,
   },
   other: {
+    id: 'other',
     title: 'Other Styles',
     titleMessage: messages.otherStylesTitle,
     thumbnail: require('./images/other.png'),
