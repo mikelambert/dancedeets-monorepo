f9859810577a6007fc4f0ba0b7238c1afcd61e0a
Mike Lambert <mlambert@gmail.com>
2016-12-23 23:40:10 -0800

    Convert boogiezone over to a mindbody scraper. Let's us re-use existing functionality for fetching future weeks.

--- server/classes/scraper/spiders/la_boogiezone.py
+++ server/classes/scraper/spiders/la_boogiezone.py
@@ -1,16 +1,18 @@
-from .. import items
 
-class Boogiezone(items.HealCodeScraper):
+from .. import browser_scraper
+
+class Boogiezone(browser_scraper.MindBodyBrowserScraper):
     name = 'Boogiezone'
     latlong = (33.831684, -118.3177987)
     address = '1951 Carson Street, Torrance, CA 90501'
 
-    healcode_id = 'd914316d8e3'
+    mindbody_studio_id = 350
+    mindbody_tab_id = 7
 
     def _valid_item(self, item, row):
         if not self._street_style(item['style']):
             return False
-        if item['teacher'] == 'Class Cancelled':
+        if item['teacher'] == 'Cancelled Today':
             return False
 
         return True
