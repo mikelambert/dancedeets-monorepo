7fe15a42f659a453f000db50b2327797ca121088
Mike Lambert <mlambert@gmail.com>
2016-12-25 01:31:46 -0800

    Open FB events in the FB app.

--- mobile/js/events/uicomponents.js
+++ mobile/js/events/uicomponents.js
@@ -224,6 +224,22 @@ class _EventSource extends React.Component {
 
   onPress() {
     trackWithEvent('Open Source', this.props.event);
+    // Try opening the event in the facebook app
+    if (this.props.event.source.name == 'Facebook') {
+      if (Platform.OS == 'ios') {
+        if (Linking.canOpenURL('fb://')) {
+          // This URL format is supposedly current as of May 2016:
+          // http://stackoverflow.com/questions/34875501/opening-facebook-event-pages-in-the-facebook-app-using-swift
+          const sourceUrl = `fb://event?id=${this.props.event.id}`;
+          try {
+            Linking.openUrl(sourceUrl);
+          } catch (err) {
+            console.error('Error opening:', sourceUrl, ', with Error:', err);
+          }
+        }
+      }
+    }
+    // Otherwise just use the web browser
     const sourceUrl = this.props.event.source.url;
     try {
       Linking.openURL(sourceUrl);
