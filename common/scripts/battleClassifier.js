bcdc81bbfb346c22c6bb98cbc83976cad437c400
Mike Lambert <mlambert@gmail.com>
2017-01-01 03:23:07 -0500

    Add a few fixes, and the results of running on a variety of playlists. We'll need to move these into tests at some point.

--- common/scripts/battleClassifier.js
+++ common/scripts/battleClassifier.js
@@ -42,6 +42,9 @@ async function getPlaylistVideos(playlistId) {
 function getContestants(video) {
   const components = video.title.split(anyRoundRegex);
   const contestantString = components.filter(x => vsRegex.test(x))[0];
+  if (!vsRegex.test(contestantString)) {
+    console.error('Video:', video, 'does not contain a "vs"');
+  }
   const contestants = contestantString.split(vsRegex).map(x => x.trim());
   if (contestants.length !== 2) {
     console.error('Video:', video, 'found too many contestants:', contestants);
@@ -54,7 +57,10 @@ function getContestants(video) {
 function getUniqueMatchesForRound(battleVideos, roundRegex, index): Array<Match> {
   const roundMatchCount = 2 ** index;
   const roundVideos = battleVideos.filter(x => roundRegex.test(x.title));
-  if (roundVideos.length != roundMatchCount) {
+  if (!roundVideos.length) {
+    return [];
+  }
+  if (roundVideos.length > roundMatchCount) {
     console.error('Found too many videos for round!', roundVideos);
   }
   const seenContestants = [];
@@ -90,6 +96,9 @@ async function buildBracketFromPlaylist(playlistId) {
   let parentRoundContestantOrder = [];
   order.forEach((roundRegex, index) => {
     const foundMatches = getUniqueMatchesForRound(battleVideos, roundRegex, index);
+    if (!foundMatches.length) {
+      return;
+    }
     let sortedMatches = null;
     if (parentRoundContestantOrder.length) {
       sortedMatches = parentRoundContestantOrder.map(contestant => {
@@ -104,13 +113,49 @@ async function buildBracketFromPlaylist(playlistId) {
     console.log('round ', index);
     console.log(sortedMatches);
     bracket.matches.push(...sortedMatches);
-    parentRoundContestantOrder = [].concat(...sortedMatches.map(x => [x.first, x.second]));
+    parentRoundContestantOrder = [].concat(...sortedMatches.map(x => x ? [x.first, x.second] : [null, null]));
   });
   return bracket;
 }
 
+// Should we handle prelims/preliminaries/circle A/B/C/D/etc videos?
+// Should we handle judge demos too?
+// We should do a better job of handling EX extra rounds
+
+// PL02EtzYP5EDOsZyptcmO8G-aPwsOAu9pO works
+// PL02EtzYP5EDOznUxhAPUi7agKWhHeUlsV works
+// PL02EtzYP5EDP8ibrNzJqDhnh0x-R8l8Pi works
+// PL02EtzYP5EDNxn_TWy_QPg4CTOwyd_mVh works
+// PL02EtzYP5EDNXRVIYOmn9JE00s-MtFVy9 works
+// PL02EtzYP5EDNC-tR53BwZO-nbacCft-Rt works
+// PL02EtzYP5EDO1IHqw_ugVZywWYMIUufZi works
+// PL02EtzYP5EDNvxRLDTrv1gGq0VuCVetiH works
+// PL02EtzYP5EDPo8t0XnbIjKJsc-xajp9p_ works
+// PL02EtzYP5EDMXAyDtNBh-waUjty8Kc4jX works
+// PL02EtzYP5EDPTN3fd1fclx6S23TizWiKT works
+// PL02EtzYP5EDMe6_JOcNn-h8wED4AI8kP7 works
+
+// PL02EtzYP5EDPmjF2qXLnvxNdgDGMDZmUW unfortunately contains the a 1v1 and 2v2 on the same playlist with no differentiation besides contestant names
+// PL02EtzYP5EDO-McJJ-3zywQpKQUkbGlOH also seems to contain two parallel battles?
+
+// PL02EtzYP5EDMsNcj1ayyIvhaf17D3CeT0 capitalization differences for contestant's name between rounds
+// PL02EtzYP5EDNKWJfaSGyXqygewSynUl83 contestant name slightly different between rounds
+
+// PL02EtzYP5EDPVY9y_Fl4RyF8xpaIFWBib (we should strip out common dance event title...as it may contain 'FINAL')
+// PL02EtzYP5EDNdSMYOktajlXLemXd6TVHI
+// PL02EtzYP5EDNGw4TDNwyPnJ2lX0Ea3r9R
+// PL02EtzYP5EDPdK7fLFyyyzKlLqQ8iysus
+// PL02EtzYP5EDMa06kzeJnCvh5uQZ_znsuL has FINALS. also has capitalization difference on DANIEL daniel. and has confusing finals 1-5
+
+// PL02EtzYP5EDMjpUNvFH3ARZdO6hfdfNM0 some battles got taken-down (or not posted). can we deal with gaps?
+// PL02EtzYP5EDPkZQ3pItIt6u5NxsMQ2Ib0 'best16 circle' screws up the matching
+// PL02EtzYP5EDMq7lgawjIVDmXkYPYa0GTQ looks like a hot mess
+// PL02EtzYP5EDNijB1DqdOxmRli8uDmg0jk best16 is actually a five-way?
+// PL02EtzYP5EDN9B55sxhyGEg8z8MbJQeZa best16 is a four-way
+// PL02EtzYP5EDONwwNOaDzh7zpCEgsKK8Pn best16 is sometimes multi-way, sometimes 1v1
+
 async function run() {
-  const bracket = await buildBracketFromPlaylist('PL02EtzYP5EDOsZyptcmO8G-aPwsOAu9pO');
+  const bracket = await buildBracketFromPlaylist('PL02EtzYP5EDMe6_JOcNn-h8wED4AI8kP7');
   console.log(bracket);
 }
 
