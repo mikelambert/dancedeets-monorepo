04b988d6f2cf4b486b37105f7f95ebf8878f356b
Mike Lambert <mlambert@gmail.com>
2017-01-01 01:26:18 -0500

    Process and parse videos into a sorted list of brackets. Need a proper datastructure still, though.

--- common/scripts/battleClassifier.js
+++ common/scripts/battleClassifier.js
@@ -11,16 +11,19 @@ import {
   getUrl,
   YoutubeKey,
 } from './_youtube';
-import {
-  Bracket
+import type {
+  Bracket,
+  Match,
 } from '../../server/assets/js/bracketModels';
 
-const regexes = {
+const roundRegexes = {
   final: /\bfinal\b/i,
   top4: /\b(?:(?:semi|1\s*\/\s*2)\W?final|(?:best|top)\W?4)\b/i,
   top8: /\b(?:(?:quarter|qtr|1\s*\/\s*4)\W?final|(?:best|top)\W?8)\b/i,
   top16: /\b(?:(?:quarter|qtr|1\s*\/\s*4)\W?final|(?:best|top)\W?16)\b/i,
 };
+
+const anyRoundRegex = new RegExp(Object.values(roundRegexes).map(x => x.source).join('|'), 'i');
 
 const vsRegex = /\bv\W?[zs]\b|versus|\bx\b|\b×\b/i;
 
@@ -36,35 +39,70 @@ async function getPlaylistVideos(playlistId) {
   return playlistItemsJson;
 }
 
+function getContestants(video) {
+  const components = video.title.split(anyRoundRegex);
+  const contestantString = components.filter(x => vsRegex.test(x))[0];
+  const contestants = contestantString.split(vsRegex).map(x => x.trim());
+  if (contestants.length !== 2) {
+    console.error('Video:', video, 'found too many contestants:', contestants);
+  }
+  //TODO: HACK:
+  const finalContestants = contestants.map(x => x.replace(' EX', '').replace(/\(.*\)/, ''));
+  return finalContestants;
+}
+
+function getUniqueMatchesForRound(battleVideos, roundRegex, index): Array<Match> {
+  const roundMatchCount = 2 ** index;
+  const roundVideos = battleVideos.filter(x => roundRegex.test(x.title));
+  if (roundVideos.length != roundMatchCount) {
+    console.error('Found too many videos for round!', roundVideos);
+  }
+  const seenContestants = [];
+  const roundMatches = [];
+  roundVideos.forEach(video => {
+    const contestants = getContestants(video);
+    if (seenContestants.includes(contestants[0]) || seenContestants.includes(contestants[1])) {
+      return;
+    }
+    seenContestants.push(contestants[0]);
+    seenContestants.push(contestants[1]);
+    roundMatches.push({
+      youtubeId: video.videoId,
+      first: contestants[0],
+      second: contestants[1],
+    });
+  });
+  return roundMatches;
+}
+
 async function run() {
   const result = await getPlaylistVideos('PL02EtzYP5EDOsZyptcmO8G-aPwsOAu9pO');
   const battleVideos = result.items.map(x => ({
     title: x.snippet.title,
-    videoId: x.id,
+    videoId: x.snippet.resourceId.videoId,
   }));
 
-  const bracket = new Bracket();
-  const order = [regexes.final, regexes.top4, regexes.top8, regexes.top16];
-  order.forEach((roundRegex, index) => {
-    const expectedCount = 2 ** index;
-    const matchedVideos = battleVideos.filter(x => roundRegex.test(x.title));
-    if (matchedVideos.length != expectedCount) {
-      console.log(matchedVideos)
-    }
-    matchedVideos.forEach(video => {
-      const components = video.title.split(roundRegex);
-      const contestantString = components.filter(x => vsRegex.test(x))[0];
-      const contestants = contestantString.split(vsRegex).map(x => x.trim());
-      console.log(contestants);
+  const bracket: Bracket = {};
+  const order = [roundRegexes.final, roundRegexes.top4, roundRegexes.top8, roundRegexes.top16];
 
-      bracket.matches.push({
-        youtubeId: video.videoId,
-        first: contestants[0],
-        second: contestants[1],
+  let parentRoundContestantOrder = [];
+  order.forEach((roundRegex, index) => {
+    const foundMatches = getUniqueMatchesForRound(battleVideos, roundRegex, index);
+    let sortedMatches = null;
+    if (parentRoundContestantOrder.length) {
+      sortedMatches = parentRoundContestantOrder.map(contestant => {
+        const result = foundMatches.find(match => (
+          match.first === contestant || match.second === contestant
+        ));
+        return result;
       });
-    })
+    } else {
+      sortedMatches = foundMatches;
+    }
+    console.log('round ', index);
+    console.log(sortedMatches);
+    parentRoundContestantOrder = [].concat(...sortedMatches.map(x => [x.first, x.second]));
   });
-  console.log(JSON.stringify(bracket.matches));
 }
 
 run();
