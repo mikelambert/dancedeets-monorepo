c614cf62fc283bab2b5f6a74cb2730c03605bfd2
Mike Lambert <mlambert@gmail.com>
2017-01-28 18:14:51 +0900

    Add a geos docker for shape manipulation libraries. And update our build files.

--- server/docker-gae-py-js/build.sh
+++ server/docker-gae-py-js/build.sh
@@ -1,6 +1,7 @@
 cd `dirname $0`
-export LOCAL_IMAGE=mlambert/gae-py-js
-export REMOTE_IMAGE=gcr.io/dancedeets-hrd/gae-py-js
+export NAME=gae-py-js
+export LOCAL_IMAGE=mlambert/$NAME
+export REMOTE_IMAGE=gcr.io/dancedeets-hrd/$NAME
 docker build -t $LOCAL_IMAGE .
 docker tag $LOCAL_IMAGE $REMOTE_IMAGE
 gcloud docker -- push $REMOTE_IMAGE
--- server/docker-geos/build.sh
+++ server/docker-geos/build.sh
@@ -1,5 +1,5 @@
 cd `dirname $0`
-export NAME=gae-modules
+export NAME=gae-geos
 export LOCAL_IMAGE=mlambert/$NAME
 export REMOTE_IMAGE=gcr.io/dancedeets-hrd/$NAME
 docker build -t $LOCAL_IMAGE .
