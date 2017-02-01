1392bdbcdb175849dd2221c7a5fe48280af686a3
Mike Lambert <mlambert@gmail.com>
2017-02-01 21:50:48 +0900

    Add unique_attending mapreduce and fix things up again. Have it do stats/processing on all the data we have available.

--- server/logic/unique_attendees.py
+++ server/logic/unique_attendees.py
@@ -1,38 +1,58 @@
+import logging
+
 from mapreduce import mapreduce_pipeline
 
+import app
+import base_servlet
 import fb_api
 from util import fb_mapreduce
 
-def map_each_attendee(db_event):
-    fbl = fb_mapreduce.get_fblookup()
-    fbl.request(fb_api.LookupEventAttending, db_event.fb_event_id)
-    fbl.batch_fetch()
-    fb_event_attending = fbl.fetched_data(fb_api.LookupEventAttending, db_event.fb_event_id)
-    for attendee in fb_event_attending['attending']['data']:
-        yield (db_event.city_name, attendee['id'])
+BATCH_SIZE = 20
+def map_each_attendee(db_events):
+    db_events = [x for x in db_events if x.is_fb_event]
 
-def reduce_just_unique_attendees(city, all_attendees):
-    yield (city, len(set(all_attendees)))
+    fbl = fb_mapreduce.get_fblookup()
+    fbl.request_multi(fb_api.LookupEventAttending, [x.fb_event_id for x in db_events])
+    fbl.batch_fetch()
+
+    for db_event in db_events:
+        fb_event_attending = fbl.fetched_data(fb_api.LookupEventAttending, db_event.id)
+        if fb_event_attending['empty']:
+            return
+        for attendee in fb_event_attending['attending']['data']:
+            yield ('City: %s' % db_event.city_name, attendee['id'])
+            yield ('Country: %s' % db_event.country, attendee['id'])
+
+def reduce_just_unique_attendees(location, all_attendees):
+    logging.info('REDUCE: %s', location)
+    yield ('Total RSVPs in %s' % location, len(set(all_attendees)))
+    yield ('Unique Attendees in %s' % location, len(all_attendees))
 
 def mr_count_attendees_per_city(fbl):
-    mrp = mapreduce_pipeline.MapreducePipeline( 
+    mapper_params = {
+        'entity_kind': 'events.eventdata.DBEvent',
+        'handle_batch_size': BATCH_SIZE,
+    }
+    mapper_params.update(fb_mapreduce.get_fblookup_params(fbl, randomize_tokens=True))
+    mrp = mapreduce_pipeline.MapreducePipeline(
         'unique_attendees',
         'logic.unique_attendees.map_each_attendee',
         'logic.unique_attendees.reduce_just_unique_attendees',
         'mapreduce.input_readers.DatastoreInputReader',
         'mapreduce.output_writers.GoogleCloudStorageOutputWriter',
-        mapper_params={
-            'entity_kind': 'events.eventdata.DBEvent',
-            'fbl_fb_uid': fbl.fb_uid,
-            'fbl_access_token': fbl.access_token,
-            'fbl_allow_cache': fbl.allow_cache,
+        mapper_params=mapper_params,
+        reducer_params={
+            'output_writer': {
+                'mime_type': 'text/plain',
+                'bucket_name': 'dancedeets-hrd.appspot.com',
+            },
         },
-        output_writer={
-            'mime_type': 'text/plain',
-            'bucket_name': 'dancedeets-hrd.appspot.com',
-        },
-        shards=2,
+        shards=8,
     )
     mrp.start()
     return mrp
 
+@app.route('/tools/unique_attendees')
+class ExportSourcesHandler(base_servlet.BaseTaskFacebookRequestHandler):
+    def get(self):
+        mr_count_attendees_per_city(self.fbl)
