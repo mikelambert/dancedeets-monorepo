32a44527d983ddc399b9dd03e13403492a415d79
Mike Lambert <mlambert@gmail.com>
2017-02-17 16:45:55 +0900

    Handle crash in our language processor, due to malformed input.

--- server/util/language.py
+++ server/util/language.py
@@ -1,4 +1,5 @@
 import cld2
+import logging
 import re
 
 illegalChars = re.compile(u'[\x00-\x08\x0b\x0c\x0e-\x1F\uD800-\uDFFF\uFFFE\uFFFF]')
@@ -7,7 +8,12 @@ def detect(text):
     text = illegalChars.sub('', text)
 
     text = text.encode('utf-8')
-    isReliable, textBytesFound, details = cld2.detect(text)
+    try:
+        isReliable, textBytesFound, details = cld2.detect(text)
+    except:
+        logging.exception('Error processing text: %r', text)
+        return None
+
     if isReliable:
         # top language, get the language code:
         # details: (('ENGLISH', 'en', 95, 1736.0), ('Unknown', 'un', 0, 0.0), ('Unknown', 'un', 0, 0.0))
