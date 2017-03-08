246e52100bbfb8ffcefdca2e701d4b8cde905f62
Mike Lambert <mlambert@gmail.com>
2017-03-08 11:59:06 +0900

    Fix path.

--- server/nose.sh
+++ server/nose.sh
@@ -26,11 +26,11 @@ done
 # We use without-sandbox, so that we properly load all the modules/tests
 cat app.yaml | sed 's/runtime: vm/runtime: python27/' > app-nose.yaml
 # If our lib/ directory exists, manage it properly, otherwise assume the modules are all installed globally
-if [ -d lib ]; then
-	rm -rf lib/tests # this is pulled in via twilio, and messes with our excludes
-	MODULES=$(find lib -maxdepth 1 | grep -v info | cut -f2- -d/ | sed 's/\.pyc?/\$/' | sed 's/^/\^/' | paste -s -d "|" -)
+if [ -d lib-local ]; then
+	rm -rf lib-local/tests # this is pulled in via twilio, and messes with our excludes
+	MODULES=$(find lib-local -maxdepth 1 | grep -v info | cut -f2- -d/ | sed 's/\.pyc?/\$/' | sed 's/^/\^/' | paste -s -d "|" -)
 	EXCLUDE=--exclude="$MODULES"
 else
 	EXCLUDE=""
 fi
-PYTHONPATH=lib $COVERAGE_PREFIX `which nosetests` --with-gae --gae-application=app-nose.yaml $EXCLUDE ${NOSE_ARGS[@]}
+PYTHONPATH=lib-local:lib-both $COVERAGE_PREFIX `which nosetests` --with-gae --gae-application=app-nose.yaml $EXCLUDE ${NOSE_ARGS[@]}
