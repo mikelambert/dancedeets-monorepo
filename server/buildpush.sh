2521bad6f06b0b49add0096b89cfcc405c13caab
Mike Lambert <mlambert@gmail.com>
2017-03-08 16:34:07 +0900

    Oops, add in the buildpush.sh script (the one that's 2x faster!)

---
+++ server/buildpush.sh
@@ -0,0 +1,8 @@
+cd `dirname $0`
+export NAME=gae-app
+export LOCAL_IMAGE=mlambert/$NAME
+export REMOTE_IMAGE=gcr.io/dancedeets-hrd/$NAME
+time docker build -t $LOCAL_IMAGE .
+docker tag $LOCAL_IMAGE $REMOTE_IMAGE
+time gcloud docker -- push $REMOTE_IMAGE
+time gcloud app deploy --quiet --image-url $REMOTE_IMAGE
