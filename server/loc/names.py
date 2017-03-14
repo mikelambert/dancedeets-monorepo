280c47447460f1ba05d81be5e6d30c8a1852ba31
Mike Lambert <mlambert@gmail.com>
2017-03-14 14:01:53 +0900

    Fix test.

--- server/loc/names.py
+++ server/loc/names.py
@@ -3,6 +3,6 @@ import pycountry
 def get_country_name(country_code):
     try:
         full_country = pycountry.countries.get(alpha_2=country_code).name
-    except:
-        full_country = country_code
-    return full_country.split(',')[0]
+        return full_country.split(',')[0]
+    except KeyError:
+        return country_code
