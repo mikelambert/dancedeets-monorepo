b618497417c47510e658801dbfb4c7d193e9b1e2
Mike Lambert <mlambert@gmail.com>
2017-03-17 04:52:52 +0900

    Update the timestamp ranks to ensure they are less than 2**31. And also start passing in the language to the search index.

--- server/search/search.py
+++ server/search/search.py
@@ -276,6 +276,7 @@ class EventsIndex(index.BaseIndex):
         if not isinstance(db_event.start_time, datetime.datetime) and not isinstance(db_event.start_time, datetime.date):
             logging.error("DB Event %s start_time is not correct format: ", db_event.id, db_event.start_time)
             return None
+        timestamp = min(int(time.mktime(db_event.start_time.timetuple())), 2**32 - 1)
         doc_event = search.Document(
             doc_id=db_event.id,
             fields=[
@@ -294,8 +295,8 @@ class EventsIndex(index.BaseIndex):
                 # so we can promote them to users when we send out daily notifications.
                 search.NumberField(name='creation_time', value=int(time.mktime(db_event.creation_time.timetuple())) if db_event.creation_time else 0),
             ],
-            # language=XX, # We have no good language detection
-            rank=int(time.mktime(db_event.start_time.timetuple())),
+            language=db_event.json_props['language'],
+            rank=timestamp,
         )
         return doc_event
 
