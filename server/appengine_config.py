449fc7ca65d1273b4b36054339b77f609060fc9f
Mike Lambert <mlambert@gmail.com>
2017-03-21 17:47:26 +0900

    Faster pipeline restarts on push.

--- server/appengine_config.py
+++ server/appengine_config.py
@@ -27,7 +27,7 @@ mapreduce_SHARD_MAX_ATTEMPTS = 1000
 # This is the max-timeout we wait before giving up.
 # Normally it'd check the logs-API to verify, but we run on managed VMs where that doesn't exist
 # Without this, it'd wait 10.5 minutes to restart the mapreduce, but we should do much better...
-mapreduce__MAX_LEASE_DURATION_SEC = mapreduce__SLICE_DURATION_SEC * 1.5
+mapreduce__MAX_LEASE_DURATION_SEC = mapreduce__SLICE_DURATION_SEC * 1.2
 
 appstats_MAX_STACK = 25
 appstats_MAX_REPR = 100
