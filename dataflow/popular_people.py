c76bc55b1300bf0980674586015d45e98911fe13
Mike Lambert <mlambert@gmail.com>
2017-03-23 15:27:01 +0900

    Okay, finish up the rest of this mapreduce (the reduce side, as well as all the testing). It seems to be producing good valid data!
    Want to add some counters, and then need to productionize it.

--- dataflow/popular_people.py
+++ dataflow/popular_people.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import os
 import random
 import site
 import sys
@@ -11,6 +12,9 @@ pip install google-cloud-datastore --user (cant use lib/)
 
 python -m popular_people --log=DEBUG
 """
+
+# Hack to fix serialization issues
+os.environ['GOOGLE_CLOUD_DISABLE_GRPC'] = 'true'
 
 site.addsitedir('lib')
 
@@ -78,31 +82,33 @@ def styles():
 #TODO: Use Constants
 STYLES_SET = set(x.index_name for x in styles())
 
+TOP_N = 100
+
 def ConvertToEntity(element):
     return helpers.entity_from_protobuf(element)
 
-def CountableEvent(event):
+def CountableEvent(db_event):
     # Don't use auto-events to train...could have a runaway AI system there!
     #TODO: Use Constants
-    if ':' in event.key.name:
-        namespace = event.key.name.split(':')[0]
+    if ':' in db_event.key.name:
+        namespace = db_event.key.name.split(':')[0]
     else:
         namespace = 'FB'
-    if namespace == 'FB' and event['creating_method'] != 'CM_AUTO_ATTENDEE':
-        yield event
+    if namespace == 'FB' and db_event['creating_method'] != 'CM_AUTO_ATTENDEE':
+        fb_event = json.loads(db_event['fb_event'])
+        if not fb_event['empty']:
+            yield db_event, fb_event
 
-def GetEventAndAttending(event):
-    client = datastore.Client()
-    key = client.key('FacebookCachedObject', '701004.%s.OBJ_EVENT_ATTENDING' % event.key.name)
+def GetEventAndAttending((db_event, fb_event), client):
+    key = client.key('FacebookCachedObject', '701004.%s.OBJ_EVENT_ATTENDING' % db_event.key.name)
     fb_event_attending_record = client.get(key)
-    if 'json_data' in fb_event_attending_record:
+    if fb_event_attending_record and 'json_data' in fb_event_attending_record:
         fb_event_attending = json.loads(fb_event_attending_record['json_data'])
         if not fb_event_attending['empty']:
-            yield event, fb_event_attending
+            yield db_event, fb_event, fb_event_attending
 
-def CountPeople((db_event, fb_event_attending)):
+def CountPeople((db_event, fb_event, fb_event_attending)):
     # Count admins
-    fb_event = json.loads(db_event['fb_event'])
     fb_info = fb_event['info']
     admins = fb_info.get('admins', {}).get('data')
     if not admins:
@@ -140,7 +146,7 @@ def track_person(person_type, db_event, person, count_once_per=None):
 
     people_info = {
         'count_once_per': count_once_per,
-        'person': person,
+        'person': '%s: %s' % (person['id'], person.get('name')),
     }
     # Not using db_event.nearby_city_names since it's way too slow.
     # And we just search-many-cities on lookup time.
@@ -151,7 +157,7 @@ def track_person(person_type, db_event, person, count_once_per=None):
             'city': city,
         }
         yield (key, people_info)
-        for category in STYLES_SET.intersection(db_event['auto_categories']):
+        for category in STYLES_SET.intersection(db_event.get('auto_categories', [])):
             key = {
                 'person_type': person_type,
                 'category': category,
@@ -160,19 +166,32 @@ def track_person(person_type, db_event, person, count_once_per=None):
             yield (key, people_info)
 
 def CountPeopleInfos((key, people_infos)):
-    print key, people_infos
-    return
     counts = {}
-    for full_person_name in people_infos:
-        count_once_per, person_name = full_person_name.split(': ', 1)
+    for person_info in people_infos:
+        person_name = person_info['person']
         if person_name in counts:
-            counts[person_name].add(count_once_per)
+            counts[person_name].add(person_info['count_once_per'])
         else:
-            counts[person_name] = set([count_once_per])
-    count_list = [(person_json, len(unique_organizers)) for (person_json, unique_organizers) in counts.items()]
+            counts[person_name] = set([person_info['person']])
+    count_list = [[person_json, len(unique_organizers)] for (person_json, unique_organizers) in counts.iteritems()]
     # count_list is [['id: name', count_of_unique_organizers], ...]
     sorted_counts = sorted(count_list, key=lambda kv: -kv[1])
-    return sorted_counts
+    return (key, sorted_counts[:TOP_N])
+
+def BuildPeopleRanking((key, top_n_counts), client):
+    key_name = '%s: %s: %s' % (key['person_type'], key['city'], key['category'])
+    db_key = client.key('PeopleRanking', key_name)
+    ranking = datastore.Entity(key=db_key)
+
+    ranking['person_type'] = key['person_type']
+    ranking['city'] = key['city']
+    ranking['category'] = key['category']
+    ranking['top_people_json'] = top_n_counts
+    return ranking
+
+def ConvertFromEntity(entity):
+    print entity
+    return helpers.entity_to_protobuf(entity)
 
 def read_from_datastore(project, pipeline_options):
     """Creates a pipeline that reads entities from Cloud Datastore."""
@@ -183,30 +202,25 @@ def read_from_datastore(project, pipeline_options):
 
     run_locally = True
     if run_locally:
-        q.key_filter(client.key('DBEvent', '9999'), '>')
+        q.key_filter(client.key('DBEvent', '99'), '>')
         q.key_filter(client.key('DBEvent', 'A'), '<')
 
-    # Read entities from Cloud Datastore into a PCollection.
-    events = (p
+    # Set up our map/reduce pipeline
+    (p
         | 'read from datastore' >> ReadFromDatastore(project, query._pb_from_query(q))
         | 'convert to entity' >> beam.Map(ConvertToEntity)
-    )
-
-    # Count the occurrences of each word.
-    admin_attendees = (events
+        # Find the events we want to count, and expand all the admins/attendees
         | 'filter events' >> beam.FlatMap(CountableEvent)
-        | 'load fb attending' >> beam.FlatMap(GetEventAndAttending)
+        | 'load fb attending' >> beam.FlatMap(GetEventAndAttending, client)
         | 'count people' >> beam.FlatMap(CountPeople)
+        # Group them and count most popular people per city-category
+        | 'group by city-category' >> beam.GroupByKey()
+        | 'count by city-category' >> beam.Map(CountPeopleInfos)
+        # And save it all back to the database
+        | 'generate database record' >> beam.Map(BuildPeopleRanking, client)
+        | 'convert from entity' >> beam.Map(ConvertFromEntity)
+        #| 'write to datastore' >> WriteToDatastore(client.project)
     )
-
-    counted_admin_attendees = (admin_attendees
-        | 'group words' >> beam.GroupByKey()
-        | 'count words' >> beam.FlatMap(CountPeopleInfos)
-    )
-
-    # Write the output using a "Write" transform that has side effects.
-    # pylint: disable=expression-not-assigned
-    counted_admin_attendees | 'write' >> beam.io.WriteToText(file_path_prefix='/Users/lambert/test/', num_shards=0)
 
     # Actually run the pipeline (all operations above are deferred).
     result = p.run()
