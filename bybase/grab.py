106072de1f1c01e125fb0385e83319b79521bc75
Mike Lambert <mlambert@gmail.com>
2017-04-03 14:45:09 +0900

    Okay, let's rework the grab.py script to work around issues with server-errors on some bad event. We try to back-off to get as many events as we can before the broken event. Then we choose a post-bad-time event to fetch everything afterwards.

--- bybase/grab.py
+++ bybase/grab.py
@@ -14,32 +14,44 @@ first_data = 'client_secret=EEEgyMhYSPbiH1daC4G4KctYE7Tlq3&client_id=7xY6XoCUj61
 data = json.loads(urllib2.urlopen(first_url, first_data).read())
 print data
 
-all_events = []
-last_id = None
-page_size = 1000
-while True:
-    args = {
-        'page_size': page_size,
-        'access_token': data['access_token'],
-    }
-    if last_id:
-        args['after_id'] = last_id
-    url = 'http://185.7.80.229/web/wp-json/bybase/v1/events?%s' % urllib.urlencode(args)
-    print url
-    try:
-        event_data = urllib2.urlopen(url, timeout=60).read()
-    except urllib2.HTTPError as e:
-        print 'Got Error:', e
-        page_size = page_size / 3
-        if page_size == 0:
+def get_json(last_id=None):
+    all_events = []
+    page_size = 1000
+    while True:
+        args = {
+            'page_size': page_size,
+            'access_token': data['access_token'],
+        }
+        if last_id:
+            args['after_id'] = last_id
+        url = 'http://185.7.80.229/web/wp-json/bybase/v1/events?%s' % urllib.urlencode(args)
+        print url
+        try:
+            event_data = urllib2.urlopen(url, timeout=60).read()
+        except urllib2.HTTPError as e:
+            print 'Got Error:', e
+            new_page_size = page_size / 3
+            if new_page_size == 0 and page_size > 1:
+                page_size = 1
+            else:
+                page_size = new_page_size
+            if page_size == 0:
+                break
+            continue
+        events = json.loads(event_data)
+        if not events:
             break
-        continue
-    events = json.loads(event_data)
-    if not events:
-        break
-    last_id = events[-1]['id']
-    all_events.extend(events)
+        last_id = events[-1]['id']
+        all_events.extend(events)
+    return all_events, page_size != 0
+
+all_events, success = get_json()
+if not success:
+    result, success = get_json(11530)
+    all_events.extend(result)
 
 d = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-open('events-%s.json' % (d), 'w').write(json.dumps(all_events))
+filename = 'events-%s.json' % (d)
+print 'Filename:', filename
+open(filename, 'w').write(json.dumps(all_events))
 
