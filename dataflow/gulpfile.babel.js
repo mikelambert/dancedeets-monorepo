628105b4a428f456a301cdb7eac85864f43184ad
Mike Lambert <mlambert@gmail.com>
2017-04-12 03:10:08 +0900

    Start passing in job names, yay.

--- dataflow/gulpfile.babel.js
+++ dataflow/gulpfile.babel.js
@@ -27,7 +27,8 @@ const sdk = 'beam-master/sdks/python/dist/apache-beam-0.7.0.dev0.tar.gz';
 const bucket = 'gs://dancedeets-hrd.appspot.com';
 
 function remoteJob(filename) {
-  return $.shell.task([`/usr/local/bin/python -m ${filename} --log=DEBUG --project dancedeets-hrd --job_name=${filename} --runner DataflowRunner --staging_location ${bucket}/staging --temp_location ${bucket}/temp --output ${bucket}/output --sdk_location ${sdk} --setup_file ./setup.py --num_workers=100`]);
+  const job_name = filename.replace(/[^-a-z0-9]/, '-');
+  return $.shell.task([`/usr/local/bin/python -m ${filename} --log=DEBUG --project dancedeets-hrd --job_name=${job_name} --runner DataflowRunner --staging_location ${bucket}/staging --temp_location ${bucket}/temp --output ${bucket}/output --sdk_location ${sdk} --setup_file ./setup.py --num_workers=100`]);
 }
 
 function localJob(filename) {
