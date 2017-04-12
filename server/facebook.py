118fcdae44ccaae3f90d9d02e6e1d697f624b2b6
Mike Lambert <mlambert@gmail.com>
2017-04-12 13:36:41 +0900

    First pass at fixing up our FB login, which broke two weeks ago (unannounced changes on FB side, I think)

--- server/facebook.py
+++ server/facebook.py
@@ -25,6 +25,7 @@ JavaScript SDK at http://github.com/facebook/connect-js/.
 
 import base64
 import cgi
+import datetime
 import hmac
 import json
 import logging
@@ -106,99 +107,41 @@ def parse_signed_request_cookie(cookies):
     return parse_signed_request(cookies.get("fbsr_" + FACEBOOK_CONFIG['app_id'], ""), FACEBOOK_CONFIG['secret_key'])
 
 def get_user_from_cookie(cookies):
-    """Parses the cookie set by the official Facebook JavaScript SDK.
+    app_id = FACEBOOK_CONFIG['app_id']
+    logging.info(cookies)
+    access_token = cookies.get("user_token_" + app_id, "")
+    if not access_token:
+        return None
 
-    cookies should be a dictionary-like object mapping cookie names to
-    cookie values.
+    result = extend_access_token(access_token)
+    fb_cookie_data = parse_signed_request_cookie(cookies)
+    if 'user_id' in fb_cookie_data:
+        result['uid'] = fb_cookie_data['user_id']
+    print result
+    return result
 
-    If the user is logged in via Facebook, we return a dictionary with the
-    keys "uid" and "access_token". The former is the user's Facebook ID,
-    and the latter can be used to make authenticated requests to the Graph API.
-    If the user is not logged in, we return None.
-
-    Download the official Facebook JavaScript SDK at
-    http://github.com/facebook/connect-js/. Read more about Facebook
-    authentication at http://developers.facebook.com/docs/authentication/.
-    """
+def extend_access_token(access_token):
     app_id = FACEBOOK_CONFIG['app_id']
     app_secret = FACEBOOK_CONFIG['secret_key']
 
-    cookie = cookies.get("fbsr_" + app_id, "")
-    if not cookie:
-        return None
-
-    try:
-        int(cookie)
-        # if it's an int, it's likely a cookie from after the user did FB.logout
-        return None
-    except ValueError:
-        # if it's not an int, it's likely to be our encoded json string we want
-        pass
-
-    # TODO(lambert): LOGIN: If we pass in the access_token from the client, we dont need to query to get the code here...
-    response = parse_signed_request(cookie, app_secret)
-    import logging
-    import datetime
-    logging.info("cookie response is %r", response)
-    if not response:
-        return None
-
     args = dict(
-        code = response['code'],
         client_id = app_id,
         client_secret = app_secret,
-        redirect_uri = '',
+        grant_type = 'fb_exchange_token',
+        fb_exchange_token = access_token
     )
-
     url = "https://graph.facebook.com/oauth/access_token?" + urllib.urlencode(args)
     file = urllib.urlopen(url)
     try:
         token_response = file.read()
     finally:
         file.close()
-    parsed_response = cgi.parse_qs(token_response)
-    logging.info("token response %r", parsed_response)
-    # This happens when we have an infinite duration token, and for some reason FB doesn't want to return a token here?
-    # I have an infinite duration token when the manage_pages permission bit is set (as part of giving access to the FB page to post)
-    # We return what we can (a user id), and rely on the calling page to fill in the remaining data using the user's existing token
-    if parsed_response == {}:
-        raise AlreadyHasLongLivedToken()
+    parsed_response = json.loads(token_response)
+    logging.info("token extension response: %r", parsed_response)
 
-    expires_time = None
-    if 'access_token' in parsed_response:
-      access_token = parsed_response["access_token"][-1]
-      if 'expires' in parsed_response:
-          expires_time = datetime.datetime.now() + datetime.timedelta(seconds=int(parsed_response["expires"][-1]))
-    else:
-      logging.error("No access_token in first parsed_response: %s", parsed_response)
-      access_token = None
-
-    if access_token:
-        args = dict(
-            client_id = app_id,
-            client_secret = app_secret,
-            grant_type = 'fb_exchange_token',
-            fb_exchange_token = access_token
-        )
-        url = "https://graph.facebook.com/oauth/access_token?" + urllib.urlencode(args)
-        file = urllib.urlopen(url)
-        try:
-            token_response = file.read()
-        finally:
-            file.close()
-        parsed_response = cgi.parse_qs(token_response)
-        logging.info("token response #2 %r", parsed_response)
-        if 'access_token' in parsed_response:
-          access_token = parsed_response["access_token"][-1]
-          if 'expires' in parsed_response:
-              expires_time = datetime.datetime.now() + datetime.timedelta(seconds=int(parsed_response["expires"][-1]))
-        else:
-          logging.error("No access_token in second parsed_response: %s", parsed_response)
-
-    # TODO(lambert): LOGIN: Do a sanity check that this access token belongs to this user? Every time??
-    return dict(
-        uid = response["user_id"],
-        access_token = access_token,
-        access_token_expires = expires_time,
-    )
+    result = {}
+    result['access_token'] = parsed_response["access_token"]
+    if 'expires_in' in parsed_response:
+        result['access_token_expires'] = datetime.datetime.now() + datetime.timedelta(seconds=int(parsed_response["expires_in"]))
+    return result
 
