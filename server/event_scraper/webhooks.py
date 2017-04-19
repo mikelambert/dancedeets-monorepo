44e5e046a134609a83356f995f2f1b2484f2f18e
Mike Lambert <mlambert@gmail.com>
2017-04-19 12:37:38 +0900

    Use the new v2.9 webhook features that allow the actual updated event-id to be sent in the user's webhook, so we don't need to fetch the user's data (or rely on the user's live access token)

--- server/event_scraper/webhooks.py
+++ server/event_scraper/webhooks.py
@@ -1,3 +1,5 @@
+import hashlib
+import hmac
 import json
 import logging
 import pprint
@@ -7,14 +9,16 @@ import webapp2
 import app
 import facebook
 import fb_api
-import hashlib
-import hmac
 import keys
-from . import potential_events_reloading
 from users import users
+from . import event_pipeline
+from . import potential_events
+from . import potential_events_reloading
+from . import thing_db
 
 
 # curl 'http://dev.dancedeets.com:8080/webhooks/user' --data '{"object": "user","entry":[{"changed_fields":["events"],"id": "701004"}]}'
+# curl 'http://dev.dancedeets.com:8080/webhooks/user' --data '{"entry": [{"time": 1492571621, "changes": [{"field": "events", "value": {"event_id": 4444444444, "verb": "accept"}}], "id": "701004", "uid": "701004"}], "object": "user"}'
 @app.route('/webhooks/user')
 class WebhookPageHandler(webapp2.RequestHandler):
     def requires_login(self):
@@ -44,16 +48,29 @@ class WebhookPageHandler(webapp2.RequestHandler):
         logging.info("Request body: %r", self.request.body)
         escaped_body = urllib.unquote_plus(self.request.body.strip('='))
         json_body = json.loads(escaped_body)
-        logging.info("json_request: %r", pprint.pformat(json_body))
+        logging.info("json_request: %s", json.dumps(json_body))
         if json_body['object'] == 'user':
-            user_ids = [x['id'] for x in json_body['entry'] if 'events' in x['changed_fields']]
-            changed_users = users.User.get_by_ids(user_ids)
-            for user_id, user in zip(user_ids, changed_users):
+            for entry in json_body['entry']:
+                user_id = entry['uid']
+                user = users.User.get_by_id(user_id)
                 if not user:
                     logging.warning("Received webhook call for user id %s, but no User object.", user_id)
-                    continue
+                event_ids = []
+                for change in entry['changes']:
+                    if change['field'] == 'events':
+                        event_ids.append(unicode(change['value']['event_id']))
+
+                discovered_list = []
+                for event_id in event_ids:
+                    de = potential_events.DiscoveredEvent(event_id, None, thing_db.FIELD_INVITES)
+                    de.source_id = user_id
+                    discovered_list.append(de)
+
+                if not user:
+                    user = users.User.get_by_id('701004')
+                fbl = user.get_fblookup()
                 try:
-                    potential_events_reloading.load_potential_events_for_user(user)
+                    event_pipeline.process_discovered_events(fbl, discovered_list)
                 except fb_api.ExpiredOAuthToken:
                     logging.warning("Found ExpiredOAuthtoken when reloading events for User: %s", user.fb_uid)
                     logging.warning("User's Clients: %s, Last Login: %s, Expiration: %s", user.clients, user.last_login_time, user.fb_access_token_expires)
