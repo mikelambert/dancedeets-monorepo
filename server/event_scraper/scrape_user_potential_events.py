6f67e38e42ec03aacc554902889de6478e11f799
Mike Lambert <mlambert@gmail.com>
2017-04-19 12:06:13 +0900

    No need to double-filter (which was broken anyway). Rely on the event_pipeline to filter down our list of ids.

--- server/event_scraper/scrape_user_potential_events.py
+++ server/event_scraper/scrape_user_potential_events.py
@@ -17,16 +17,8 @@ def get_potential_dance_events(fbl, user_id, fb_user_events):
     source.put()
 
     discovered_list = [potential_events.DiscoveredEvent(x, source, thing_db.FIELD_INVITES) for x in event_ids]
-    discovered_list_to_process = event_pipeline.get_unprocessed_discovered_events(discovered_list)
-    logging.info("For user id %s, removing discovered, leaves %s events to process", user_id, len(discovered_list_to_process))
 
-    # Only do 150 events a time so we don't blow up memory
-    # TODO(lambert): Maybe do something whereby we don't load fb-events for things we just need to add a source-id on, and only load for things without a potential-event at all. Will need to change the make_potential_event_with_source() API around though...perhaps it's about time.
-    if len(event_ids) > 150:
-        logging.info("Trimming to 150 events so we don't blow through memory, we'll get the rest of the events next time we run...")
-        event_ids = sorted(event_ids)[:150]
-
-    logging.info("Going to look up %s events", len(event_ids))
+    logging.info("Going to filter-and-lookup %s events", len(discovered_list))
 
     event_pipeline.process_discovered_events(fbl, discovered_list)
 
