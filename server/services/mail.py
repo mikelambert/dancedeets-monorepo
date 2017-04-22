58efce65d8cbccf090133d20a16294f309c47f76
Mike Lambert <mlambert@gmail.com>
2017-04-22 10:51:41 +0900

    Better logging and error handling in our mandrill sending

--- server/services/mail.py
+++ server/services/mail.py
@@ -1,3 +1,4 @@
+import logging
 import mandrill
 
 import keys
@@ -5,5 +6,10 @@ import keys
 mandrill_client = mandrill.Mandrill(keys.get('mandrill_api_key'))
 
 def send_message(message):
-    result = mandrill_client.messages.send(message=message, async=False)
+    try:
+        result = mandrill_client.messages.send(message=message, async=False)
+        logging.info('Message Contents: %s', message)
+        logging.info('Message Result: %s', result)
+    except mandrill.Error, e:
+         logging.error('A mandrill error occurred: %s: %s', e.__class__, e)
     return result
