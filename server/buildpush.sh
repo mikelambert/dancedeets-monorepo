3e15293ba5f2bfb938dadbfe6560a2262bc78bf0
Mike Lambert <mlambert@gmail.com>
2017-04-23 17:01:50 +0900

    Hopefully avoid errors when pushing

--- server/buildpush.sh
+++ server/buildpush.sh
@@ -6,5 +6,7 @@ export REMOTE_IMAGE=gcr.io/dancedeets-hrd/$NAME
 time docker build -t $LOCAL_IMAGE .
 docker tag $LOCAL_IMAGE $REMOTE_IMAGE
 time gcloud docker -- push $REMOTE_IMAGE
-time gcloud app deploy --quiet --image-url $REMOTE_IMAGE batch.yaml &
+# I am sleeping, because it seems running concurrently can trigger this error:
+# Error Response: [13] Unexpected Error. (AppLock already held.)
+sleep 10 && time gcloud app deploy --quiet --image-url $REMOTE_IMAGE batch.yaml &
 time gcloud app deploy --quiet --image-url $REMOTE_IMAGE app.yaml
