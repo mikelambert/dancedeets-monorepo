2180597205de3d897fc83f23bb25a8e06b040575
Mike Lambert <mlambert@gmail.com>
2017-04-30 16:48:55 +0900

    Add fetch_adgeolocs, import_cities, starting to move on our geonames pipeline.

--- server/geonames/fetch_adgeolocs.py
+++ server/geonames/fetch_adgeolocs.py
@@ -11,9 +11,12 @@ import urllib
 import facebook
 from util import urls
 
+from . import sqlite_db
 
-FILENAME_CITIES = 'rankings/cities_only.db'
-FILENAME_ADLOCS = 'rankings/adlocs_only.db'
+FILENAME_ADLOCS = '/Users/lambert/Dropbox/dancedeets/data/adlocs_only.db'
+
+FILENAME_CITIES_TXT = '/Users/lambert/Dropbox/dancedeets/data/cities5000.txt'
+
 
 FACEBOOK_CONFIG = facebook.load_yaml('facebook-dev.yaml')
 
@@ -35,8 +38,8 @@ def get_fb_targeting_key(data):
         'access_token': FACEBOOK_CONFIG['app_access_token'],
         'locale': 'en_US', # because app_access_token is locale-less and seems to use a IP-locale fallback
     }
-    cursor_adlocs.execute('select data from AdGeoLocation where q = ? and country_code = ?', (geo_search['q'], geo_search['country_code']))
-    result = cursor_adlocs.fetchone()
+    cursor.execute('select data from AdGeoLocation where q = ? and country_code = ?', (geo_search['q'], geo_search['country_code']))
+    result = cursor.fetchone()
     if not result:
         result = urllib.urlopen('https://graph.facebook.com/v2.9/search?%s' % urls.urlencode(geo_search)).read()
         result_json = json.loads(result)
@@ -47,36 +50,19 @@ def get_fb_targeting_key(data):
             'country_code': geo_search['country_code'],
             'data': array_json,
         }
-        insert_record(cursor_adlocs, 'AdGeoLocation', data)
+        sqlite_db.insert_record(cursor, 'AdGeoLocation', data)
         print '\n'.join('- ' + str(x) for x in result_json['data'])
 
-def insert_record(cursor, table_name, data):
-    key_values = data.items()
-
-    insert_sql = 'INSERT INTO %s (%s) VALUES (%s)' % (
-        table_name,
-        ', '.join(kv[0] for kv in key_values),
-        ', '.join('?' for kv in key_values),
-    )
-    try:
-        result = cursor.execute(insert_sql, [kv[1] for kv in key_values])
-    except Exception as e:
-        logging.exception('Found problems with data: %s', data)
-    cursor.connection.commit()
-
-conn_adlocs = sqlite3.connect(FILENAME_ADLOCS)
-cursor_adlocs = conn_adlocs.cursor()
-cursor_adlocs.execute('''CREATE TABLE IF NOT EXISTS AdGeoLocation
-             (geonameid integer primary key, q text, country_code text, data text)''')
-
-conn_cities = sqlite3.connect(FILENAME_CITIES)
-cursor_cities = conn_cities.cursor()
-cursor_cities.execute('''DROP TABLE City''')
-cursor_cities.execute('''CREATE TABLE City
-             (geonameid integer primary key, city_name text, state_name text, country_name text, latitude real, longitude real, population integer, timezone text)''')
+conn = sqlite3.connect(FILENAME_ADLOCS)
+cursor = conn.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS AdGeoLocation
+    (q text, country_code text, data text,
+    PRIMARY KEY (q, country_code))
+''')
 
 count = 0
-for line in open('/Users/lambert/Dropbox/dancedeets/data/cities5000.txt'):
+for line in open(FILENAME_CITIES_TXT):
     count += 1
     line = line.decode('utf-8')
 
@@ -98,11 +84,8 @@ for line in open('/Users/lambert/Dropbox/dancedeets/data/cities5000.txt'):
         'longitude': float(longitude),
         'population': int(population),
         'timezone': timezone,
+
     }
 
-
-    key = get_fb_targeting_key(data)
-
-    insert_record(cursor_cities, 'City', data)
-conn_cities.commit()
-conn_adlocs.commit()
+    get_fb_targeting_key(data)
+conn.commit()
---
+++ server/geonames/sqlite_db.py
@@ -0,0 +1,15 @@
+
+
+def insert_record(cursor, table_name, data):
+    key_values = data.items()
+
+    insert_sql = 'INSERT INTO %s (%s) VALUES (%s)' % (
+        table_name,
+        ', '.join(kv[0] for kv in key_values),
+        ', '.join('?' for kv in key_values),
+    )
+    try:
+        result = cursor.execute(insert_sql, [kv[1] for kv in key_values])
+    except Exception as e:
+        logging.exception('Found problems with data: %s', data)
+    cursor.connection.commit()
