63ddd863c3d044df5b025ac335fccd1347cd06e5
Mike Lambert <mlambert@gmail.com>
2017-04-30 22:56:52 +0900

    Add the index to our generation!

--- server/geonames/build_cities_db.py
+++ server/geonames/build_cities_db.py
@@ -34,7 +34,8 @@ def save_cities_db(cities_db_filename):
     cursor_cities.execute('''DROP TABLE IF EXISTS City''')
     cursor_cities.execute('''CREATE TABLE City
                  (geoname_id integer primary key, ascii_name text, admin1_code text, country_code text, latitude real, longitude real, population integer, timezone text, adgeolocation_key)''')
-
+    # We index on longitude first, since it's likely to have the greatest variability and pull in the least amount of cities
+    cursor_cities.execute('''CREATE INDEX geo on City (country_code, longitude, latitude);''')
     for geoname in geoname_files.cities(5000):
         adgeolocation_key = get_fb_targeting_key(cursor_adlocs, geoname)
 
