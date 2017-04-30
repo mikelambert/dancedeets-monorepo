2b8d30a0ac451ac7b8e3b0969f81026ca8231cbf
Mike Lambert <mlambert@gmail.com>
2017-04-30 22:57:14 +0900

    Fix our gulpfile, and regenerate our cities.db with an index.

--- server/gulpfile.babel.js
+++ server/gulpfile.babel.js
@@ -47,8 +47,8 @@ const baseAssetsDir = `/Users/${username.sync()}/Dropbox/dancedeets/art/build-as
 gulp.task('web:events:resave', cb => fetch('http://www.dancedeets.com/tasks/reload_events?user_id=701004&allow_cache=1&disable_updates=regeocode,photo&queue=fast-queue&only_if_updated=0').then(x => console.log(x)))
 
 gulp.task('compile:geonames:fetch_adgeolocs', $.shell.task('python ./geonames/fetch_adgeolocs.py'))
-gulp.task('compile:geonames:build_cities_db', ['geonames:fetch_adgeolocs'], $.shell.task('python ./geonames/build_cities_db.py ./geonames/cities.db'))
-gulp.task('compile:geonames', ['geonames:build_cities_db'])
+gulp.task('compile:geonames:build_cities_db', ['compile:geonames:fetch_adgeolocs'], $.shell.task('python ./geonames/build_cities_db.py ./geonames/cities.db'))
+gulp.task('compile:geonames', ['compile:geonames:build_cities_db'])
 
 gulp.task('compile:images:favicons', () => gulp
   .src('assets/img/deets-head.png')
