fca0a4779d8b4a7b4f37b0a2e6397f682c41b9e9
Mike Lambert <mlambert@gmail.com>
2017-05-01 19:22:58 +0900

    Handle dev/test environments

--- server/util/ips.py
+++ server/util/ips.py
@@ -1,3 +1,3 @@
 def get_remote_ip(request):
     # This is bogus Google IPs: self.request.remote_addr
-    return request.headers.get('x-forwarded-for').split(', ')[0]
+    return request.headers.get('x-forwarded-for', '127.0.0.1').split(', ')[0]
