ee6826f96ebdca41a206580dbf5ecc829d20944a
Mike Lambert <mlambert@gmail.com>
2017-05-05 14:01:01 +0900

    Add some better logging/debugging to our taskqueue logic, so we can understand how pending retry tasks get stuck

--- server/util/taskqueue.py
+++ server/util/taskqueue.py
@@ -2,5 +2,15 @@ from google.appengine.api.taskqueue import *
 from google.appengine.api import taskqueue
 
 def add(*args, **kwargs):
+    import logging
+    import os
+    from google.appengine.api import app_identity
+    logging.info('TQADD: (*%s, **%s)', args, kwargs)
+    logging.info('TQADD: DEFAULT_VERSION_HOSTNAME = %s', os.getenv('DEFAULT_VERSION_HOSTNAME'))
+    logging.info('TQADD: app_identity.get_default_version_hostname = %s', app_identity.get_default_version_hostname())
+    logging.info('TQADD: SERVER_SOFTWARE = %s', os.environ.get('SERVER_SOFTWARE', ''))
     kwargs['target'] = taskqueue.DEFAULT_APP_VERSION
-    return taskqueue.add(*args, **kwargs)
+    result = taskqueue.add(*args, **kwargs)
+    logging.info('TQADD: self.__target = %s', result._Task__target)
+    logging.info('TQADD: self.__headers = %s', result._Task__headers)
+    return result
