cd8532ad263ee8d45d5557ac7b2578cb3bf71ae7
Mike Lambert <mlambert@gmail.com>
2017-06-02 02:18:49 +0900

    Upgrade to RN 0.44 (no testing!) and Flow 0.47 (no reduction of flowtype errors!)

--- mobile/android/app/src/main/java/com/dancedeets/android/MainApplication.java
+++ mobile/android/app/src/main/java/com/dancedeets/android/MainApplication.java
@@ -22,6 +22,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.soloader.SoLoader;
 import com.higo.zhangyp.segmented.AndroidSegmentedPackage;
 import com.inprogress.reactnativeyoutube.ReactNativeYouTube;
 import com.joshblour.reactnativepermissions.ReactNativePermissionsPackage;
@@ -46,81 +47,82 @@ import io.fabric.sdk.android.Fabric;
 import io.fixd.rctlocale.RCTLocalePackage;
 import io.fullstack.firestack.FirestackPackage;
 
-class MyReactNativeHost extends ReactNativeHost implements ReactInstanceHolder {
+public class MainApplication extends MultiDexApplication implements ReactApplication {
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     static CallbackManager mCallbackManager = CallbackManager.Factory.create();
 
-    protected MyReactNativeHost(Application application) {
-        super(application);
-    }
-
     @Override
-    protected boolean getUseDeveloperSupport() {
-        return BuildConfig.DEBUG;
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
     }
 
     @Override
     protected List<ReactPackage> getPackages() {
-        return Arrays.<ReactPackage>asList(
-            new MainReactPackage(),
-            new VectorIconsPackage(),
-            new RNDeviceInfo(),
-            new FirestackPackage(),
-            new ReactNativeYouTube(),
-            new GoogleApiAvailabilityPackage(),
-            new ReactNativePushNotificationPackage(),
-            new PhotoViewPackage(),
-            new ReactNativePermissionsPackage(),
-            new FirebasePackage(),
-            new RCTNativeEnvPackage(BuildConfig.class),
-            new RCTLocalePackage(),
-            new CodePush(BuildConfig.CODEPUSH_KEY, getApplication(), BuildConfig.DEBUG),
-            new RNSharePackage(),
-            new RNMail(),
-            new RNAdMobPackage(),
-            new RNMixpanel(),
-            new RNGeocoderPackage(),
-            new MapsPackage(),
-            new FabricPackage(),
-            new LinearGradientPackage(),
-            new AndroidSegmentedPackage(),
-            new RNSendIntentPackage(),
-            new FBSDKPackage(mCallbackManager)
-        );
+      return Arrays.<ReactPackage>asList(
+        new MainReactPackage(),
+        new VectorIconsPackage(),
+        new RNDeviceInfo(),
+        new FirestackPackage(),
+        new ReactNativeYouTube(),
+        new GoogleApiAvailabilityPackage(),
+        new ReactNativePushNotificationPackage(),
+        new PhotoViewPackage(),
+        new ReactNativePermissionsPackage(),
+        new FirebasePackage(),
+        new RCTNativeEnvPackage(BuildConfig.class),
+        new RCTLocalePackage(),
+        new CodePush(BuildConfig.CODEPUSH_KEY, getApplication(), BuildConfig.DEBUG),
+        new RNSharePackage(),
+        new RNMail(),
+        new RNAdMobPackage(),
+        new RNMixpanel(),
+        new RNGeocoderPackage(),
+        new MapsPackage(),
+        new FabricPackage(),
+        new LinearGradientPackage(),
+        new AndroidSegmentedPackage(),
+        new RNSendIntentPackage(),
+        new FBSDKPackage(mCallbackManager)
+      );
     }
 
     @Override
     protected String getJSBundleFile() {
-        return CodePush.getBundleUrl();
+      return CodePush.getBundleUrl();
     }
-}
+  };
 
-public class MainApplication extends MultiDexApplication implements ReactApplication {
-    private final MyReactNativeHost mReactNativeHost = new MyReactNativeHost(this);
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+ 
 
-    protected static CallbackManager getCallbackManager() {
-        return MyReactNativeHost.mCallbackManager;
-    }
+  protected static CallbackManager getCallbackManager() {
+    return MyReactNativeHost.mCallbackManager;
+  }
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        // If we want to have Crashlytics report the version of our JS (and not our app),
-        // because we are using CodePush or AppHub, then we need to pass in a stubbed Context.
-        // It will wrap-and-delegate-to-"this", except for:
-        //   PackageManager packageManager = context.getPackageManager();
-        //   PackageInfo packageInfo = packageManager.getPackageInfo(this.packageName, 0);
-        //   this.versionCode = Integer.toString(packageInfo.versionCode);
-        //   this.versionName = (packageInfo.versionName == null ? "0.0" : packageInfo.versionName);
-        Fabric.with(this, new Crashlytics());
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    // If we want to have Crashlytics report the version of our JS (and not our app),
+    // because we are using CodePush or AppHub, then we need to pass in a stubbed Context.
+    // It will wrap-and-delegate-to-"this", except for:
+    //   PackageManager packageManager = context.getPackageManager();
+    //   PackageInfo packageInfo = packageManager.getPackageInfo(this.packageName, 0);
+    //   this.versionCode = Integer.toString(packageInfo.versionCode);
+    //   this.versionName = (packageInfo.versionName == null ? "0.0" : packageInfo.versionName);
+    Fabric.with(this, new Crashlytics());
 
-        // Initialize the SDK before executing any other operations,
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        AppEventsLogger.activateApp(this);
-        CodePush.setReactInstanceHolder(mReactNativeHost);
-    }
+    // Initialize the SDK before executing any other operations,
+    FacebookSdk.sdkInitialize(getApplicationContext());
+    AppEventsLogger.activateApp(this);
+    CodePush.setReactInstanceHolder(mReactNativeHost);
+    SoLoader.init(this, /* native exopackage */ false);
+  }
 
-    @Override
-    public ReactNativeHost getReactNativeHost() {
-        return mReactNativeHost;
-    }
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
 }
