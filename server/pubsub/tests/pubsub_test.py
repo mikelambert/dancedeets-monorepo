95d8ef2045c4304776997dbcfd1f724292262544
Mike Lambert <mlambert@gmail.com>
2017-06-30 14:58:04 -0700

    Fix https link

--- server/pubsub/tests/pubsub_test.py
+++ server/pubsub/tests/pubsub_test.py
@@ -111,7 +111,7 @@ class TestImports(unittest.TestCase):
             start_time = datetime.datetime(2010, 1, 1, 12)
             actual_city_name = 'Sacramento, CA, United States'
             fb_event = FB_EVENT
-        url = 'http://www.dancedeets.com/events/555/?utm_campaign=autopost&utm_medium=share&utm_source=twitter_feed'
+        url = 'https://www.dancedeets.com/events/555/?utm_campaign=autopost&utm_medium=share&utm_source=twitter_feed'
         self.assertEqual(
             event.format_twitter_post(config, DBEvent(), media=False, handles=[]),
             u'2010/01/01: Sacramento, CA, United States: Some really long name here that just keeps on going and may or may not ev… %s' % url)
