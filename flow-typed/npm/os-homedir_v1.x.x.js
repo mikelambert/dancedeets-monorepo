e869255777239fdff07b1f767ed9041478892883
Mike Lambert <mlambert@gmail.com>
2017-06-30 16:03:45 -0700

    Add flowtyped configs, and set up our dependencies and flow configs so they work from the base level.

---
+++ flow-typed/npm/os-homedir_v1.x.x.js
@@ -0,0 +1,6 @@
+// flow-typed signature: cb2d81c3b3ef1df09fff89eb01cdaee6
+// flow-typed version: 94e9f7e0a4/os-homedir_v1.x.x/flow_>=v0.28.x
+
+declare module 'os-homedir' {
+  declare module.exports: () => string;
+}
