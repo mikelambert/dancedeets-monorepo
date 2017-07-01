5c205a2c4840c59ed086bfed04c0b4809b323ba4
Mike Lambert <mlambert@gmail.com>
2017-07-01 01:43:24 -0700

    Copy over the fabric.properties at app init.

--- mobile/setup.sh
+++ mobile/setup.sh
@@ -2,5 +2,6 @@
 
 cp ~/Dropbox/dancedeets/private/sentry.properties ./ios/
 cp ~/Dropbox/dancedeets/private/sentry.properties ./android/
+cp ~/Dropbox/dancedeets/private/fabric.properties ./android/app/
 
 npm install
