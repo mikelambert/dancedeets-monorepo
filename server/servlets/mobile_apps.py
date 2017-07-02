5b24af862af7805265b6c7d53015a85ffb9d7d6e
Mike Lambert <mlambert@gmail.com>
2017-07-02 02:00:01 -0700

    Remove walkout animation

--- server/servlets/mobile_apps.py
+++ server/servlets/mobile_apps.py
@@ -38,7 +38,6 @@ class MobileAppsHandler(base_servlet.BaseRequestHandler):
         self.display['android_url'] = mobile.ANDROID_URL
         self.display['ios_url'] = mobile.IOS_URL
         self.display['total_time'] = total_time
-        self.display['walkout_animation'] = self.get_walkout_animation(total_time, -1000)
         if message:
             self.display['messages'] = [message]
         if error:
@@ -72,38 +71,3 @@ class MobileAppsHandler(base_servlet.BaseRequestHandler):
                 return
 
             self.render_page(message="Thank you, your SMS should be arriving shortly. Just open the link on your phone to download the DanceDeets app.")
-
-    def get_walkout_animation(self, total_time, total_distance):
-        jumps = int(total_time / 0.5)
-        if jumps * 0.5 != total_time:
-            raise ValueError("total_time(%s) must be multiple of 0.5" % total_time)
-        loop_pct = 100.0 / jumps
-        half_loop_pct = loop_pct / 2
-
-        jump_distance = total_distance / jumps
-
-        stages = []
-        for i in range(jumps):
-            this_loop_pct = i*loop_pct
-            this_half_loop_pct = this_loop_pct + half_loop_pct
-            this_distance = i*jump_distance
-            animation_stage = """
-            %(half_loop_pct)s%% {
-                left: %(distance)spx;
-            }
-            %(loop_pct)s%% {
-                left: %(distance)spx;
-                -webkit-animation-timing-function: linear;
-                animation-timing-function: linear;
-            }
-            """ % dict(half_loop_pct=this_half_loop_pct, loop_pct=this_loop_pct, distance=this_distance)
-            stages.append(animation_stage)
-
-        stages.append("""
-            100%% {
-                left: %(distance)spx;
-            }
-        """ % dict(distance=total_distance))
-        keyframes = ''.join(stages)
-        animations = "@-webkit-keyframes walkOut {%s}\n@keyframes walkOut {%s}" % (keyframes, keyframes)
-        return animations
