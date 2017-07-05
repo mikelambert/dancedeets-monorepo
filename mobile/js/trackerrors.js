7acd99bd7677d2fa0d564d9ce357e02d7555493b
Mike Lambert <mlambert@gmail.com>
2017-07-05 02:54:45 -0700

    Speed up event transitions

--- mobile/js/trackerrors.js
+++ mobile/js/trackerrors.js
@@ -8,9 +8,12 @@ export default function init() {
     // Live reloading and hot reloading in particular lead to tons of noise...
     return;
   }
+  // We deactivate stacktrace merging due to performance issues:
+  // https://github.com/getsentry/react-native-sentry/issues/109
+  // Seems that we pay a 100-200ms+ penalty on event-navigation transitions without this.
   Sentry.config(
     'https://56b58e9bc5c44335ab7ef39a1f85d1aa:5d0631b36af0489fab9eae00b9953e4f@sentry.io/106536'
-  ).install();
+  ).install({ deactivateStacktraceMerging: true });
 
   trackjs.init({
     token: '77a8a7079d734df7a94150f8f0d7e16f',
