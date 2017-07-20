124afedf8e091507278b364f5c889b9d64c9a90b
Mike Lambert <mlambert@gmail.com>
2017-07-19 21:17:22 -0400

    Add react-native-video

--- mobile/android/app/src/main/java/com/dancedeets/android/MainApplication.java
+++ mobile/android/app/src/main/java/com/dancedeets/android/MainApplication.java
@@ -16,6 +16,7 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.react.ReactApplication;
+import com.brentvatne.react.ReactVideoPackage;
 import io.sentry.RNSentryPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
@@ -66,6 +67,7 @@ class MyReactNativeHost extends ReactNativeHost implements ReactInstanceHolder {
   protected List<ReactPackage> getPackages() {
     return Arrays.<ReactPackage>asList(
       new MainReactPackage(),
+            new ReactVideoPackage(),
       new RNSentryPackage((ReactApplication)getApplication()),
       new VectorIconsPackage(),
       new RNDeviceInfo(),
