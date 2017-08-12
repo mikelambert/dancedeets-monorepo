dfb7fab997995dc9ad628ce2d5b592e9ad5ec742
Mike Lambert <mlambert@gmail.com>
2017-08-12 15:52:37 -0400

    Oops, add our address.py file necessary to get a search's city and produce breadcrumbs.

---
+++ server/loc/address.py
@@ -0,0 +1,16 @@
+from loc import formatting
+
+def get_address_from_geocode(geocode):
+    if not geocode:
+        return {}
+    country = geocode.country(long=True)
+    address = {}
+    if country:
+        address['country'] = country
+    city = formatting.get_city(geocode)
+    if city:
+        address['city'] = city
+    state = formatting.get_state(geocode)
+    if state:
+        address['state'] = state
+    return address
