42635ed44368ce8a59b94e995f8614b8bac6db18
Mike Lambert <mlambert@gmail.com>
2017-08-11 23:48:32 -0400

    Stop sending STS header for subdomains, instead send it for every domain except our dev domain.

--- server/base_servlet.py
+++ server/base_servlet.py
@@ -585,7 +585,8 @@ class BaseRequestHandler(BareBaseRequestHandler):
         # This only 'takes effect' when it is returned on an https domain,
         # so we still need to make sure to add an https redirect.
         https_redirect_duration = 60 * 60 * 24 * 7
-        self.response.headers.add_header('Strict-Transport-Security', 'max-age=%s; includeSubDomains' % https_redirect_duration)
+        if url.netloc != 'dev.dancedeets.com':
+            self.response.headers.add_header('Strict-Transport-Security', 'max-age=%s' % https_redirect_duration)
         # This is how we detect if the incoming url is on https in GAE Flex (we cannot trust request.url)
         if request.method == 'GET' and request.environ.get('HTTP_X_FORWARDED_PROTO') == 'http':
             new_url = urlparse.urlunsplit([
