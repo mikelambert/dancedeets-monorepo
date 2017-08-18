661d7430a99562e6b2210f889b4cc15acc765881
Mike Lambert <mlambert@gmail.com>
2017-08-18 16:45:44 -0400

    On initial setup, the dist-includes/ directory (and associated amp files) don't exist yet. Which make it impossible to generate AMP pages.

--- server/servlets/event.py
+++ server/servlets/event.py
@@ -133,11 +133,15 @@ class ShowEventHandler(base_servlet.BaseRequestHandler):
                 # Because minification interferes with html-validity when producing:
                 # <meta name=viewport content=width=device-width,minimum-scale=1,initial-scale=1,maximum-scale=1,user-scalable=no>
                 self.allow_minify = False
-                event_amp_css_filename = os.path.join(os.path.dirname(__file__), '..', 'dist-includes/css/eventAmp.css')
-                event_amp_css = open(event_amp_css_filename).read()
-                event_amp_css = re.sub(r'@-ms-viewport\s*{.*?}', '', event_amp_css)
-                event_amp_css = re.sub(r'!important', '', event_amp_css)
-                event_amp_css = event_amp_css.replace('url(..', 'url(/dist-' + self._get_static_version())
+                try:
+                    event_amp_css_filename = os.path.join(os.path.dirname(__file__), '..', 'dist-includes/css/eventAmp.css')
+                    event_amp_css = open(event_amp_css_filename).read()
+                    event_amp_css = re.sub(r'@-ms-viewport\s*{.*?}', '', event_amp_css)
+                    event_amp_css = re.sub(r'!important', '', event_amp_css)
+                    event_amp_css = event_amp_css.replace('url(..', 'url(/dist-' + self._get_static_version())
+                except IOError as e:
+                    logging.exception('Failed to load AMP CSS')
+                    event_amp_css = ''
                 self.display['event_amp_stylesheet'] = event_amp_css
                 self.render_template('event_amp')
             else:
