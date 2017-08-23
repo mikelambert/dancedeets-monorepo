43f75300e53d18b7e616aaff5ae66a362c5a0e34
Mike Lambert <mlambert@gmail.com>
2017-08-23 18:38:40 -0400

    Fix setup script to pull from our new directories

--- server/setup_private.sh
+++ server/setup_private.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 BASE_DIR=$(pwd)
 
 echo "Copying over private configs"
-cp ~/Dropbox/dancedeets/git-assets/development/server/*.yaml $BASE_DIR/
-cp ~/Dropbox/dancedeets/git-assets/production/server/*.yaml $BASE_DIR/
+cp ~/Dropbox/dancedeets-development/server/*.yaml $BASE_DIR/
+cp ~/Dropbox/dancedeets-production/server/*.yaml $BASE_DIR/
