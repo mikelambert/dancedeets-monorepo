afc2be0f7cae44d6871ee444baeb57c00a45c064
Mike Lambert <mlambert@gmail.com>
2017-09-06 14:14:10 -0600

    Let's rework our css imports to import a single SCSS file. This means only one SCSS file needs to be passed through our postcss-loader, which means uncss only needs to load and render the pages one time, resulting in a much faster evaluation.

--- server/assets/js/all-css.js
+++ server/assets/js/all-css.js
@@ -4,27 +4,4 @@
  * @flow
  */
 
-require('../../node_modules/font-awesome/css/font-awesome.css');
-require('../../node_modules/jquery.smartbanner/jquery.smartbanner.css');
-require('../css/bootstrap-custom.scss');
-
-// media queries to complement our React JS
-require('../css/tutorials.scss');
-require('../css/resultsSearchBox.scss');
-require('../css/results.scss');
-
-require('../css/fonts.scss');
-require('../css/dancedeets.scss');
-require('../css/tabs.scss');
-require('../css/eventPage.scss');
-require('../css/social-hovers.scss');
-require('../css/header.scss');
-require('../css/footer.scss');
-require('../css/custom.scss');
-require('../css/dancedeets-old.scss');
-
-require('slick-carousel/slick/slick.scss');
-require('slick-carousel/slick/slick-theme.scss');
-require('../css/slick.scss');
-require('../css/rc-collapse.scss');
-require('react-spinkit/css/circle.css');
+require('../css/everything.scss');
