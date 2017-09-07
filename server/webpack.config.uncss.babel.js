07de0dc56ac0c75b9f1274dcaa14631e1180e0eb
Mike Lambert <mlambert@gmail.com>
2017-09-07 15:21:12 -0400

    Explain modal div whitelists

--- server/webpack.config.uncss.babel.js
+++ server/webpack.config.uncss.babel.js
@@ -9,5 +9,7 @@ import uncssWebpackGenerator from './uncssWebpackGenerator';
 module.exports = uncssWebpackGenerator(
   'full',
   ['amp/generated/*-full.html'],
+  // This contains the css to hide modals on initial display (until shown).
+  // Unfortunately our source files don't yet contain these modal divs.
   ['.modal']
 );
