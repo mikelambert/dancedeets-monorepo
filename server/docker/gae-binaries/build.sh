27dd611cc44a715843811e824e701e9ae29fa354
Mike Lambert <mlambert@gmail.com>
2017-09-12 16:34:36 -0400

    Ensure our builds actually build everything.

--- server/docker/gae-binaries/build.sh
+++ server/docker/gae-binaries/build.sh
@@ -3,6 +3,6 @@ cd `dirname $0`
 export NAME=gae-binaries
 export LOCAL_IMAGE=mlambert/$NAME
 export REMOTE_IMAGE=gcr.io/dancedeets-hrd/$NAME
-docker build -t $LOCAL_IMAGE .
+docker build --no-cache -t $LOCAL_IMAGE .
 docker tag $LOCAL_IMAGE $REMOTE_IMAGE
 gcloud docker -- push $REMOTE_IMAGE
