6e54e01fe07543ab31044c0fe0be1f9a9d242828
Mike Lambert <mlambert@gmail.com>
2017-09-14 01:58:55 -0400

    Hopefully fix timezone formatting one last time.

--- server/sitemaps/serve.py
+++ server/sitemaps/serve.py
@@ -102,7 +102,7 @@ def sitemap_node(name, date):
     sitemap_node.append(loc_node)
     if date:
         lastmod_node = etree.Element('lastmod')
-        lastmod_node.text = date.strftime('%Y-%m-%dT%H:%M:%S%z')
+        lastmod_node.text = date.strftime('%Y-%m-%dT%H:%M:%S+00:00')
         sitemap_node.append(lastmod_node)
     return sitemap_node
 
