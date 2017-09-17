22b63757a262b117c3f7b03d7452bc861598a585
Mike Lambert <mlambert@gmail.com>
2017-09-17 15:26:41 -0400

    Fix up our setup.py to grab all the subdirectory files we probably want/need.

--- scrapers/scrapy/setup.py
+++ scrapers/scrapy/setup.py
@@ -6,7 +6,7 @@ import glob
 setup(
     name='dancedeets-scrapy',
     version='1.0',
-    packages=['dancedeets'] + find_packages(),
+    packages=['dancedeets.%s' % x for x in find_packages('../../server')] + find_packages(),
     package_dir={'dancedeets': '../../server'},
     install_requires=[
         # I'm not sure this really does anything more than documentation,
@@ -15,5 +15,15 @@ setup(
         'dateparser',
         'html2text',
     ],
+    data_files=[
+        ('dancedeets/dance_keywords', glob.glob('nlp/dance_keywords/*.txt')),
+        ('dancedeets', [
+            'keys.yaml',
+            'keys-dev.yaml',
+            'facebook-prod.yaml',
+            'facebook-test.yaml',
+        ]),
+    ],
+    include_package_data=True,
     entry_points={'scrapy': ['settings = scrapy_settings']},
 )
