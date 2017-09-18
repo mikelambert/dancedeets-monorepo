cdc75985e8799d723b009fa44e9aaa7c66cb1841
Mike Lambert <mlambert@gmail.com>
2017-09-17 20:58:12 -0400

    Fix how we do a DBEvent test, not need to "claim" all DBEvents with our subclass here, it just causes more problems than it is worth.

--- server/dancedeets/pubsub/tests/pubsub_test.py
+++ server/dancedeets/pubsub/tests/pubsub_test.py
@@ -115,38 +115,37 @@ class TestImports(unittest.TestCase):
             'characters_reserved_per_media': 24,
         }
 
-        class DBEvent(eventdata.DBEvent):
-            id = '555'
-            start_time = datetime.datetime(2010, 1, 1, 12)
-            actual_city_name = 'Sacramento, CA, United States'
-            fb_event = FB_EVENT
+        db_event = eventdata.DBEvent(id='555')
+        db_event.start_time = datetime.datetime(2010, 1, 1, 12)
+        db_event.actual_city_name = 'Sacramento, CA, United States'
+        db_event.fb_event = FB_EVENT
 
         url = 'https://www.dancedeets.com/events/555/some-really-long-name-here-that-just-keeps-on-going-and-may-or-may-not-ever-get-truncated-but-we-will-just-have-to-wait-and-see?utm_campaign=autopost&utm_medium=share&utm_source=twitter_feed'
         self.maxDiff = 1000
         self.assertEqual(
-            event.format_twitter_post(config, DBEvent(), media=False, handles=[]),
+            event.format_twitter_post(config, db_event, media=False, handles=[]),
             u'2010/01/01: Sacramento, CA, United States: Some really long name here that just keeps on going and may or may not ev… %s' %
             url
         )
         self.assertEqual(
-            event.format_twitter_post(config, DBEvent(), media=False, handles=['@name']),
+            event.format_twitter_post(config, db_event, media=False, handles=['@name']),
             u'2010/01/01: Sacramento, CA, United States: Some really long name here that just keeps on going and may or may … %s @name' %
             url
         )
         self.assertEqual(
             event.format_twitter_post(
-                config, DBEvent(), media=False, handles=['@name1', '@name2', '@name3', '@name4', '@name5', '@name6', '@name7']
+                config, db_event, media=False, handles=['@name1', '@name2', '@name3', '@name4', '@name5', '@name6', '@name7']
             ), u'2010/01/01: Sacramento, CA, United States: Some really long name here that just k… %s @name1 @name2 @name3 @name4 @name5' %
             url
         )
         self.assertEqual(
             event.format_twitter_post(
-                config, DBEvent(), media=False, handles=['@mspersia', '@grooveologydc', '@groovealils', '@dam_sf', '@mishmashboutique']
+                config, db_event, media=False, handles=['@mspersia', '@grooveologydc', '@groovealils', '@dam_sf', '@mishmashboutique']
             ), u'2010/01/01: Sacramento, CA, United States: Some really long name here that jus… %s @mspersia @grooveologydc @groovealils' %
             url
         )
         self.assertEqual(
-            event.format_twitter_post(config, DBEvent(), media=False, handles=['@jodywisternoff', '@jodywisternoff', '@Lane8music']),
+            event.format_twitter_post(config, db_event, media=False, handles=['@jodywisternoff', '@jodywisternoff', '@Lane8music']),
             u'2010/01/01: Sacramento, CA, United States: Some really long name here that just keep… %s @jodywisternoff @jodywisternoff' %
             url
         )
