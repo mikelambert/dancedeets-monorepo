97c8139e7b2d5440e172cfde31772dd92fb6143c
Mike Lambert <mlambert@gmail.com>
2017-09-18 16:14:23 -0400

    Use a set of allowable domains, that supports a single-redirect at the base level. And skip /mindbody/, which appears to be a sinkhole on the first site.

--- scrapers/scrapy/studios/spiders/studios.py
+++ scrapers/scrapy/studios/spiders/studios.py
@@ -11,18 +11,23 @@ from .. import yelp
 from .. import facebook
 
 class SameBaseDomainLinkExtractor(LinkExtractor):
+    def __init__(self, allowed_domains):
+        super(SameBaseDomainLinkExtractor, self).__init__()
+        self.allowed_domains = allowed_domains
+
     def _extract_links(self, selector, response_url, response_encoding, base_url):
         links = super(SameBaseDomainLinkExtractor, self)._extract_links(selector, response_url, response_encoding, base_url)
-        parsed_base_url = urlparse(base_url)
         for link in links:
+            if '/mindbody/' in link.url:
+                continue
             parsed_link_url = urlparse(link.url)
-            if parsed_base_url.netloc.lower() == parsed_link_url.netloc.lower():
+            if parsed_link_url.netloc.lower() in self.allowed_domains:
                 yield link
 
 
 class AllStudiosScraper(spiders.CrawlSpider):
     name = 'AllStudios'
-    allowed_domains = []
+    allowed_domains = set()
     handle_httpstatus_list = [403]
 
     custom_settings = {
@@ -30,10 +35,24 @@ class AllStudiosScraper(spiders.CrawlSpider):
         }
     }
 
-    rules = (
-        spiders.Rule(SameBaseDomainLinkExtractor()),
-    )
+    def parse_start_url(self, response):
+        # Allow start_urls to redirect somewhere else, and include that redirected domain
+        parsed_url = urlparse(response.url)
+        self.allowed_domains.add(parsed_url.netloc.lower())
+        return []
 
+
+    def parse_subpage(self, response):
+        print response.url
+        return None
+        #parsed_url = urlparse(response.request.url)
+
+    def __init__(self, *args, **kwargs):
+        self.rules = (
+            spiders.Rule(SameBaseDomainLinkExtractor(allowed_domains=self.allowed_domains), callback=self.parse_subpage, follow=True),
+        )
+        # We must set up self.rules before calling super, since super calls _compile_rules().
+        super(AllStudiosScraper, self).__init__(*args, **kwargs)
 
     def start_requests(self):
         self.businesses = yelp.Yelp().fetch_all(self.city)
@@ -50,6 +69,11 @@ class AllStudiosScraper(spiders.CrawlSpider):
                     website = result['website']
                     if not website.startswith('http'):
                         website = 'http://%s' % website
-                    yield scrapy.Request(website, self.parse)
+
+                    parsed_url = urlparse(website)
+                    self.allowed_domains.add(parsed_url.netloc.lower())
+                    request = scrapy.Request(website, self.parse)
+                    request.allow_redirect = True
+                    yield request
                     return
 
