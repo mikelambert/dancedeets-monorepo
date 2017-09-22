41651d33b038bbba4231487d50baeee9cfed7111
Mike Lambert <mlambert@gmail.com>
2017-09-22 18:19:36 -0400

    Add country blacklist, and fix our late-filtering logic.

--- server/dancedeets/pubsub/event.py
+++ server/dancedeets/pubsub/event.py
@@ -10,6 +10,91 @@ from .facebook import fb_util
 from .twitter import event as twitter_event
 from . import db
 
+blacklisted_countries = [
+    # Africa
+    "DZ",
+    "AO",
+    "BJ",
+    "BW",
+    "BF",
+    "BI",
+    "CM",
+    "CV",
+    "CF",
+    "KM",
+    "CD",
+    "DJ",
+    "EG",
+    "GQ",
+    "ER",
+    "ET",
+    "GA",
+    "GM",
+    "GH",
+    "GN",
+    "GW",
+    "CI",
+    "KE",
+    "LS",
+    "LR",
+    "LY",
+    "MG",
+    "MW",
+    "ML",
+    "MR",
+    "MU",
+    "MA",
+    "MZ",
+    "NA",
+    "NE",
+    "NG",
+    "of",
+    "RE",
+    "RW",
+    "SH",
+    "ST",
+    "SN",
+    "SC",
+    "SL",
+    "SO",
+    "ZA",
+    "SS",
+    "SD",
+    "SZ",
+    "TZ",
+    "TG",
+    "TN",
+    "UG",
+    "EH",
+    "ZM",
+    "ZW",
+    # Oceania
+    "AS",
+    "AU",
+    "CK",
+    "TL",
+    "FJ",
+    "PF",
+    "GU",
+    "KI",
+    "MH",
+    "FM",
+    "NR",
+    "NC",
+    "NZ",
+    "NU",
+    "NF",
+    "MP",
+    "PW",
+    "PG",
+    "PN",
+    "WS",
+    "SB",
+    "TK",
+    "TV",
+    "VU",
+]
+
 
 def _should_post_event_common(auth_token, db_event):
     geocode = db_event.get_geocode()
@@ -20,6 +105,7 @@ def _should_post_event_common(auth_token, db_event):
     if auth_token and auth_token.country_filters and event_country not in auth_token.country_filters:
         logging.info("Skipping event due to country filters")
         return False
+
     return True
 
 
@@ -34,8 +120,7 @@ def _event_has_enough_attendees(db_event):
     logging.info('Checking event %s and found %s overlap_ids', db_event.id, len(matcher.overlap_ids))
     if len(matcher.overlap_ids) < 30:
         return False
-    # Re-run these checks here, since they're fast, and things may have changed in the interim if the queue is long
-    return should_post_event_to_account(db_event)
+    return True
 
 
 def should_post_event_to_account(auth_token, db_event):
@@ -44,6 +129,16 @@ def should_post_event_to_account(auth_token, db_event):
     # Add some filters based on number-attendees
     if db_event.attendee_count < 100:
         return False
+
+    # Country filter blacklist
+    geocode = db_event.get_geocode()
+    if not geocode:
+        # Don't post events without a location. It's too confusing...
+        return False
+    event_country = geocode.country()
+    if event_country in blacklisted_countries:
+        return False
+
     return True
 
 
@@ -102,6 +197,9 @@ def _post_event(auth_token, db_event):
         return twitter_event.twitter_post(auth_token, db_event)
     elif auth_token.application == db.APP_FACEBOOK:
         result = facebook_event.facebook_post(auth_token, db_event)
+        # Re-run these checks here, since they're fast, and things may have changed in the interim if the queue is long
+        if not should_post_event_to_account(db_event):
+            return True
         return fb_util.processed_task(auth_token, result)
     elif auth_token.application == db.APP_FACEBOOK_WALL:
         result = facebook_event.post_on_event_wall(db_event)
