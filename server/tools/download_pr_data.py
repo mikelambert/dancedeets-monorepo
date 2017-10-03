9defce35d5f159039036be54b3f8802536cef7a4
Mike Lambert <mlambert@gmail.com>
2017-10-03 14:03:02 -0400

    Handle copying over to a truncated prcitycategory data file.

--- server/tools/download_pr_data.py
+++ server/tools/download_pr_data.py
@@ -2,6 +2,7 @@
 #
 # import google cloud datastore into sqlite
 
+import json
 import getpass
 import sqlite3
 import sys
@@ -9,7 +10,8 @@ import sys
 from google.cloud import datastore
 from dancedeets.geonames import sqlite_db
 
-FILENAME_DB = '/Users/%s/Dropbox/dancedeets-development/server/generated/pr_city_category.db' % getpass.getuser()
+FULL_DB = '/Users/%s/Dropbox/dancedeets-development/server/generated/pr_city_category_full.db' % getpass.getuser()
+TRIMMED_DB = '/Users/%s/Dropbox/dancedeets-development/server/generated/pr_city_category.db' % getpass.getuser()
 
 
 def all_rows():
@@ -17,7 +19,7 @@ def all_rows():
 
 
 def save_db(clear=False):
-    conn = sqlite3.connect(FILENAME_DB)
+    conn = sqlite3.connect(FULL_DB)
     cursor = conn.cursor()
     if clear:
         cursor.execute('''DROP TABLE IF EXISTS PRCityCategory''')
@@ -41,7 +43,6 @@ def save_db(clear=False):
     client = datastore.Client()
     query = client.query(kind='PRCityCategory')
     query.order = ['person_type', 'city', 'category']
-    query.add_filter('person_type', '=', 'ATTENDEE')
     for data in query.fetch():
         print data['person_type'], data['city'], data['category']
         sqlite_db.insert_record(cursor, 'PRCityCategory', data)
@@ -49,8 +50,43 @@ def save_db(clear=False):
     conn.commit()
 
 
+def copy_db(clear=True):
+    old_conn = sqlite3.connect(FULL_DB)
+    old_cursor = old_conn.cursor()
+    new_conn = sqlite3.connect(TRIMMED_DB)
+    new_cursor = new_conn.cursor()
+    if clear:
+        new_cursor.execute('''DROP TABLE IF EXISTS PRCityCategory''')
+        new_cursor.execute(
+            '''
+            CREATE TABLE PRCityCategory
+            (
+            created_date text not null,
+            person_type text not null,
+            city text not null,
+            category text not null,
+            top_people_json text,
+            PRIMARY KEY (person_type, city, category)
+            )
+            '''
+        )
+        new_cursor.execute('''
+            CREATE INDEX no_person_type ON PRCityCategory
+            (city, category)
+            ''')
+    old_cursor.execute('SELECT created_date, person_type, city, category, top_people_json FROM PRCityCategory')
+    for data_row in old_cursor.fetchall():
+        data = dict(created_date=data_row[0], person_type=data_row[1], city=data_row[2], category=data_row[3], top_people_json=data_row[4])
+        print data['person_type'], data['city'], data['category']
+        data['top_people_json'] = json.dumps(json.loads(data['top_people_json'])[:100])
+        sqlite_db.insert_record(new_cursor, 'PRCityCategory', data)
+
+    new_conn.commit()
+
+
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == 'db_name':
-        print FILENAME_DB
+        print TRIMMED_DB
     else:
-        save_db(clear=False)
+        save_db()
+        copy_db()
