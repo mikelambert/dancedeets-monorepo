7942dc4c3b4a3e87a0a11e69384b857bbef301dd
Mike Lambert <mlambert@gmail.com>
2017-10-03 01:58:53 -0400

    Allow re-insertion of existing records.

--- server/dancedeets/geonames/sqlite_db.py
+++ server/dancedeets/geonames/sqlite_db.py
@@ -4,13 +4,15 @@ import logging
 def insert_record(cursor, table_name, data):
     key_values = data.items()
 
-    insert_sql = 'INSERT INTO %s (%s) VALUES (%s)' % (
+    insert_sql = 'INSERT OR IGNORE INTO %s (%s) VALUES (%s)' % (
         table_name,
         ', '.join(kv[0] for kv in key_values),
         ', '.join('?' for kv in key_values),
     )
     try:
         result = cursor.execute(insert_sql, [kv[1] for kv in key_values])
-    except Exception as e:
+    except Exception:
         logging.exception('Found problems with data: %s', data)
+        result = None
     cursor.connection.commit()
+    return result
