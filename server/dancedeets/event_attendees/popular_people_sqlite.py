3c1d3f97a2976636a50a59a4e87b08c6d3519b9c
Mike Lambert <mlambert@gmail.com>
2017-10-13 03:50:21 -0400

    Factor our our logic for loading/prepping the DB

--- server/dancedeets/event_attendees/popular_people_sqlite.py
+++ server/dancedeets/event_attendees/popular_people_sqlite.py
@@ -10,46 +10,8 @@ import os
 from google.cloud import storage
 
 from dancedeets.util import runtime
+from dancedeets.util import sqlite_db
 from dancedeets.util import timelog
-
-DEV_DB = '/Users/%s/Dropbox/dancedeets-development/server/generated/pr_city_category.db' % getpass.getuser()
-SERVER_DB = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pr_city_category.db')
-
-FILE_COPY_LOCK = threading.Lock()
-
-
-def download_sqlite():
-    logging.info('Grabbing file-copy lock')
-    if FILE_COPY_LOCK.acquire():
-        # Check it again, now that we've finally gotten the lock
-        logging.info('Grabbed lock, checking if file exists: %s', os.path.exists(SERVER_DB))
-        if not os.path.exists(SERVER_DB):
-            # TODO: ensure we copy to a staging ground, and move it into place (so no one tries to open a malformed file)
-            client = storage.Client('dancedeets-hrd')
-            bucket = client.get_bucket('dancedeets-dependencies')
-            logging.info('Downloading file')
-            blob = bucket.get_blob('pr_city_category.db')
-            # Luckily this only takes around 5-10 seconds (for street dance) when run on GCE instances
-            contents = blob.download_as_string()
-            with atomic_write(SERVER_DB, overwrite=True) as f:
-                f.write(contents)
-                # SERVER_DB doesn't exist yet.
-            # Now it does.
-        FILE_COPY_LOCK.release()
-
-
-def _get_connection():
-    if runtime.is_local_appengine():
-        filename_db = DEV_DB
-    else:
-        filename_db = SERVER_DB
-        if not os.path.exists(filename_db):
-            start = time.time()
-            download_sqlite()
-            timelog.log_time_since('Downloading PRCityCategory sqlite db', start)
-    conn = sqlite3.connect(filename_db)
-    # Cannot be shared between threads, be careful if making this global!
-    return conn
 
 
 class FakePRCityCategory(object):
@@ -57,7 +19,7 @@ class FakePRCityCategory(object):
 
 
 def get_people_rankings_for_city_names_sqlite(city_names, attendees_only):
-    conn = _get_connection()
+    conn = sqlite_db.get_connection('pr_city_category')
     cursor = conn.cursor()
 
     query = 'SELECT person_type, city, category, top_people_json from PRCityCategory where city in (%s)' % ','.join('?' * len(city_names))
---
+++ server/dancedeets/util/sqlite_db.py
@@ -0,0 +1,52 @@
+from atomicwrites import atomic_write
+import sqlite3
+import getpass
+import logging
+import threading
+import time
+import os
+
+from google.cloud import storage
+
+from dancedeets.util import runtime
+from dancedeets.util import timelog
+
+DEV_PATH = '/Users/%s/Dropbox/dancedeets-development/server/generated/' % getpass.getuser()
+SERVER_PATH = os.path.dirname(os.path.realpath(__file__))
+
+FILE_COPY_LOCK = threading.Lock()
+
+
+def _download_sqlite(db_path):
+    logging.info('Grabbing file-copy lock')
+    if FILE_COPY_LOCK.acquire():
+        # Check it again, now that we've finally gotten the lock
+        logging.info('Grabbed lock, checking if file exists: %s', os.path.exists(db_path))
+        if not os.path.exists(db_path):
+            # TODO: ensure we copy to a staging ground, and move it into place (so no one tries to open a malformed file)
+            client = storage.Client('dancedeets-hrd')
+            bucket = client.get_bucket('dancedeets-dependencies')
+            logging.info('Downloading file')
+            blob = bucket.get_blob(os.path.basename(db_path))
+            # Luckily this only takes around 5-10 seconds (for street dance) when run on GCE instances
+            contents = blob.download_as_string()
+            with atomic_write(db_path, overwrite=True) as f:
+                f.write(contents)
+                # db_path doesn't exist yet.
+            # Now it does.
+        FILE_COPY_LOCK.release()
+
+
+def get_connection(database_name):
+    full_db_name = '%s.db' % database_name
+    if runtime.is_local_appengine():
+        db_path = os.path.join(DEV_PATH, full_db_name)
+    else:
+        db_path = os.path.join(SERVER_DB, full_db_name)
+        if not os.path.exists(db_path):
+            start = time.time()
+            _download_sqlite(db_path)
+            timelog.log_time_since('Downloading PRCityCategory sqlite db', start)
+    conn = sqlite3.connect(db_path)
+    # Cannot be shared between threads, be careful if making this global!
+    return conn
