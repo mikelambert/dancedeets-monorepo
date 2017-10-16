41bd87161e03620a01a449f8f4200c56b60ee42e
Mike Lambert <mlambert@gmail.com>
2017-10-16 01:49:47 -0400

    Cleanup dead code

--- server/dancedeets/web_events/web_events_servlets.py
+++ server/dancedeets/web_events/web_events_servlets.py
@@ -1,4 +1,3 @@
-import datetime
 import json
 import logging
 from dancedeets import keys
@@ -23,40 +22,6 @@ class JsonDataHandler(webapp2.RequestHandler):
             self.json_body = None
 
 
-def process_upload_finalization(studio_name):
-    return  # TODO: WEB_EVENTS If the webpage disappears, we need to delete the event.
-    logging.info('De-duping all classes for %s', studio_name)
-    historical_fixup = datetime.datetime.today() - datetime.timedelta(days=2)
-    # TODO: sometimes this query returns stale data. In particular,
-    # it returns objects that don't have the latest updated scrape_time.
-    # This then confuses dedupe_classes() logic, which wants to delete
-    # these presumed-dead classes, when in fact they are still alive.
-    # Disabling the memcache and incontext NDB caches doesn't appear to help,
-    # since the stale objects are returned from the dev_apperver DB directly.
-    query = None
-    #class_models.StudioClass.query(
-    #    class_models.StudioClass.studio_name == studio_name,
-    #    class_models.StudioClass.start_time >= historical_fixup)
-    num_events = 1000
-    results = query.fetch(num_events)
-    if len(results) == num_events:
-        logging.error("Processing %s events for studio %s, and did not reach the end of days-with-duplicates", num_events, studio_name)
-
-    max_scrape_time = max(results, key=lambda x: x.scrape_time).scrape_time
-
-    classes_by_date = {}
-    for studio_class in results:
-        classes_by_date.setdefault(studio_class.start_time.date(), []).append(studio_class)
-    for date, classes in classes_by_date.iteritems():
-        # RISKY!!! uses current date (in what timezone??)
-        if date < datetime.date.today():
-            scrape_time_to_keep = max(classes, key=lambda x: x.scrape_time).scrape_time
-        else:
-            scrape_time_to_keep = max_scrape_time
-        print date, scrape_time_to_keep, classes
-        # dedupe_classes(scrape_time_to_keep, classes)
-
-
 @app.route('/web_events/upload_multi')
 class WebMultiUploadHandler(JsonDataHandler):
     def post(self):
@@ -78,5 +43,4 @@ class WebMultiUploadHandler(JsonDataHandler):
             logging.info("New event, publishing to twitter/facebook: %s", event_id)
             deferred.defer(pubsub.eventually_publish_event, event_id)
 
-        process_upload_finalization(self.json_body['studio_name'])
         self.response.status = 200
