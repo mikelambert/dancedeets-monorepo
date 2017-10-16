bb52e3c5fe1a88ff61aa178a4c5051e54bffb8ce
Mike Lambert <mlambert@gmail.com>
2017-10-16 16:43:09 -0400

    Add our first scraper. Will likely move/rename, but this is a place to get started.

---
+++ server/dancedeets/scrape_apis/jwjam.py
@@ -0,0 +1,52 @@
+import json
+import urllib
+import urllib2
+from dancedeets import keys
+from dancedeets.events import web_events_reloading
+from dancedeets.events import namespaces
+
+
+def make_request(server, path, params):
+    new_params = params.copy()
+    new_params['scrapinghub_key'] = keys.get('scrapinghub_key')
+    data = json.dumps(new_params)
+    quoted_data = urllib.quote_plus(data)
+    f = urllib2.urlopen('http://%s/%s' % (server, path), quoted_data)
+    result = f.read()
+    return result
+
+
+def push_items(items):
+    server = 'dev.dancedeets.com:8080'
+    path = 'web_events/upload_multi'
+    params = {
+        'items': items,
+    }
+    make_request(server, path, params)
+
+
+def fetch_all_ids(namespace, start_id=0):
+    trailing_count = 20
+    missing = 0
+    id = start_id
+    while True:
+        item = web_events_reloading.fetch_jwjam(namespace, id)
+        print id, item
+        id += 1
+
+        if not item:
+            continue
+        push_items([item])
+
+        if item:
+            missing = 0
+        else:
+            missing += 1
+        if missing >= trailing_count:
+            break
+    last_id = id - missing
+    return last_id
+
+
+last_id = fetch_all_ids(namespaces.CHINA_JWJAM_JAM, start_id=0)
+fetch_all_ids(namespaces.CHINA_JWJAM_COURSE, start_id=0)
