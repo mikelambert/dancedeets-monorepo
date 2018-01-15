08dc58bab64b867ee9f8002c9b76e5ed5ac56aeb
Mike Lambert <mlambert@gmail.com>
2018-01-15 17:58:29 -0500

    Fix jp dancedelight

--- scrapers/scrapy/web_events/spiders/jp_dancedelight.py
+++ scrapers/scrapy/web_events/spiders/jp_dancedelight.py
@@ -18,31 +18,32 @@ class DanceDelightScraper(items.WebEventScraper):
     namespace = namespaces.JAPAN_DD
 
     def start_requests(self):
-        yield scrapy.Request('http://www.dancedelight.net/wordpress/?cat=6')
+        yield scrapy.Request('https://www.dancedelight.net/event/?pg=0')
 
     def parse(self, response):
-        if '?cat=' in response.url:
+        if '?pg=' in response.url:
             return self.parseList(response)
         else:
             return self.parseEvent(response)
 
     def parseList(self, response):
-        next_url = response.xpath('//a[@rel="next"]/@href').extract()
+        next_url = response.xpath('//dd/a/@href').extract()
         for url in next_url:
-            yield scrapy.Request(urlparse.urljoin(response.url, url))
+            if '/event/' in url:
+                yield scrapy.Request(urlparse.urljoin(response.url, url))
 
-        event_urls = response.xpath('//a[@rel="bookmark"]/@href').extract()
+        event_urls = response.css('table.topics_list').xpath('.//a/@href').extract()
         for url in event_urls:
             yield scrapy.Request(urlparse.urljoin(response.url, url))
 
-    def parseDateTimes(self, content_date, post_text):
-        month_date_re = ur'(\d+)月\s*(\d+)'
-        date_match = re.search(month_date_re, content_date)
-        month, day = jp_spider._intall(date_match.groups())
-        start_date = datetime.date.today().replace(month=month, day=day)
-        if datetime.date.today() - datetime.timedelta(days=7) > start_date:
-            start_date = start_date.replace(year=start_date.year + 1)
-        return jp_spider.parse_date_times(start_date, post_text)
+    def parseDateTimes(self, content_date):
+        if ' ' in content_date:
+            date_string, times_string = content_date.split(' ', 1)
+        else:
+            date_string = content_date
+            times_string = ''
+        date = datetime.datetime.strptime(date_string.strip(), '%Y/%m/%d').date()
+        return jp_spider.parse_date_times(date, times_string.strip())
 
     def parseEvent(self, response):
         print response.url
@@ -50,29 +51,22 @@ class DanceDelightScraper(items.WebEventScraper):
         item = items.WebEvent()
         item['country'] = 'JP'
         item['namespace'] = self.namespace
-        item['namespaced_id'] = re.search(r'\?p=(\d+)', response.url).group(1)
-        item['name'] = items.extract_text(response.xpath('//a[@rel="bookmark"]/text()'))
+        item['namespaced_id'] = re.search(r'/event/(\d+)', response.url).group(1)
+        item['name'] = items.extract_text(response.css('div.title h2').xpath('.//text()'))
 
-        post = response.css('.entry-content')
-
-        photos = post.css('img.size-full').xpath('./@src').extract()
+        photos = response.css('a.cb_photo').xpath('./@href').extract()
         if photos:
             item['photo'] = self.abs_url(response, photos[0])
         else:
             item['photo'] = None
 
-        post_html = post.extract()[0]
-        post_top = response.css('.social4i').extract()[0]
-        post_html = post_html.replace(post_top, '')
-
-        full_description = items.format_text(post_html)
-        item['description'] = full_description
+        item['description'] = items.format_text(response.css('div.tag_preview').extract()[0])
 
         jp_addresses = japanese_addresses.find_addresses(item['description'])
         venue = jp_spider.get_venue_from_description(item['description'])
         jp_spider.setup_location(venue, jp_addresses, item)
 
-        content_date = ''.join(response.css('.contentdate').xpath('.//text()').extract())
-        item['start_time'], item['end_time'] = self.parseDateTimes(content_date, full_description)
+        content_date = response.css('tr.event_date td').xpath('./text()').extract()[0]
+        item['start_time'], item['end_time'] = self.parseDateTimes(content_date.strip())
 
         yield item
