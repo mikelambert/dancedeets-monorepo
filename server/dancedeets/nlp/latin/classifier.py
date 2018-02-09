916d54ae9e02b13877a245da1a469fa7c3d602b5
Mike Lambert <mlambert@gmail.com>
2018-02-08 15:35:48 -1000

    Handle food-salsa differently

--- server/dancedeets/nlp/latin/classifier.py
+++ server/dancedeets/nlp/latin/classifier.py
@@ -2,7 +2,7 @@
 
 from dancedeets import event_types
 from .. import grammar
-from ..street.keywords import EASY_DANCE
+from ..street import keywords
 
 Any = grammar.Any
 Name = grammar.Name
@@ -10,7 +10,7 @@ connected = grammar.connected
 commutative_connected = grammar.commutative_connected
 
 REAL_SALSA = Name(
-    'REAL_SALSA',
+    'LATIN_REAL_SALSA',
     Any(
         'salsa on1',
         'salsa on2',
@@ -21,27 +21,40 @@ REAL_SALSA = Name(
         'salsa\W?tanz',
         'lad(?:y|ies) styling|styling ladies',
         'shines ladies|ladies shines',
+        'salser[oa]s?',
     )
 )
 
+SALSA = Any(
+    'salsa',
+    u'サルサ',
+)
+
 AMBIGUOUS_DANCE_MUSIC = Name(
-    'AMBIGUOUS_DANCE_MUSIC',
-    Any(
-        'salsa',
-        u'サルサ',
+    'LATIN_AMBIGUOUS_DANCE_MUSIC', Any(
         'chacha',
         'samba',
         u'サンバ',
         'bachata',
         u'バチャータ',
         'merengue',
-        'salsa',
         'rh?umba',
         'afro\W?[ck]uba\w+',
     )
 )
 
-GOOD_DANCE = commutative_connected(AMBIGUOUS_DANCE_MUSIC, EASY_DANCE),
+FOOD = Any(
+    'food',
+    'tastings',
+    'cinco de mayo',
+)
+
+GOOD_DANCE = commutative_connected(AMBIGUOUS_DANCE_MUSIC, keywords.EASY_DANCE)
+
+good_battle = Any(keywords.BATTLE, keywords.N_X_N, keywords.CONTEST)
+GOOD_DANCE_BATTLE = Name('LATIN_GOOD_DANCE_BATTLE', commutative_connected(AMBIGUOUS_DANCE_MUSIC, good_battle))
+
+SALSA_DANCE_BATTLE = Name('LATIN_SALSA_DANCE_BATTLE', commutative_connected(SALSA, good_battle))
 
 
 def is_dance_event(classified_event):
@@ -52,5 +65,14 @@ def is_dance_event(classified_event):
     dance_keywords = classified_event.processed_text.get_tokens(GOOD_DANCE)
     if dance_keywords:
         return (True, 'Found strong dance keywords: %s' % dance_keywords, event_types.VERTICALS.LATIN)
+
+    good_contest = classified_event.processed_text.get_tokens(GOOD_DANCE_BATTLE)
+    if good_contest:
+        return (True, 'Found dance contest: %s' % good_contest, event_types.VERTICALS.LATIN)
+
+    salsa_contest = classified_event.processed_text.get_tokens(SALSA_DANCE_BATTLE)
+    food_terms = classified_event.processed_text.get_tokens(FOOD)
+    if salsa_contest and not food_terms:
+        return (True, 'Found non-food salsa contest: %s' % salsa_contest, event_types.VERTICALS.LATIN)
 
     return (False, '', [])
