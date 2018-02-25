77e5ea2a541a0909c0319e95d55bea757dad42bc
Mike Lambert <mlambert@gmail.com>
2018-02-25 06:08:41 -0500

    Add a script to fetch a bunch of ids and store them as-is. (False positives suck and require reworking properly)

---
+++ server/dancedeets/nlp/styles/tests/generate_test_ids.py
@@ -0,0 +1,51 @@
+# -*-*- encoding: utf-8 -*-*-
+
+import logging
+import os
+import time
+
+logging.basicConfig(level=logging.INFO)
+
+from dancedeets import runner
+
+runner.setup()
+
+from dancedeets import fb_api
+from dancedeets.event_scraper import keyword_search
+from dancedeets.nlp import styles
+from dancedeets.test_utils import unittest
+
+TEST_IDS_PATH = os.path.join(os.path.dirname(styles.__file__), 'test_ids')
+
+
+def load_for(style_name, fbl):
+    all_ids = set()
+    chunks = keyword_search.get_chunks(style_name)
+    for chunk in chunks:
+        all_keywords = keyword_search.expand_chunk(chunk)
+        for query in all_keywords:
+            logging.info('Loading %s', query)
+            ids = keyword_search.get_ids_for_keyword(fbl, query)
+            time.sleep(2)
+            all_ids.update(ids)
+    return all_ids
+
+
+def write_ids(style_name, ids):
+    filename = os.path.join(TEST_IDS_PATH, '%s.txt' % style_name)
+    f = open(filename, 'w')
+    for id in ids:
+        f.write('%s:%s\n' % (style_name, id))
+    f.close()
+
+
+def main():
+    style_name = 'HUSTLE'
+    fbl = fb_api.FBLookup("dummyid", unittest.get_local_access_token_for_testing())
+    fbl.make_passthrough()
+    all_ids = load_for(style_name, fbl)
+    write_ids(style_name, all_ids)
+
+
+if __name__ == '__main__':
+    main()
