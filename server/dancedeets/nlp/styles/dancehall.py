78a12ce4a2efb904a5e8004adf245cf5439e1f49
Mike Lambert <mlambert@gmail.com>
2018-03-07 19:02:33 -0500

    Add soca/calypso

--- server/dancedeets/nlp/styles/dancehall.py
+++ server/dancedeets/nlp/styles/dancehall.py
@@ -16,16 +16,60 @@ REAL_DANCE = Any(
     u'דאנסהול',  # hebrew dancehall
 )
 
-AMBIGUOUS_DANCE = Any(
-    'dancehall',
+DANCEHALL = Any(
+    u'dancehall',
     u'ダンスホール',
     u'댄스홀',
-    'reggae',
-    u'レゲエ',
-    u'레게',
-    'reggaeton',
-    u'レゲトン',
-    u'레게 ?톤',
+)
+
+REGGAE = Any(
+    u'rege muzika',  # croatian
+    u'reggae',  # english
+    u'ρέγκε',  # greek
+    u'регги',  # russian
+    u'реге',  # macedonian (not english letters)
+    u'רגאיי',  # hebrew
+    u'الريغي',  # arabic
+    u'เร้กเก้',  # thai
+    u'レゲエ',  # japanese
+    u'雷鬼',  # chinese simplified
+    u'레게',  # korean
+)
+
+REGGAETON = Any(
+    u"רג 'טון",  # hebrew
+    u'reggaeton\w*',  # english
+    u'reguetón',  # spanish
+    u'реггитон',  # russian
+    u'регетон',  # macedonian
+    u'レゲトン',  # japanese
+    u'레게 ?톤',  # korean
+)
+
+SOCA = Any(u'soca',)
+
+CALYPSO = Any(
+    u'calipso',  # romanian
+    u'calypso',  # english
+    u'kalipso',  # croatian
+    u'türkü',  # turkish
+    u'είδος χορού των δυτικών ινδίων',  # greek
+    u'калипсо',  # macedonian
+    u'קליפסו',  # hebrew
+    u'كاليبسو',  # arabic
+    u'คาลิปโซ่',  # thai
+    u'カリプソ',  # japanese
+    u'卡吕普索',  # chinese simplified
+    u'卡呂普索',  # chinese traditional
+    u'칼립소',  # korean
+)
+
+AMBIGUOUS_DANCE = Any(
+    DANCEHALL,
+    REGGAE,
+    REGGAETON,
+    SOCA,
+    CALYPSO,
 )
 
 
@@ -46,6 +90,8 @@ class Style(style_base.Style):
     def get_rare_search_keywords(cls):
         return [
             u'ダンスホール',
+            u'レゲエ',  # japanese
+            u'レゲトン',  # japanese
         ]
 
     @classmethod
@@ -55,6 +101,8 @@ class Style(style_base.Style):
             'ragga jam',
             'reggae',
             'reggaeton',
+            'soca',
+            'calypso',
         ]
 
     @classmethod
